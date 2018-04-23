a3d86c4bc26ef233d47454078c3081021fe7a279
Edric Chan <edric.chan.1997@gmail.com>
2018-04-23 10:38:04 +0800

    chore(changelog): add conventional changelog

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const conventionalChangelog = require('gulp-conventional-changelog');
+
+gulp.task('changelog', () => {
+	return gulp.src('CHANGELOG.md')
+		.pipe(conventionalChangelog({
+			preset: 'angular',
+			// Regenerate everything
+			releaseCount: 0
+		}))
+		.pipe(gulp.dest('./'));
+})
