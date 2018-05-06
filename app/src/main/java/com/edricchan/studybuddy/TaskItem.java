c98c246a1603559a7217af07ebeaaf4711f920f3
Edric Chan <edric.chan.1997@gmail.com>
2018-05-06 16:38:31 +0800

    chore(task-item-class): restructure to use public variables

--- app/src/main/java/com/edricchan/studybuddy/TaskItem.java
+++ app/src/main/java/com/edricchan/studybuddy/TaskItem.java
@@ -7,87 +7,23 @@ import java.util.Date;
  */
 
 public class TaskItem {
-	private String title;
-	private String project;
-	private String content;
-	private Date dueDate;
+	public String title;
+	public String project;
+	public String content;
+	public Date dueDate;
+	public TaskItem() {
 
-	public TaskItem(String title, Date dueDate, String project, String content) {
+	}
+	public TaskItem(String title, String project, String content, Date dueDate) {
 		this.title = title;
-		this.dueDate = dueDate;
 		this.project = project;
 		this.content = content;
-	}
-
-	/**
-	 * Gets the task name.
-	 *
-	 * @return The name of the task in a string format.
-	 */
-	public String getTitle() {
-		return title;
-	}
-
-	/**
-	 * Sets the task name.
-	 *
-	 * @param title The task name.
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	/**
-	 * Gets the task due date.
-	 *
-	 * @return The task's due date in a `Date` format.
-	 */
-	public Date getDueDate() {
-		return dueDate;
-	}
-
-	/**
-	 * Sets the task's due date.
-	 *
-	 * @param dueDate The task's due date. (Date)
-	 */
-	public void setDueDate(Date dueDate) {
 		this.dueDate = dueDate;
 	}
-
-	/**
-	 * Gets the task project.
-	 *
-	 * @return The task's project in a <code>string</code> format.
-	 */
-	public String getProject() {
-		return project;
-	}
-
-	/**
-	 * Sets the task project.
-	 *
-	 * @param project The task's project. (String)
-	 */
-	public void setProject(String project) {
+	public TaskItem(String title, String project, String content) {
+		this.title = title;
 		this.project = project;
-	}
-
-	/**
-	 * Gets the task content.
-	 *
-	 * @return The task's content in a <code>string</code> format.
-	 */
-	public String getContent() {
-		return content;
-	}
-
-	/**
-	 * Sets the task content.
-	 *
-	 * @param content The task's content. (<code>String</code>)
-	 */
-	public void setContent(String content) {
 		this.content = content;
+		this.dueDate = new Date();
 	}
 }
