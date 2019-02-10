33cdfe8d462d6dda46d690d7eb4b14808b65d415
Edric Chan <edric.chan.1997@gmail.com>
2019-02-10 23:03:34 +0800

    Add support for Kotlin extension functions

---
+++ app/src/main/java/com/edricchan/studybuddy/extensions/TextInputEditTextExts.kt
@@ -0,0 +1,25 @@
+package com.edricchan.studybuddy.extensions
+
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import com.google.android.material.textfield.TextInputEditText
+
+/**
+ * Getter/setter for a [TextInputEditText]'s value
+ * Returns/requires a [String]
+ */
+var TextInputEditText.strValue: String?
+	get() = this.text.toString()
+	set(value) {
+		this.text = SpannableStringBuilder(value)
+	}
+
+/**
+ * Getter/setter for a [TextInputEditText]'s value
+ * Returns/requires an [Editable]
+ */
+var TextInputEditText.value: Editable?
+	get() = this.text
+	set(value) {
+		this.text = value
+	}
---
+++ app/src/main/java/com/edricchan/studybuddy/extensions/TextInputLayoutExts.kt
@@ -0,0 +1,25 @@
+package com.edricchan.studybuddy.extensions
+
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import com.google.android.material.textfield.TextInputLayout
+
+/**
+ * Getter/setter for the [TextInputLayout]'s [EditText] value
+ * Returns/requires an [Editable]
+ */
+var TextInputLayout.editTextValue: Editable?
+	get() = this.editText?.text
+	set(value) {
+		this.editText?.text = value
+	}
+
+/**
+ * Getter/setter for the [TextInputLayout]'s [EdiText] value
+ * Returns/requires a [String]
+ */
+var TextInputLayout.editTextStrValue: String?
+	get() = this.editText?.text.toString()
+	set(value) {
+		this.editText?.text = SpannableStringBuilder(value)
+	}
