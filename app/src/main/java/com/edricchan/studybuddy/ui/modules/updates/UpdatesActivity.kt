8e0178d0685a5903d3b6f9a26d9b16a81a6405bc
Edric Chan <edric.chan.1997@gmail.com>
2019-03-30 22:12:47 +0800

    UpdatesActivity: Use Kotlin view binding Android extension; update imports; document code

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/updates/UpdatesActivity.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/updates/UpdatesActivity.kt
@@ -2,10 +2,7 @@ package com.edricchan.studybuddy.ui.modules.updates
 
 import android.Manifest
 import android.app.DownloadManager
-import android.content.Context
-import android.content.DialogInterface
-import android.content.Intent
-import android.content.SharedPreferences
+import android.content.*
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -15,15 +12,13 @@ import android.provider.Settings
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.annotation.ContentView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.edricchan.studybuddy.BuildConfig
 import com.edricchan.studybuddy.R
+import com.edricchan.studybuddy.utils.IOUtils
 import com.edricchan.studybuddy.utils.SharedUtils
 import com.github.javiersantos.appupdater.AppUpdaterUtils
 import com.github.javiersantos.appupdater.enums.AppUpdaterError
@@ -31,26 +26,23 @@ import com.github.javiersantos.appupdater.enums.UpdateFrom
 import com.github.javiersantos.appupdater.objects.Update
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_updates.*
 import java.io.File
-import java.util.*
 
 @ContentView(R.layout.activity_updates)
 class UpdatesActivity : AppCompatActivity() {
 
+	// Whether the user has pressed the "check for updates" menu item
 	private var isChecking = false
 	// The tag used for Log.* calls
 	private val TAG = SharedUtils.getTag(this::class.java)
 	private lateinit var appUpdate: Update
-	private lateinit var checkForUpdatesBtn: Button
-	private lateinit var emptyStateLayout: RelativeLayout
 	private lateinit var preferences: SharedPreferences
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		checkForUpdatesBtn = findViewById(R.id.empty_state_view_cta)
-		checkForUpdatesBtn.setOnClickListener { click -> checkForUpdates() }
-		emptyStateLayout = findViewById(R.id.updates_empty_state_view)
 		supportActionBar?.setDisplayHomeAsUpEnabled(true)
+		emptyStateCtaBtn.setOnClickListener { checkForUpdates() }
 		preferences = PreferenceManager.getDefaultSharedPreferences(this)
 	}
 
@@ -66,8 +58,8 @@ class UpdatesActivity : AppCompatActivity() {
 				true
 			}
 			R.id.action_check_for_updates -> {
-				Snackbar.make(findViewById<View>(R.id.updatesView), R.string.update_snackbar_checking, Snackbar.LENGTH_SHORT)
 				Log.d(TAG, "Check for updates clicked!")
+				Snackbar.make(mainCoordinatorLayout, R.string.update_snackbar_checking, Snackbar.LENGTH_SHORT)
 						.show()
 				checkForUpdates()
 				true
@@ -138,16 +130,16 @@ class UpdatesActivity : AppCompatActivity() {
 			val builder = MaterialAlertDialogBuilder(this)
 			builder.setTitle(R.string.update_activity_update_already_downloaded_dialog_title)
 			builder.setMessage(R.string.update_activity_update_already_downloaded_dialog_msg)
-			builder.setPositiveButton(R.string.update_activity_update_already_downloaded_dialog_positive_btn) { dialogInterface: DialogInterface, i: Int ->
+			builder.setPositiveButton(R.string.update_activity_update_already_downloaded_dialog_positive_btn) { dialogInterface: DialogInterface, _: Int ->
 				installUpdate(fileName)
 				dialogInterface.dismiss()
 			}
-			builder.setNegativeButton(R.string.update_activity_update_already_downloaded_dialog_negative_btn) { dialogInterface: DialogInterface, i: Int ->
+			builder.setNegativeButton(R.string.update_activity_update_already_downloaded_dialog_negative_btn) { _: DialogInterface, _: Int ->
 				if (File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/" + fileName).delete()) {
-					Toast.makeText(this@UpdatesActivity, R.string.update_activity_delete_update_success_toast, Toast.LENGTH_LONG).show()
+					Toast.makeText(this, R.string.update_activity_delete_update_success_toast, Toast.LENGTH_LONG).show()
 					downloadUpdate(downloadUrl, version, false, true)
 				} else {
-					Toast.makeText(this@UpdatesActivity, R.string.update_activity_cannot_delete_update_toast, Toast.LENGTH_LONG).show()
+					Toast.makeText(this, R.string.update_activity_cannot_delete_update_toast, Toast.LENGTH_LONG).show()
 				}
 			}
 			builder.show()
@@ -158,26 +150,33 @@ class UpdatesActivity : AppCompatActivity() {
 		// See https://android-developers.googleblog.com/2017/08/making-it-safer-to-get-apps-on-android-o.html
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-			if (this@UpdatesActivity.packageManager.canRequestPackageInstalls()) {
+			if (packageManager.canRequestPackageInstalls()) {
 				val installIntent = Intent(Intent.ACTION_VIEW)
+				// Set the data for the intent to open
 				installIntent.setDataAndType(FileProvider.getUriForFile(applicationContext, "com.edricchan.studybuddy.provider", File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/" + fileName)), "application/vnd.android.package-archive")
+				// Mark this as a new task
 				installIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 				// Allow reading the file
 				installIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+				// Lastly, start the activity
 				startActivity(installIntent)
 			} else {
+				// User has not allowed the app as an unknown app source
 				val builder = MaterialAlertDialogBuilder(this)
 				builder.setMessage(R.string.update_activity_enable_unknown_sources_dialog_msg)
 						.setNegativeButton(R.string.dialog_action_cancel) { dialog, which -> dialog.dismiss() }
 						.setNeutralButton(R.string.dialog_action_retry) { dialog, which ->
 							dialog.dismiss()
+							// Call this method again
 							installUpdate(fileName)
 						}
 						.setPositiveButton(R.string.update_activity_enable_unknown_sources_dialog_positive_btn) { dialog, which ->
+							// Create an intent to take the user to Settings for unknown app sources
 							val allowUnknownAppsIntent = Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES)
 							allowUnknownAppsIntent.data = Uri.parse("package:$packageName")
 							startActivity(allowUnknownAppsIntent)
 						}
+						// Mark it as non-dismissable
 						.setCancelable(false)
 						.show()
 			}
@@ -192,7 +191,6 @@ class UpdatesActivity : AppCompatActivity() {
 	}
 
 	private fun showUpdateDialog() {
-		// New update
 		val builder = MaterialAlertDialogBuilder(this)
 		builder.setTitle(getString(R.string.update_dialog_title_new, appUpdate.latestVersion))
 		builder.setIcon(R.drawable.ic_system_update_24dp)
@@ -205,6 +203,7 @@ class UpdatesActivity : AppCompatActivity() {
 				if (SharedUtils.checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, this@UpdatesActivity)) {
 					downloadUpdate(appUpdate.urlToDownload.toString(), appUpdate.latestVersion)
 				} else {
+					// User has pressed "Deny" to the permission prompt
 					if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
 						val permBuilder = MaterialAlertDialogBuilder(this)
 						permBuilder.setTitle(R.string.update_perm_rationale_dialog_title)
@@ -213,6 +212,8 @@ class UpdatesActivity : AppCompatActivity() {
 								.setPositiveButton(R.string.update_perm_rationale_dialog_grant) { dialog, which -> requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0) }
 								.show()
 					} else {
+						// Request permissions for writting to the external storage
+						// Note: requestPermissions requires an array of permissions as the first parameter
 						requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
 					}
 				}
@@ -220,11 +221,13 @@ class UpdatesActivity : AppCompatActivity() {
 				downloadUpdate(appUpdate.urlToDownload.toString(), appUpdate.latestVersion)
 			}
 		}
-		builder.setOnDismissListener { dialogInterface ->
+		builder.setOnDismissListener {
+			// Mark the check for updates menu item as selectable again
 			isChecking = false
 			invalidateOptionsMenu()
 		}
-		builder.setOnCancelListener { dialogInterface ->
+		builder.setOnCancelListener {
+			// Mark the check for updates menu item as selectable again
 			isChecking = false
 			invalidateOptionsMenu()
 		}
@@ -242,7 +245,7 @@ class UpdatesActivity : AppCompatActivity() {
 						appUpdate = update
 						if (update.latestVersionCode == BuildConfig.VERSION_CODE && !updateAvailable!!) {
 							// User is running latest version
-							Snackbar.make(findViewById<View>(R.id.updatesView), R.string.update_snackbar_latest, Snackbar.LENGTH_SHORT)
+							Snackbar.make(mainCoordinatorLayout, R.string.update_snackbar_latest, Snackbar.LENGTH_SHORT)
 									.show()
 						} else {
 							showUpdateDialog()
@@ -253,13 +256,13 @@ class UpdatesActivity : AppCompatActivity() {
 						isChecking = false
 						invalidateOptionsMenu()
 						when (appUpdaterError) {
-							AppUpdaterError.NETWORK_NOT_AVAILABLE -> Snackbar.make(findViewById<View>(R.id.updatesView), R.string.update_snackbar_error_no_internet, Snackbar.LENGTH_LONG)
+							AppUpdaterError.NETWORK_NOT_AVAILABLE -> Snackbar.make(mainCoordinatorLayout, R.string.update_snackbar_error_no_internet, Snackbar.LENGTH_LONG)
 									.setAction(R.string.dialog_action_retry) { checkForUpdates() }
 									.show()
-							AppUpdaterError.JSON_ERROR -> Snackbar.make(findViewById<View>(R.id.updatesView), R.string.update_snackbar_error_malformed, Snackbar.LENGTH_LONG)
+							AppUpdaterError.JSON_ERROR -> Snackbar.make(mainCoordinatorLayout, R.string.update_snackbar_error_malformed, Snackbar.LENGTH_LONG)
 									.setAction(R.string.dialog_action_retry) { checkForUpdates() }
 									.show()
-							else -> Snackbar.make(findViewById<View>(R.id.updatesView), R.string.update_snackbar_error, Snackbar.LENGTH_LONG)
+							else -> Snackbar.make(mainCoordinatorLayout, R.string.update_snackbar_error, Snackbar.LENGTH_LONG)
 									.setAction(R.string.dialog_action_retry) { checkForUpdates() }
 									.show()
 						}
