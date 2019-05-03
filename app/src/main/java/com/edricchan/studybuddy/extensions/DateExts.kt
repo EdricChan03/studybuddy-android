2c9d2f8d0ab6f15dc4c6b3b0fa67eef9117e8a4b
Edric Chan <edric.chan.1997@gmail.com>
2019-05-03 20:56:32 +0800

    DateExts: Add extension function to convert a date to a formatted string

--- app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
@@ -1,6 +1,7 @@
 package com.edricchan.studybuddy.extensions
 
 import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -8,3 +9,12 @@ import java.util.*
  * @return A [Timestamp]
  */
 fun Date?.toTimestamp() = Timestamp(this)
+
+/**
+ * Converts a date to the specified [format]
+ * @param format A format that [SimpleDateFormat] supports
+ * @see [SimpleDateFormat docs](https://docs.oracle.com/javase/8/docs/api/java/text/SimpleDateFormat.html)
+ */
+fun Date.toFormat(format: String): String {
+	return SimpleDateFormat(format, Locale.getDefault()).format(this)
+}
