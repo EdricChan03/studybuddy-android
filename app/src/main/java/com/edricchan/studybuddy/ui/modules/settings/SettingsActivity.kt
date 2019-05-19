a327fabe7b2df57a032d595da41ac8c3aadece5a
Edric Chan <edric.chan.1997@gmail.com>
2019-05-19 14:42:25 +0800

    SettingsActivity: Update `Constants` import location

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/settings/SettingsActivity.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/settings/SettingsActivity.kt
@@ -12,10 +12,10 @@ import androidx.preference.PreferenceManager
 import com.edricchan.studybuddy.R
 import com.edricchan.studybuddy.annotations.AppDeepLink
 import com.edricchan.studybuddy.annotations.WebDeepLink
+import com.edricchan.studybuddy.constants.Constants
 import com.edricchan.studybuddy.extensions.startActivity
 import com.edricchan.studybuddy.ui.modules.help.HelpActivity
 import com.edricchan.studybuddy.ui.modules.settings.fragment.SettingsFragment
-import com.edricchan.studybuddy.utils.Constants
 import com.edricchan.studybuddy.utils.SharedUtils
 
 @WebDeepLink(["/settings"])
