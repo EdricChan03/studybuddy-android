20d3a7c3b2c2b711bc7a42662e68b6308a8aa313
Edric Chan <edric.chan.1997@gmail.com>
2019-05-20 01:51:41 +0800

    LongExts: Add new extensions for converting a Long to a date format

--- app/src/main/java/com/edricchan/studybuddy/extensions/LongExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/LongExts.kt
@@ -1,5 +1,6 @@
 package com.edricchan.studybuddy.extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -11,3 +12,19 @@ fun Long.toDate(): Date = Date(this)
  * Converts a [Long] to a [Date]
  */
 fun Long?.toDate(): Date? = this?.let { Date(it) }
+
+/**
+ * Converts a [Long] to a [SimpleDateFormat]
+ * @param format A format that [SimpleDateFormat] supports
+ * @see SimpleDateFormat
+ * @see Date.toFormat
+ */
+fun Long.toDateFormat(format: String) = SimpleDateFormat(format, Locale.getDefault()).format(Date(this))
+
+/**
+ * Converts a [Long] to a [SimpleDateFormat]
+ * @param format A format that [SimpleDateFormat] supports
+ * @see SimpleDateFormat
+ * @see Date.toFormat
+ */
+fun Long?.toDateFormat(format: String) = this?.let { SimpleDateFormat(format, Locale.getDefault()).format(Date(it)) }
