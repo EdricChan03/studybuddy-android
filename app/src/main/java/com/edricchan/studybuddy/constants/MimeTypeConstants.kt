49cea983dd8484731a6c951422c5f599c759842e
Edric Chan <edric.chan.1997@gmail.com>
2019-05-19 14:31:58 +0800

    Add new constant objects

---
+++ app/src/main/java/com/edricchan/studybuddy/constants/MimeTypeConstants.kt
@@ -0,0 +1,16 @@
+package com.edricchan.studybuddy.constants
+
+/**
+ * Common constants for MIME types
+ */
+object MimeTypeConstants {
+	/**
+	 * Indicates that the MIME type should be `text/plain`
+	 */
+	const val textPlainMime = "text/plain"
+
+	/**
+	 * Indicates that the MIME type should be `application/vnd.android.package-archive`
+	 */
+	const val appPackageArchiveMime = "application/vnd.android.package-archive"
+}
---
+++ app/src/main/java/com/edricchan/studybuddy/constants/sharedprefs/UpdateInfoPrefConstants.kt
@@ -0,0 +1,23 @@
+package com.edricchan.studybuddy.constants.sharedprefs
+
+/**
+ * Constants to be used for shared preferences regarding update info
+ */
+object UpdateInfoPrefConstants {
+	/**
+	 * File to be used for storing information about updates
+	 */
+	const val FILE_UPDATE_INFO = "update_info"
+	/**
+	 * Indicates when the app was last updated via the Update UI in Settings > Versions & About
+	 *
+	 * This key is stored as a [Long] representation of a date (Use [java.util.Date.getTime] to convert a [java.util.Date] to a [Long])
+	 */
+	const val PREF_LAST_UPDATED_DATE = "last_updated_date"
+	/**
+	 * Indicates when the app last checked for updates
+	 *
+	 * This key is stored as a [Long] representation of a date (Use [java.util.Date.getTime] to convert a [java.util.Date] to a [Long])
+	 */
+	const val PREF_LAST_CHECKED_FOR_UPDATES_DATE = "last_checked_for_updates_date"
+}
