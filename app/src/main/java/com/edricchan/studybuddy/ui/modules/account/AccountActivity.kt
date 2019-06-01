b0273893bd0f15299e53cc391b4c8066bfb683cf
Edric Chan <edric.chan.1997@gmail.com>
2019-06-01 23:57:38 +0800

    strings: Cleanup account activity strings

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/account/AccountActivity.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/account/AccountActivity.kt
@@ -45,7 +45,7 @@ class AccountActivity : AppCompatActivity(R.layout.activity_account), FirebaseAu
 		accountActionsButton
 				.setOnClickListener {
 					val builder = MaterialAlertDialogBuilder(this)
-					builder.setTitle(R.string.account_activity_account_actions)
+					builder.setTitle(R.string.account_account_actions_button_title)
 							.setItems(R.array.account_activity_account_actions_array) { dialog, which ->
 								when (which) {
 									0 -> deleteAccount()
@@ -149,7 +149,7 @@ class AccountActivity : AppCompatActivity(R.layout.activity_account), FirebaseAu
 
 	private fun deleteAccount() {
 		val confirmBuilder = MaterialAlertDialogBuilder(this)
-		confirmBuilder.setTitle(R.string.account_activity_delete_account_dialog_title)
+		confirmBuilder.setTitle(R.string.account_delete_account_dialog_title)
 				.setNegativeButton(R.string.dialog_action_cancel) { dialog, _ -> dialog.dismiss() }
 				.setPositiveButton(R.string.dialog_action_delete_account) { _, _ ->
 					mUser?.delete()
@@ -196,7 +196,7 @@ class AccountActivity : AppCompatActivity(R.layout.activity_account), FirebaseAu
 
 	private fun signOut() {
 		val confirmBuilder = MaterialAlertDialogBuilder(this)
-		confirmBuilder.setTitle(R.string.account_activity_sign_out_dialog_title)
+		confirmBuilder.setTitle(R.string.account_sign_out_dialog_title)
 				.setNegativeButton(R.string.dialog_action_cancel) { dialog, _ -> dialog.dismiss() }
 				.setPositiveButton(R.string.dialog_action_sign_out) { dialog, _ ->
 					mAuth?.signOut()
@@ -209,11 +209,11 @@ class AccountActivity : AppCompatActivity(R.layout.activity_account), FirebaseAu
 	private fun updateEmail() {
 		val promptDialogView = layoutInflater.inflate(R.layout.edit_text_dialog, null)
 		val textInputLayout = promptDialogView.findViewById<TextInputLayout>(R.id.textInputLayout)
-		textInputLayout.editText?.setHint(R.string.account_activity_new_email_dialog_edittext_title)
+		textInputLayout.editText?.setHint(R.string.account_new_email_dialog_edittext_title)
 		// TODO: Add check for email address
 		val promptBuilder = MaterialAlertDialogBuilder(this)
 		promptBuilder.setView(promptDialogView)
-				.setTitle(R.string.account_activity_new_email_dialog_title)
+				.setTitle(R.string.account_new_email_dialog_title)
 				.setPositiveButton(R.string.dialog_action_update_email) { dialog, which ->
 					textInputLayout.editTextStrValue?.let {
 						mUser?.updateEmail(it)
@@ -234,10 +234,10 @@ class AccountActivity : AppCompatActivity(R.layout.activity_account), FirebaseAu
 	private fun updateName() {
 		val promptDialogView = layoutInflater.inflate(R.layout.edit_text_dialog, null)
 		val textInputLayout = promptDialogView.findViewById<TextInputLayout>(R.id.textInputLayout)
-		textInputLayout.editText?.setHint(R.string.account_activity_new_name_dialog_edittext_title)
+		textInputLayout.editText?.setHint(R.string.account_new_name_dialog_edittext_title)
 		val promptBuilder = MaterialAlertDialogBuilder(this)
 		promptBuilder.setView(promptDialogView)
-				.setTitle(R.string.account_activity_new_name_dialog_title)
+				.setTitle(R.string.account_new_name_dialog_title)
 				.setPositiveButton(R.string.dialog_action_update_name) { dialog, _ ->
 					val requestBuilder = UserProfileChangeRequest.Builder()
 					requestBuilder.setDisplayName(textInputLayout.editTextStrValue)
@@ -258,10 +258,10 @@ class AccountActivity : AppCompatActivity(R.layout.activity_account), FirebaseAu
 	private fun updatePassword() {
 		val promptDialogView = layoutInflater.inflate(R.layout.edit_text_dialog, null)
 		val textInputLayout = promptDialogView.findViewById<TextInputLayout>(R.id.textInputLayout)
-		textInputLayout.editText?.setHint(R.string.account_activity_new_password_dialog_edittext_title)
+		textInputLayout.editText?.setHint(R.string.account_new_password_dialog_edittext_title)
 		val promptBuilder = MaterialAlertDialogBuilder(this)
 		promptBuilder.setView(promptDialogView)
-				.setTitle(R.string.account_activity_new_password_dialog_title)
+				.setTitle(R.string.account_new_password_dialog_title)
 				.setPositiveButton(R.string.dialog_action_update_password) { dialog, _ ->
 					textInputLayout.editTextStrValue?.let {
 						mUser?.updatePassword(it)
@@ -282,8 +282,8 @@ class AccountActivity : AppCompatActivity(R.layout.activity_account), FirebaseAu
 	private fun updateProfilePicture() {
 		// TODO: Add support for updating a profile picture
 		val promptBuilder = MaterialAlertDialogBuilder(this)
-		promptBuilder.setTitle(R.string.account_activity_new_profile_pic_dialog_title)
-				.setMessage(R.string.account_activity_new_profile_pic_dialog_msg)
+		promptBuilder.setTitle(R.string.account_new_profile_pic_dialog_title)
+				.setMessage(R.string.account_new_profile_pic_dialog_msg)
 				.setPositiveButton(R.string.dialog_action_update_profile_picture) { dialog, _ ->
 					Toast.makeText(this, "Successfully updated profile picture!", Toast.LENGTH_SHORT).show()
 					dialog.dismiss()
