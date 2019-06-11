b056e578cd5679bc1a96cb2bc626528ff9628cc5
Edric Chan <edric.chan.1997@gmail.com>
2019-06-11 23:57:24 +0800

    DateExts: Add support for specifying locale for `toFormat` extension

--- app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
@@ -13,6 +13,8 @@ fun Date?.toTimestamp() = Timestamp(this)
 /**
  * Converts a date to the specified [format]
  * @param format A format that [SimpleDateFormat] supports
+ * @param locale The locale to be used to convert the date
  * @see [SimpleDateFormat docs](https://docs.oracle.com/javase/8/docs/api/java/text/SimpleDateFormat.html)
  */
-fun Date.toFormat(format: String): String = SimpleDateFormat(format, Locale.getDefault()).format(this)
+fun Date.toFormat(format: String, locale: Locale = Locale.getDefault()): String = SimpleDateFormat(format, locale)
+		.format(this)
