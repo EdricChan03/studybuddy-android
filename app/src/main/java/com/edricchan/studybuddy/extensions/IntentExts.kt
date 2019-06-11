ba40b56f1bddca298bedd3616992e01911b1af30
Edric Chan <edric.chan.1997@gmail.com>
2019-06-12 00:40:38 +0800

    IntentExts: Add KDoc for what the functions return

--- app/src/main/java/com/edricchan/studybuddy/extensions/IntentExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/IntentExts.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 /**
  * A builder to create an [Intent]
  * @param builderAction Inline functions to be called within the created [Intent]
+ * @return The created intent
  */
 inline fun buildIntent(builderAction: Intent.() -> Unit) = Intent().apply(builderAction)
 
@@ -12,6 +13,8 @@ inline fun buildIntent(builderAction: Intent.() -> Unit) = Intent().apply(builde
  * A builder to create an [Intent]
  * @param context The context of the [Intent]
  * @param builderAction Inline functions to be called within the created [Intent]
+ * @param T A valid Java class
+ * @return The created intent
  */
 inline fun <reified T> buildIntent(
 		context: Context,
