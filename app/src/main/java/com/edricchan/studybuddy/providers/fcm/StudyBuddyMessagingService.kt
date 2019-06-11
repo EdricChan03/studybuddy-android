b5ff9b907f4b3304ba2b92913987fb4ef33db4f0
Edric Chan <edric.chan.1997@gmail.com>
2019-06-12 00:37:20 +0800

    StudyBuddyMessagingService: Code cleanup
    The code was rather messy, so I decided to clean up the code.
    
    Changes made:
    * Use string templates where possible
    * Remove redundant comments that don't benefit future people (or me!) who read the code
    * Remove support for specifying custom notification channel ID in the remote msg's data (this is already supported in the FCM API)
    * Use `NotificationManagerCompat` class instead of platform `NotificationManager`
    * Add linebreaks where needed to separate code blocks
    * Use `Resources.ID_NULL` instead of `0` where possible
    * Don't use custom `isNotNull`/`isNull` extension functions - replace them with their former versions
    * Assign the remote message the default notification channel `uncategorised` if no such channel ID exists (and log a warning to logcat about this)
    * Use `listOf` Kotlin function instead of `ArrayList` constructor
    * Use `Log.e` instead of directly printing stacktrace
    * Use safe call operators for null safety
    * Use `buildIntent` extension where possible
    * Other minor fixes probably :P

--- app/src/main/java/com/edricchan/studybuddy/providers/fcm/StudyBuddyMessagingService.kt
+++ app/src/main/java/com/edricchan/studybuddy/providers/fcm/StudyBuddyMessagingService.kt
@@ -1,55 +1,53 @@
 package com.edricchan.studybuddy.providers.fcm
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.crashlytics.android.Crashlytics
 import com.edricchan.studybuddy.R
-import com.edricchan.studybuddy.extensions.isNotNull
+import com.edricchan.studybuddy.constants.Constants
+import com.edricchan.studybuddy.extensions.buildIntent
 import com.edricchan.studybuddy.interfaces.NotificationAction
 import com.edricchan.studybuddy.ui.modules.main.MainActivity
 import com.edricchan.studybuddy.ui.modules.settings.SettingsActivity
-import com.edricchan.studybuddy.constants.Constants
 import com.edricchan.studybuddy.utils.SharedUtils
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
-import java.util.*
 
 class StudyBuddyMessagingService : FirebaseMessagingService() {
-	private val sharedHelper = SharedUtils(this)
-	// To be used for Android's Log
+	private val sharedHelper = SharedUtils()
 	private val TAG = SharedUtils.getTag(this::class.java)
 
 	override fun onMessageReceived(remoteMessage: RemoteMessage?) {
 		super.onMessageReceived(remoteMessage)
+
 		if (remoteMessage != null) {
-			val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+			val manager = NotificationManagerCompat.from(this)
 			val builder = NotificationCompat.Builder(this, getString(R.string.notification_channel_uncategorised_id))
-			if (remoteMessage.notification.isNotNull()) {
-				// Check if remote message has a title
-				if (remoteMessage.notification?.title.isNotNull()) {
+
+			if (remoteMessage.notification != null) {
+				if (remoteMessage.notification?.title != null) {
 					builder.setContentTitle(remoteMessage.notification?.title)
-					builder.setStyle(NotificationCompat.BigTextStyle().bigText(remoteMessage.notification!!.body))
+					builder.setStyle(NotificationCompat.BigTextStyle().bigText(remoteMessage.notification?.body))
 				}
-				// Check if the remote message has a message body
-				if (remoteMessage.notification?.body.isNotNull()) {
+
+				if (remoteMessage.notification?.body != null) {
 					builder.setContentText(remoteMessage.notification?.body)
 				}
-				// Check if icon property exists
-				if (remoteMessage.notification?.icon.isNotNull()) {
-					val icon = resources.getIdentifier(remoteMessage.notification!!.icon, "drawable", packageName)
-					if (icon != 0) {
-						// Use the icon
+
+				if (remoteMessage.notification?.icon != null) {
+					val icon = resources.getIdentifier(remoteMessage.notification?.icon, "drawable", packageName)
+					// getIdentifier returns Resources.ID_NULL (0) if not such identifier exists
+					if (icon != Resources.ID_NULL) {
 						builder.setSmallIcon(icon)
 					} else {
 						// Use the default icon
@@ -59,102 +57,90 @@ class StudyBuddyMessagingService : FirebaseMessagingService() {
 					// Use the default icon
 					builder.setSmallIcon(R.drawable.ic_notification_studybuddy_pencil_24dp)
 				}
-				// Check if color property exists
-				if (remoteMessage.notification?.color.isNotNull()) {
-					// Use the color
-					builder.color = Color.parseColor(remoteMessage.notification!!.color)
+
+				if (remoteMessage.notification?.color != null) {
+					builder.color = Color.parseColor(remoteMessage.notification?.color)
 				} else {
 					// Use the default color
 					builder.color = ContextCompat.getColor(this, R.color.colorPrimary)
 				}
+
 				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-					if (remoteMessage.notification!!.channelId.isNotNull()) {
-						// Checks if the specified notification channel ID exists
-						if (manager.getNotificationChannel(remoteMessage.notification!!.channelId!!) == null) {
-							// Create a notification channel
-							val notificationChannel = NotificationChannel(remoteMessage.notification!!.channelId!!, "Notification Channel", NotificationManager.IMPORTANCE_DEFAULT)
-							notificationChannel.description = "Auto-generated channel"
-							manager.createNotificationChannel(notificationChannel)
+					if (remoteMessage.notification?.channelId != null) {
+						if (manager.getNotificationChannel(remoteMessage.notification?.channelId!!) == null) {
+							Log.w(TAG, "No such notification channel ${remoteMessage.notification?.channelId} exists." +
+									"Assigning \"uncategorised\" channel.")
+							builder.setChannelId(getString(R.string.notification_channel_uncategorised_id))
+						} else {
+							builder.setChannelId(remoteMessage.notification?.channelId!!)
 						}
-						// Set the channel ID
-						builder.setChannelId(remoteMessage.notification!!.channelId!!)
 					}
 				}
 			}
+
 			if (remoteMessage.data != null) {
-				// Check if the data of the remote message has a notificationChannelId as well as if the device is Android Oreo and up
-				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-					/*
-					Check if notificationChannelID exists
-					Note that since the notification channel ID is already defined in the Builder above, there's no need for an else statement
-					 */
-					if (remoteMessage.data.containsKey("notificationChannelId") && remoteMessage.notification?.channelId.isNullOrEmpty()) {
-						// Checks if the specified notification channel ID exists
-						if (manager.getNotificationChannel(remoteMessage.data["notificationChannelId"]) == null) {
-							// Create a notification channel
-							val notificationChannel = NotificationChannel(remoteMessage.data["notificationChannelId"], "Notification Channel", NotificationManager.IMPORTANCE_DEFAULT)
-							notificationChannel.description = "Auto-generated channel"
-							manager.createNotificationChannel(notificationChannel)
-						}
-						// Set the channel ID
-						builder.setChannelId(remoteMessage.data["notificationChannelId"]!!)
-					}
-				}
-				// Check if the data of the remote message has a notificationActions
 				if (remoteMessage.data.containsKey("notificationActions")) {
-					Log.d(TAG, "notificationActions: " + remoteMessage.data["notificationActions"]!!)
-					var notificationActions: List<NotificationAction> = ArrayList()
+					Log.d(TAG, "notificationActions: ${remoteMessage.data["notificationActions"]}")
+					var notificationActions: List<NotificationAction> = listOf()
+
 					try {
 						val gson = Gson()
-						val remoteNotificationActions = gson.fromJson(remoteMessage.data["notificationActions"], Array<NotificationAction>::class.java)
-						notificationActions = Arrays.asList(*remoteNotificationActions)
-						Log.d(TAG, "Size: " + remoteNotificationActions.size)
-						Log.d(TAG, "JSON: " + remoteNotificationActions.toString())
-						Log.d(TAG, "NotificationAction#actionTitle: " + remoteNotificationActions[0].actionTitle)
+						// TODO: Use Kotson library (https://github.com/SalomonBrys/Kotson)
+						val remoteNotificationActions = gson.fromJson(remoteMessage.data["notificationActions"],
+								Array<NotificationAction>::class.java)
+						notificationActions = remoteNotificationActions.asList()
+						Log.d(TAG, "Size: ${remoteNotificationActions.size}")
+						Log.d(TAG, "JSON: $remoteNotificationActions")
+						Log.d(TAG, "NotificationAction#actionTitle: ${remoteNotificationActions[0].actionTitle}")
 					} catch (e: Exception) {
-						e.printStackTrace()
+						Log.e(TAG, "Could not parse notification actions:", e)
 						Crashlytics.logException(e)
 					}
 
 					for (notificationAction in notificationActions) {
+						// This property is set to 0 by default to indicate that no such icon exists
 						var icon = 0
-						// Initial intent
 						val intent: Intent
-						// Notification pending intent
 						var notificationPendingIntent: PendingIntent? = null
 						val drawableIcon = resources.getIdentifier(notificationAction.actionIcon, "drawable", packageName)
-						// If getIdentifier is returned with 0, this means that no such drawable could be found
-						if (drawableIcon != 0) {
+						// getIdentifier returns Resources.ID_NULL (0) if no such resource exists
+						if (drawableIcon != Resources.ID_NULL) {
 							icon = drawableIcon
 						}
+
 						when (notificationAction.actionType) {
+							// TODO: Don't hardcode action types
 							Constants.actionNotificationsSettingsIntent -> {
-								intent = Intent(this, SettingsActivity::class.java)
-								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+								intent = buildIntent<SettingsActivity>(this) {
+									flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+								}
 								notificationPendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
 							}
-							else -> Log.w(TAG, "Unknown action type \"" + notificationAction.actionType + "\"!")
+							else -> Log.w(TAG, "Unknown action type ${notificationAction.actionType} specified for $notificationAction.")
 						}
 						builder.addAction(NotificationCompat.Action(icon, notificationAction.actionTitle, notificationPendingIntent))
 					}
 				}
 			}
-			val mainActivityIntent = Intent(this, MainActivity::class.java)
-			mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+			val mainActivityIntent = buildIntent<MainActivity>(this) {
+				flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+			}
 			val mainPendingIntent = PendingIntent.getActivity(this, 0, mainActivityIntent, PendingIntent.FLAG_ONE_SHOT)
 			builder.setContentIntent(mainPendingIntent)
+
 			manager.notify(sharedHelper.getDynamicId(), builder.build())
 		}
 	}
 
 	override fun onNewToken(token: String?) {
-		Log.d(TAG, "Refreshed token: " + token!!)
+		Log.d(TAG, "Refreshed token: $token")
 
 		// Add token to Firebase Firestore in the user's document
 		val fs = FirebaseFirestore.getInstance()
 		val auth = FirebaseAuth.getInstance()
 		if (auth.currentUser != null) {
-			fs.document("users/" + auth.currentUser!!.uid)
+			fs.document("users/${auth.currentUser?.uid}")
 					.update("registrationToken", token)
 					.addOnCompleteListener { task ->
 						if (task.isSuccessful) {
