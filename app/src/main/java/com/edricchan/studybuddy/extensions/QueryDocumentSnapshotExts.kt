5da5e678380bd5263b43f2236fa45dc5d13001c7
Edric Chan <edric.chan.1997@gmail.com>
2019-06-13 00:44:11 +0800

    QueryDocumentSnapshotExts: Use `toObject` extension

--- app/src/main/java/com/edricchan/studybuddy/extensions/QueryDocumentSnapshotExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/QueryDocumentSnapshotExts.kt
@@ -2,13 +2,15 @@ package com.edricchan.studybuddy.extensions
 
 import com.edricchan.studybuddy.interfaces.HasId
 import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.ktx.toObject
 
 /**
  * Converts a document snapshot value to its object equivalent with the document's ID attached
- * @param T A data class to convert the document snapshot's value to. Note that the data class specified must implement the [HasId] interface.
+ * @param T A data class to convert the document snapshot's value to.
+ * Note that the data class specified must implement the [HasId] interface.
  */
 inline fun <reified T : HasId> QueryDocumentSnapshot.toObjectWithId(): T {
-	val model = this.toObject(T::class.java)
+	val model = this.toObject<T>()
 	model.id = this.id
 	return model
 }
