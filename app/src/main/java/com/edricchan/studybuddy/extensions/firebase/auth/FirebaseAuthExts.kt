7e6148d2a20f2066888694be237787ca8cf9c2fd
Edric Chan <edric.chan.1997@gmail.com>
2019-06-21 01:45:56 +0800

    FirebaseAuthExts: Initial extension functions for getting user document from `FirebaseUser`

---
+++ app/src/main/java/com/edricchan/studybuddy/extensions/firebase/auth/FirebaseAuthExts.kt
@@ -0,0 +1,11 @@
+package com.edricchan.studybuddy.extensions.firebase.auth
+
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+
+/**
+ * Retrieves the user's Firestore document
+ * @param fs An instance of [FirebaseFirestore] (TIP: Use [FirebaseFirestore.getInstance] to retrieve an instance)
+ * @return The user's Firestore document
+ */
+fun FirebaseUser.getUserDocument(fs: FirebaseFirestore) = fs.document("users/${this.uid}")
