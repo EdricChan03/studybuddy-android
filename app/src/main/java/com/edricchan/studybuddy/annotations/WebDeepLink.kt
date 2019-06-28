ccb02cbe4ccea1857fbb82884b7f41093993bf5e
Edric Chan <edric.chan.1997@gmail.com>
2019-06-29 00:29:25 +0800

    WebDeepLink: Add URL prefixes for new web.app domain

--- app/src/main/java/com/edricchan/studybuddy/annotations/WebDeepLink.kt
+++ app/src/main/java/com/edricchan/studybuddy/annotations/WebDeepLink.kt
@@ -2,6 +2,11 @@ package com.edricchan.studybuddy.annotations
 
 import com.airbnb.deeplinkdispatch.DeepLinkSpec
 
-@DeepLinkSpec(prefix = ["http://studybuddy-e5f46.firebaseapp.com", "https://studybuddy-e5f46.firebaseapp.com"])
+@DeepLinkSpec(prefix = [
+	"http://studybuddy-e5f46.firebaseapp.com",
+	"https://studybuddy-e5f46.firebaseapp.com",
+	"http://studybuddy-e5f46.web.app",
+	"https://studybuddy-e5f46.web.app"
+])
 @Target(AnnotationTarget.CLASS)
 annotation class WebDeepLink(val value: Array<String>)
