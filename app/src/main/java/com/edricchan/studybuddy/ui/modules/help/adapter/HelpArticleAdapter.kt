c64922045c2e3bcd4620a662c82cffaced03d8c7
Edric Chan <edric.chan.1997@gmail.com>
2019-07-08 01:18:46 +0800

    HelpArticleAdapter: Change naming of variables to align Kotlin coding conventions

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/help/adapter/HelpArticleAdapter.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/help/adapter/HelpArticleAdapter.kt
@@ -10,14 +10,14 @@ import com.edricchan.studybuddy.R
 import com.edricchan.studybuddy.interfaces.HelpArticle
 
 class HelpArticleAdapter(
-		private val mHelpArticles: List<HelpArticle>?
+		private val helpArticlesList: List<HelpArticle>?
 ) : RecyclerView.Adapter<HelpArticleAdapter.Holder>() {
-	private var mListener: OnItemClickListener? = null
+	private var listener: OnItemClickListener? = null
 
 	var onItemClickListener: OnItemClickListener?
-		get() = this.mListener
+		get() = this.listener
 		set(listener) {
-			this.mListener = listener
+			this.listener = listener
 		}
 
 	fun setOnItemClickListener(listener: (article: HelpArticle, position: Int) -> Unit) {
@@ -36,8 +36,8 @@ class HelpArticleAdapter(
 		return Holder(itemView)
 	}
 
-		val (articleDesc, _, articleTitle) = mHelpArticles!![position]
 	override fun onBindViewHolder(holder: Holder, position: Int) {
+		val (articleDesc, _, articleTitle) = helpArticlesList!![position]
 		val descTextView = holder.descTextView
 		val titleTextView = holder.titleTextView
 
@@ -50,7 +50,7 @@ class HelpArticleAdapter(
 	}
 
 	override fun getItemCount(): Int {
-		return mHelpArticles?.size ?: 0
+		return helpArticlesList?.size ?: 0
 		// Return 0 if the help articles don't exist
 	}
 
@@ -62,15 +62,15 @@ class HelpArticleAdapter(
 
 		init {
 			if (adapterPosition != RecyclerView.NO_POSITION) {
-				itemView.isEnabled = (!mHelpArticles!![adapterPosition].isDisabled!!)
-				itemView.visibility = if (mHelpArticles[adapterPosition].isHidden!!) View.GONE else View.VISIBLE
+				itemView.isEnabled = (!helpArticlesList!![adapterPosition].isDisabled!!)
+				itemView.visibility = if (helpArticlesList[adapterPosition].isHidden!!) View.GONE else View.VISIBLE
 			}
 			articleImageView = itemView.findViewById(R.id.articleImageView)
 			descTextView = itemView.findViewById(R.id.descTextView)
 			titleTextView = itemView.findViewById(R.id.titleTextView)
 			itemView.setOnClickListener {
-				if (adapterPosition != RecyclerView.NO_POSITION && mListener != null) {
-					mListener!!.onItemClick(mHelpArticles!![adapterPosition], adapterPosition)
+				if (adapterPosition != RecyclerView.NO_POSITION && listener != null) {
+					listener!!.onItemClick(helpArticlesList!![adapterPosition], adapterPosition)
 				}
 			}
 		}
