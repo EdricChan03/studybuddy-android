843339c1b7b5dd8db1a93f954dd29f3525165c06
Edric Chan <edric.chan.1997@gmail.com>
2019-07-08 18:24:16 +0800

    FirebaseFirestoreExts: Update KDoc for `QueryDocumentSnapshot.toObjectWithId`

--- app/src/main/java/com/edricchan/studybuddy/extensions/firebase/firestore/FirebaseFirestoreExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/firebase/firestore/FirebaseFirestoreExts.kt
@@ -5,9 +5,10 @@ import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.toObject
 
 /**
- * Converts a document snapshot value to its object equivalent with the document's ID attached
+ * Converts a query document snapshot value to its object equivalent with the document's ID attached
  * @param T A data class to convert the document snapshot's value to.
  * Note that the data class specified must implement the [HasId] interface.
+ * @see HasId
  */
 inline fun <reified T : HasId> QueryDocumentSnapshot.toObjectWithId(): T {
 	val model = this.toObject<T>()
