317c16c5f25090c947b27faa68a3f36a2098b446
Edric Chan <edric.chan.1997@gmail.com>
2019-08-22 00:06:57 +0800

    Add extension function for converting a `Timestamp` to a date format

---
+++ app/src/main/java/com/edricchan/studybuddy/extensions/firebase/TimestampExts.kt
@@ -0,0 +1,15 @@
+package com.edricchan.studybuddy.extensions.firebase
+
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Converts a [Timestamp] to the specified [format]
+ *
+ * See the [`SimpleDateFormat` docs](https://docs.oracle.com/javase/8/docs/api/java/text/SimpleDateFormat.html) for more info
+ * @param format A format that [SimpleDateFormat] supports
+ * @param locale The locale to be used to convert the date
+ */
+fun Timestamp.toDateFormat(format: String, locale: Locale = Locale.getDefault()): String = SimpleDateFormat(format, locale)
+		.format(this.toDate())
