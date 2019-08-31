8aafbf65bf3661c25da6023ee75f9fbd687a1884
Edric Chan <edric.chan.1997@gmail.com>
2019-08-31 17:14:35 +0800

    ModalBottomSheetGroup: Specify default checkable behaviour

--- app/src/main/java/com/edricchan/studybuddy/ui/widget/bottomsheet/interfaces/ModalBottomSheetGroup.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/widget/bottomsheet/interfaces/ModalBottomSheetGroup.kt
@@ -2,19 +2,22 @@ package com.edricchan.studybuddy.ui.widget.bottomsheet.interfaces
 
 import com.edricchan.studybuddy.ui.widget.bottomsheet.ModalBottomSheetAdapter
 import com.edricchan.studybuddy.ui.widget.bottomsheet.annotations.ModalBottomSheetCheckableBehavior
+import com.edricchan.studybuddy.ui.widget.bottomsheet.interfaces.ModalBottomSheetGroup.Companion.CHECKABLE_BEHAVIOR_NONE
 
 /**
  * Represents a group of [ModalBottomSheetItem]s
  * @property id An non-zero integer ID so that [ModalBottomSheetItem]s can use the ID
- * @property checkableBehavior The type of the checkable behavior for the group
- * @property onItemCheckedChangeListener A listener which is called when an item's checked state is toggled
+ * @property checkableBehavior The type of the checkable behavior for the group (Default:
+ * [CHECKABLE_BEHAVIOR_NONE])
+ * @property onItemCheckedChangeListener A listener which is called when an item's checked state is
+ * toggled
  * @property visible Whether the group is visible
  * @property enabled Whether the group is enabled
  * @property selected The selected items
  */
 class ModalBottomSheetGroup(
 		var id: Int,
-		@ModalBottomSheetCheckableBehavior var checkableBehavior: String,
+		@ModalBottomSheetCheckableBehavior var checkableBehavior: String = CHECKABLE_BEHAVIOR_NONE,
 		var onItemCheckedChangeListener: ModalBottomSheetAdapter.OnItemCheckedChangeListener? = null,
 		var visible: Boolean = true,
 		var enabled: Boolean = true,
