409c0c48d3b91d8ed71cd91c8ff7e92fc9cb99fb
Edric Chan <edric.chan.1997@gmail.com>
2019-08-31 17:08:39 +0800

    ChatIconAsset: Remove `p` from `1px`

--- app/src/main/java/com/edricchan/studybuddy/interfaces/chat/icon/ChatIconAsset.kt
+++ app/src/main/java/com/edricchan/studybuddy/interfaces/chat/icon/ChatIconAsset.kt
@@ -3,13 +3,13 @@ package com.edricchan.studybuddy.interfaces.chat.icon
 /**
  * Data class used to represent an icon asset in [ChatIcon].
  * @property fileType The file type of the asset
- * @property size The size of the asset (e.g. `1px`, `3px`, etc.)
+ * @property size The size of the asset (e.g. `1x`, `3x`, etc.)
  * @property src The src of the asset (ideally a Firebase Storage download URL)
  * @property storageLocation The Firebase storage location of the asset
  */
 data class ChatIconAsset(
 		val fileType: String? = null,
-		val size: String? = "1px",
+		val size: String? = "1x",
 		val src: String? = null,
 		val storageLocation: String? = null
 )
