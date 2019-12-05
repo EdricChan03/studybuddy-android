fa271569a9f3afbf01af17c707d3c07a0d39f043
Edric <edric.chan.1997@gmail.com>
2019-12-05 19:22:20 +0800

    NetworkUtils: Initial code
    Adds a new class for network-related functionality

---
+++ app/src/main/java/com/edricchan/studybuddy/utils/NetworkUtils.kt
@@ -0,0 +1,29 @@
+package com.edricchan.studybuddy.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import androidx.core.content.getSystemService
+
+/**
+ * Utility class for network-related functionality.
+ * @param context The context.
+ */
+class NetworkUtils(
+		private val context: Context
+) {
+	/**
+	 * Whether the device is online.
+	 */
+	fun isOnline(): Boolean {
+		val cm = context.getSystemService<ConnectivityManager>()
+		val netInfo = cm?.activeNetworkInfo
+		return netInfo != null && netInfo.isConnectedOrConnecting
+	}
+
+	companion object {
+		/**
+		 * Creates a new instance of the [NetworkUtils] class.
+		 */
+		fun getInstance(context: Context) = NetworkUtils(context)
+	}
+}
