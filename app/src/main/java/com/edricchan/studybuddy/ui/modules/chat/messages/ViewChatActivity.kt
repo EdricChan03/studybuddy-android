6e3d610779a4d43bcaefb8cea7893d017eefcdd5
Edric <edric.chan.1997@gmail.com>
2019-12-05 19:21:35 +0800

    Rename and move ViewChatMessagesActivity to dedicated subdirectory

---
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/chat/messages/ViewChatActivity.kt
@@ -0,0 +1,47 @@
+package com.edricchan.studybuddy.ui.modules.chat.messages
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.deeplinkdispatch.DeepLink
+import com.edricchan.studybuddy.R
+import com.edricchan.studybuddy.annotations.AppDeepLink
+import com.edricchan.studybuddy.annotations.WebDeepLink
+import com.edricchan.studybuddy.extensions.TAG
+
+@WebDeepLink(["/chat/{id}", "/chats/view/{id}"])
+@AppDeepLink(["/chat/{id}", "/chats/view/{id}"])
+class ViewChatActivity : AppCompatActivity(R.layout.activity_view_chat) {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+		if (intent.getBooleanExtra(DeepLink.IS_DEEP_LINK, false)) {
+			Log.d(TAG, "Chat ID (deep link): ${intent.extras?.getString("id")}")
+		} else if (intent != null && intent.extras != null && intent.extras?.containsKey(KEY_CHAT_ID) == true) {
+			Log.d(TAG, "Chat ID: ${intent.extras?.get(KEY_CHAT_ID)}")
+		}
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		menuInflater.inflate(R.menu.menu_view_chat, menu)
+		return true
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		return when (item.itemId) {
+			android.R.id.home -> {
+				onBackPressed()
+				true
+			}
+			else -> super.onOptionsItemSelected(item)
+		}
+	}
+
+	companion object {
+		const val KEY_CHAT_ID = "CHAT_ID"
+	}
+}
