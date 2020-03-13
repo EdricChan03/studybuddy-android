676dd9879569435916c0a152197336fedef81157
Edric <edric.chan.1997@gmail.com>
2020-03-14 03:04:02 +0800

    StartupReceiver: Remove redundant code for Shared Prefs

--- app/src/main/java/com/edricchan/studybuddy/receivers/StartupReceiver.kt
+++ app/src/main/java/com/edricchan/studybuddy/receivers/StartupReceiver.kt
@@ -3,16 +3,12 @@ package com.edricchan.studybuddy.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.util.Log
-import androidx.preference.PreferenceManagerFix
 import com.edricchan.studybuddy.extensions.TAG
 import com.edricchan.studybuddy.utils.SharedUtils
 
 class StartupReceiver : BroadcastReceiver() {
-    private lateinit var sharedPreferences: SharedPreferences
     override fun onReceive(context: Context, intent: Intent) {
-        sharedPreferences = PreferenceManagerFix.getDefaultSharedPreferences(context)
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             Log.d(TAG, "Device has booted. Setting up check for updates worker...")
             SharedUtils.enqueueCheckForUpdatesWorker(context)
