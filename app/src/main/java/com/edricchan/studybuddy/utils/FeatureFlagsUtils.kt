286f81fc9585dce1ec61b5452d00e0a460002da6
Edric <edric.chan.1997@gmail.com>
2020-03-14 15:25:03 +0800

    FeatureFlagUtils: Add support for message string resource for `resetFeatureFlags`

--- app/src/main/java/com/edricchan/studybuddy/utils/FeatureFlagsUtils.kt
+++ app/src/main/java/com/edricchan/studybuddy/utils/FeatureFlagsUtils.kt
@@ -3,6 +3,7 @@ package com.edricchan.studybuddy.utils
 import android.content.Context
 import android.content.SharedPreferences
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.core.content.edit
 import com.edricchan.studybuddy.constants.sharedprefs.FeatureFlagsPrefConstants
 import com.edricchan.studybuddy.extensions.showToast
@@ -28,14 +29,25 @@ class FeatureFlagsUtils(
         featureFlagsPrefs.getBoolean(featureFlag, false)
 
     /**
+     * Resets the feature flags with an optional [messageRes] argument specifying the message to be
+     * shown when the feature flags have been reset.
+     * @param messageRes A string resource ID to the message to be shown on reset.
+     */
+    fun resetFeatureFlags(@StringRes messageRes: Int) {
+        resetFeatureFlags(context.getString(messageRes))
+    }
+
+    /**
      * Resets the feature flags. Optionally, the [message] parameter can be specified to override
      * the default message which would be used for the toast.
      * @param message The message to be shown as a [Toast] when the feature flags have been reset.
      */
-    fun resetFeatureFlags(message: String = "Successfully reset feature flags!") {
+    fun resetFeatureFlags(message: String? = null) {
         featureFlagsPrefs.edit {
             clear()
-            context.showToast(message, Toast.LENGTH_LONG)
+            if (message != null) {
+                context.showToast(message, Toast.LENGTH_LONG)
+            }
         }
     }
 }
