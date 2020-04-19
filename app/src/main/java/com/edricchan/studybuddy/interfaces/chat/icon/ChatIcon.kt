4d3de707411f6ac265d39bba7003a7d96ce768ed
Edric <edric.chan.1997@gmail.com>
2020-04-20 07:48:29 +0800

    ChatIcon: Mark `id` property as document ID

--- app/src/main/java/com/edricchan/studybuddy/interfaces/chat/icon/ChatIcon.kt
+++ app/src/main/java/com/edricchan/studybuddy/interfaces/chat/icon/ChatIcon.kt
@@ -1,12 +1,14 @@
 package com.edricchan.studybuddy.interfaces.chat.icon
 
+import com.edricchan.studybuddy.interfaces.HasId
 import com.edricchan.studybuddy.interfaces.HasTimestampMetadata
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentId
 
 /**
  * Data class used to represent a chat icon.
  * @property assets The list of icon assets (See [ChatIconAsset] for more info)
- * @property id The ID of the chat icon
+ * @property id The chat icon's document ID
  * @property name The name of the chat icon (if the chat icon is a default)
  * @property description A description of the chat icon
  * @property subjectType The type of subject that the chat icon represents
@@ -15,8 +17,8 @@ import com.google.firebase.Timestamp
  * @property userSubmitted Whether the chat icon was submitted by a user
  */
 data class ChatIcon(
+    @DocumentId override var id: String = "",
     val assets: List<ChatIconAsset>? = null,
-    val id: String? = null,
     val name: String? = null,
     val description: String? = null,
     @ChatIconSubjectTypeAnnotation val subjectType: String? = ChatIconSubjectType.OTHER,
@@ -25,4 +27,4 @@ data class ChatIcon(
     val userSubmitted: Boolean? = true,
     override val createdAt: Timestamp? = null,
     override val lastModified: Timestamp? = null
-) : HasTimestampMetadata
+) : HasId, HasTimestampMetadata
