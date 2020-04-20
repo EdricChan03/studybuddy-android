a525cc11f2f622a7bc82bcf68266d92218032e0c
Edric <edric.chan.1997@gmail.com>
2020-04-20 08:13:50 +0800

    StringExts: Improve KtDoc

--- app/src/main/java/com/edricchan/studybuddy/extensions/StringExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/StringExts.kt
@@ -4,12 +4,12 @@ import androidx.core.util.PatternsCompat
 
 /**
  * Checks if the specified [String] is a valid email address
- * @return [true] if it is a valid email address, [false] otherwise
+ * @return `true` if it is a valid email address, `false` otherwise
  */
 fun String.isValidEmail() = PatternsCompat.EMAIL_ADDRESS.matcher(this).matches()
 
 /**
  * Checks if the specified [String] is not a valid email address
- * @return [true] if it is an invalid email address, [false] otherwise
+ * @return `true` if it is an invalid email address, `false` otherwise
  */
 fun String.isInvalidEmail() = !isValidEmail()
