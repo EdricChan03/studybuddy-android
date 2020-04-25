94bf78f5892a5c473eb09660c5a2effdbdbdcd7d
Edric <edric.chan.1997@gmail.com>
2020-04-26 00:09:58 +0800

    FirebaseAuthExts: Add variable to retrieve user document without `fs` argument

--- app/src/main/java/com/edricchan/studybuddy/extensions/firebase/auth/FirebaseAuthExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/firebase/auth/FirebaseAuthExts.kt
@@ -1,11 +1,21 @@
 package com.edricchan.studybuddy.extensions.firebase.auth
 
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 /**
  * Retrieves the user's Firestore document
- * @param fs An instance of [FirebaseFirestore] (TIP: Use [FirebaseFirestore.getInstance] to retrieve an instance)
+ * @param fs An instance of [FirebaseFirestore]
  * @return The user's Firestore document
  */
-fun FirebaseUser?.getUserDocument(fs: FirebaseFirestore) = fs.document("users/${this?.uid}")
+fun FirebaseUser?.getUserDocument(fs: FirebaseFirestore) =
+    fs.document("users/${this?.uid}")
+
+/**
+ * Retrieves the user's Firestore document.
+ */
+val FirebaseUser?.userDocument: DocumentReference
+    get() = this.getUserDocument(Firebase.firestore)
