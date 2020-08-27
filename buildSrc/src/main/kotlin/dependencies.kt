b1f4e8308043693048d7965e1f852fbfde8b445c
Edric <edric.chan.1997@gmail.com>
2020-08-27 22:09:58 +0800

    Update dependencies
    After a long hiatus, I've decided to re-maintain this repository. :tada:

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -2,11 +2,12 @@
 
 import java.io.File
 
+@Suppress("HardCodedStringLiteral")
 object deps {
     object versions {
-        const val androidxTest = "1.3.0-beta01"
-        const val kotlin = "1.3.72"
-        const val markwon = "4.3.1"
+        const val androidxTest = "1.3.0"
+        const val kotlin = "1.4.0"
+        const val markwon = "4.5.1"
     }
 
     object build {
@@ -28,39 +29,39 @@ object deps {
 
     object android {
         object androidx {
-            const val appCompat = "androidx.appcompat:appcompat:1.2.0-beta01"
-            const val browser = "androidx.browser:browser:1.3.0-alpha01"
+            const val appCompat = "androidx.appcompat:appcompat:1.3.0-alpha02"
+            const val browser = "androidx.browser:browser:1.3.0-alpha05"
             // Only needed for com.github.daniel-stoneuk:material-about-library
             const val cardView = "androidx.cardview:cardview:1.0.0"
-            const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
-            const val coreKtx = "androidx.core:core-ktx:1.3.0-rc01"
-            const val emojiAppCompat = "androidx.emoji:emoji-appcompat:1.1.0-rc01"
-            const val materialComponents = "com.google.android.material:material:1.2.0-alpha06"
+            const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0"
+            const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha02"
+            const val emojiAppCompat = "androidx.emoji:emoji-appcompat:1.2.0-alpha01"
+            const val materialComponents = "com.google.android.material:material:1.3.0-alpha02"
             const val preferenceKtx = "androidx.preference:preference-ktx:1.1.1"
-            const val recyclerView = "androidx.recyclerview:recyclerview:1.2.0-alpha03"
+            const val recyclerView = "androidx.recyclerview:recyclerview:1.2.0-alpha05"
             const val recyclerViewSelection = "androidx.recyclerview:recyclerview-selection:1.1.0-rc01"
-            const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0-rc01"
-            const val workRuntimeKtx = "androidx.work:work-runtime-ktx:2.4.0-alpha03"
+            const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01"
+            const val workRuntimeKtx = "androidx.work:work-runtime-ktx:2.5.0-alpha01"
         }
 
-        const val gradlePlugin = "com.android.tools.build:gradle:4.1.0-alpha08"
+        const val gradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha07"
     }
 
     object deepLink {
-        const val deepLinkDispatch = "com.airbnb:deeplinkdispatch:5.1.0"
-        const val processor = "com.airbnb:deeplinkdispatch-processor:5.1.0"
+        const val deepLinkDispatch = "com.airbnb:deeplinkdispatch:5.2.0"
+        const val processor = "com.airbnb:deeplinkdispatch-processor:5.2.0"
     }
 
     object firebase {
-        const val analyticsKtx = "com.google.firebase:firebase-analytics-ktx:17.4.0"
-        const val authKtx = "com.google.firebase:firebase-auth-ktx:19.3.1"
+        const val analyticsKtx = "com.google.firebase:firebase-analytics-ktx:17.5.0"
+        const val authKtx = "com.google.firebase:firebase-auth-ktx:19.3.2"
         const val playServicesAuth = "com.google.android.gms:play-services-auth:18.0.0"
-        const val crashlytics = "com.google.firebase:firebase-crashlytics:17.0.0"
+        const val crashlytics = "com.google.firebase:firebase-crashlytics:17.2.1"
         const val crashlyticsPlugin = "com.google.firebase:firebase-crashlytics-gradle:2.0.0"
         const val dynamicLinksKtx = "com.google.firebase:firebase-dynamic-links-ktx:19.1.0"
-        const val firestoreKtx = "com.google.firebase:firebase-firestore-ktx:21.4.3"
-        const val messaging = "com.google.firebase:firebase-messaging:20.1.6"
-        const val perf = "com.google.firebase:firebase-perf:19.0.7"
+        const val firestoreKtx = "com.google.firebase:firebase-firestore-ktx:21.5.0"
+        const val messaging = "com.google.firebase:firebase-messaging:20.2.4"
+        const val perf = "com.google.firebase:firebase-perf:19.0.8"
         const val perfPlugin = "com.google.firebase:perf-plugin:1.3.1"
         const val guava = "com.google.guava:guava:29.0-android"
         const val gradlePlugin = "com.google.gms:google-services:4.3.3"
@@ -99,17 +100,17 @@ object deps {
     }
 
     object misc {
-        const val about = "com.github.daniel-stoneuk:material-about-library:2.4.2"
+        const val about = "com.github.daniel-stoneuk:material-about-library:3.2.0-rc01"
         const val appUpdater = "com.github.javiersantos:AppUpdater:2.7"
         const val gson = "com.google.code.gson:gson:2.8.6"
-        const val imagePicker = "com.github.esafirm.android-image-picker:imagepicker:2.1.0"
+        const val imagePicker = "com.github.esafirm.android-image-picker:imagepicker:2.3.0"
         const val streamSupport = "net.sourceforge.streamsupport:streamsupport:1.7.2"
         const val takisoftPreferencex = "com.takisoft.preferencex:preferencex:1.1.0"
     }
 
     object test {
         object android {
-            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.3.0"
             const val testCore = "androidx.test:core:${versions.androidxTest}"
             const val runner = "androidx.test:runner:${versions.androidxTest}"
             const val rules = "androidx.test:rules:${versions.androidxTest}"
