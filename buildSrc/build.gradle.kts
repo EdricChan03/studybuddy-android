c5a732da484e6622ee1e17a491e57102dbc0f82a
Edric <edric.chan.1997@gmail.com>
2020-08-30 01:45:24 +0800

    Add workaround for Kotlin DSL + AGP 4.2.0-alpha08 issues
    See https://issuetracker.google.com/issues/166468915 for more info

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,3 +5,8 @@ plugins {
 repositories {
     jcenter()
 }
+
+// Workaround for https://issuetracker.google.com/issues/166468915
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.0")
+}
