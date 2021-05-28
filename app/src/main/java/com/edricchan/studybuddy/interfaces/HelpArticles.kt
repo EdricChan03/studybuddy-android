18a11f7bc3bf36c41a47babc32dc7b27944bc730
Edric Chan <edric.chan.1997@gmail.com>
2021-05-29 00:29:06 +0800

    HelpArticles: Remove unused `Builder` class

--- app/src/main/java/com/edricchan/studybuddy/interfaces/HelpArticles.kt
+++ app/src/main/java/com/edricchan/studybuddy/interfaces/HelpArticles.kt
@@ -10,41 +10,4 @@ data class HelpArticles(
     val articles: List<HelpArticle>? = null,
     val version: Int? = 0,
     val `$schema`: String? = null
-) {
-    private constructor(builder: Builder) : this(
-        builder.articles,
-        builder.version,
-        builder.`$schema`
-    )
-
-    companion object {
-        /**
-         * Creates a [HelpArticles] using a [Builder] (with support for inlined setting of variables)
-         * @return The created [HelpArticles]
-         */
-        @Deprecated("This method is kept for compatibility reasons. Please do not use.")
-        inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
-    }
-
-    @Deprecated("This class is kept for compatibility reasons. Please do not use.")
-    class Builder {
-        /**
-         * The help articles' articles
-         */
-        var articles: MutableList<HelpArticle>? = mutableListOf()
-        /**
-         * The help articles' JSON schema
-         */
-        var `$schema`: String? = null
-        /**
-         * The help articles' version
-         */
-        var version: Int? = null
-
-        /**
-         * Returns the created [HelpArticles]
-         * @return The created [HelpArticles]
-         */
-        fun build() = HelpArticles(this)
-    }
-}
+)
