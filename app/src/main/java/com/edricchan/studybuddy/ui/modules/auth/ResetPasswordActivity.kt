b222ad71a937c5412fd72f338bac0cbab1a8cb99
Edric Chan <edric.chan.1997@gmail.com>
2021-09-09 01:45:45 +0800

    ResetPasswordActivity: Switch to View Binding

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/auth/ResetPasswordActivity.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/auth/ResetPasswordActivity.kt
@@ -1,18 +1,15 @@
 package com.edricchan.studybuddy.ui.modules.auth
 
 import android.os.Bundle
-import android.text.TextUtils
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import com.edricchan.studybuddy.R
 import com.edricchan.studybuddy.annotations.AppDeepLink
 import com.edricchan.studybuddy.annotations.WebDeepLink
+import com.edricchan.studybuddy.databinding.ActivityResetPasswordBinding
 import com.edricchan.studybuddy.extensions.TAG
+import com.edricchan.studybuddy.extensions.showSnackbar
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -20,65 +17,60 @@ import com.google.firebase.ktx.Firebase
 
 @WebDeepLink(["/resetPassword", "/reset-password"])
 @AppDeepLink(["/resetPassword", "/reset-password"])
-class ResetPasswordActivity : AppCompatActivity(R.layout.activity_reset_password) {
+class ResetPasswordActivity : AppCompatActivity() {
 
-    private lateinit var inputEmail: EditText
-    private lateinit var btnReset: Button
-    private lateinit var btnBack: Button
     private lateinit var auth: FirebaseAuth
-    private lateinit var progressBar: ProgressBar
+    private lateinit var binding: ActivityResetPasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        inputEmail = findViewById(R.id.email)
-        btnReset = findViewById(R.id.btn_reset_password)
-        btnBack = findViewById(R.id.btn_back)
-        progressBar = findViewById(R.id.progressBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
 
         auth = Firebase.auth
 
-        btnBack.setOnClickListener { finish() }
+        binding.apply {
+            btnBack.setOnClickListener { finish() }
 
-        btnReset.setOnClickListener {
+            btnResetPassword.setOnClickListener {
 
-            val email = inputEmail.text.toString().trim { it <= ' ' }
+                val email = inputEmail.text.toString().trim { it <= ' ' }
 
-            if (TextUtils.isEmpty(email)) {
-                Snackbar.make(
-                    findViewById(R.id.mainView),
-                    "Please enter an email address",
-                    Snackbar.LENGTH_LONG
-                ).show()
-                return@setOnClickListener
-            }
-
-            progressBar.visibility = View.VISIBLE
-            auth.sendPasswordResetEmail(email)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Snackbar.make(
-                            findViewById(R.id.mainView),
-                            "An email has been sent to the email address that you have specified",
-                            Snackbar.LENGTH_LONG
-                        )
-                            .show()
-                    } else {
-                        Snackbar.make(
-                            findViewById(R.id.mainView),
-                            "An error occurred while attempting to send an email. Please try again later",
-                            Snackbar.LENGTH_LONG
-                        )
-                            .show()
-                        Log.e(
-                            TAG,
-                            "An error occurred while attempting to send a reset password email.",
-                            task.exception
-                        )
-                    }
-
-                    progressBar.visibility = View.GONE
+                if (email.isEmpty()) {
+                    showSnackbar(
+                        mainView,
+                        "Please enter an email address",
+                        Snackbar.LENGTH_LONG
+                    )
+                    return@setOnClickListener
                 }
+
+                progressBar.visibility = View.VISIBLE
+                auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            showSnackbar(
+                                mainView,
+                                "An email has been sent to the email address that you have specified",
+                                Snackbar.LENGTH_LONG
+                            )
+                        } else {
+                            showSnackbar(
+                                mainView,
+                                "An error occurred while attempting to send an email. Please try again later",
+                                Snackbar.LENGTH_LONG
+                            )
+                            Log.e(
+                                TAG,
+                                "An error occurred while attempting to send a reset password email.",
+                                task.exception
+                            )
+                        }
+
+                        progressBar.visibility = View.GONE
+                    }
+            }
         }
     }
 
