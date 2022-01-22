EdricChan03/studybuddy-android

commit 921a8829120931cef283b868f1ae9caf92c7f0d3

Update dependencies
Other changes:
- Remove the unused streamsupport dependency (usage was removed in
5f8cd351a819f9fd8957823d1d4094d78f96c44d
  but the dependency itself was never removed)
- Remove the Guava dependency introduced in 8eb38b627b50d865bf74a34041aa065f0a4ecd9b

app/build.gradle.kts
@@ -261,7 +261,6 @@ dependencies {
     implementation(deps.firebase.installationsKtx)
     implementation(deps.firebase.messagingKtx)
     implementation(deps.firebase.perfKtx)
-    implementation(deps.firebase.guava)
 
     // Other dependencies
     implementation(deps.misc.about)

buildSrc/src/main/kotlin/dependencies.kt
@@ -4,8 +4,8 @@ import java.io.File
 
 object deps {
     object versions {
-        const val androidxTest = "1.4.0-alpha06"
-        const val kotlin = "1.5.10"
+        const val androidxTest = "1.4.1-alpha03"
+        const val kotlin = "1.6.10"
         const val markwon = "4.6.2"
     }
 
@@ -28,41 +28,42 @@ object deps {
 
     object android {
         object androidx {
-            const val appCompat = "androidx.appcompat:appcompat:1.4.0-alpha02"
-            const val browser = "androidx.browser:browser:1.3.0"
-            const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.0-beta02"
-            const val coreKtx = "androidx.core:core-ktx:1.6.0-beta02"
-            const val materialComponents = "com.google.android.material:material:1.4.0-rc01"
-            const val preferenceKtx = "androidx.preference:preference-ktx:1.1.1"
-            const val recyclerView = "androidx.recyclerview:recyclerview:1.2.1"
-            const val recyclerViewSelection = "androidx.recyclerview:recyclerview-selection:1.2.0-alpha01"
-            const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01"
-            const val workRuntimeKtx = "androidx.work:work-runtime-ktx:2.6.0-beta01"
+            const val appCompat = "androidx.appcompat:appcompat:1.4.0"
+            const val browser = "androidx.browser:browser:1.4.0"
+            const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.2"
+            const val coreKtx = "androidx.core:core-ktx:1.8.0-alpha02"
+            const val materialComponents = "com.google.android.material:material:1.6.0-alpha02"
+            const val preferenceKtx = "androidx.preference:preference-ktx:1.2.0-rc01"
+            const val recyclerView = "androidx.recyclerview:recyclerview:1.3.0-alpha01"
+            const val recyclerViewSelection =
+                "androidx.recyclerview:recyclerview-selection:1.2.0-alpha01"
+            const val swipeRefreshLayout =
+                "androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01"
+            const val workRuntimeKtx = "androidx.work:work-runtime-ktx:2.8.0-alpha01"
         }
 
-        const val gradlePlugin = "com.android.tools.build:gradle:7.2.0-alpha03"
+        const val gradlePlugin = "com.android.tools.build:gradle:7.2.0-alpha07"
     }
 
     object deepLink {
-        const val deepLinkDispatch = "com.airbnb:deeplinkdispatch:5.4.3"
-        const val processor = "com.airbnb:deeplinkdispatch-processor:5.4.3"
+        const val deepLinkDispatch = "com.airbnb:deeplinkdispatch:6.0.0-beta06"
+        const val processor = "com.airbnb:deeplinkdispatch-processor:6.0.0-beta06"
     }
 
     object firebase {
-        const val bom = "com.google.firebase:firebase-bom:28.1.0"
+        const val bom = "com.google.firebase:firebase-bom:29.0.4"
         const val analyticsKtx = "com.google.firebase:firebase-analytics-ktx"
         const val authKtx = "com.google.firebase:firebase-auth-ktx"
-        const val playServicesAuth = "com.google.android.gms:play-services-auth:19.0.0"
+        const val playServicesAuth = "com.google.android.gms:play-services-auth:20.0.1"
         const val crashlyticsKtx = "com.google.firebase:firebase-crashlytics-ktx"
-        const val crashlyticsPlugin = "com.google.firebase:firebase-crashlytics-gradle:2.5.2"
+        const val crashlyticsPlugin = "com.google.firebase:firebase-crashlytics-gradle:2.8.1"
         const val dynamicLinksKtx = "com.google.firebase:firebase-dynamic-links-ktx"
         const val firestoreKtx = "com.google.firebase:firebase-firestore-ktx"
         const val installationsKtx = "com.google.firebase:firebase-installations-ktx"
         const val messagingKtx = "com.google.firebase:firebase-messaging-ktx"
         const val perfKtx = "com.google.firebase:firebase-perf-ktx"
-        const val perfPlugin = "com.google.firebase:perf-plugin:1.4.0"
-        const val guava = "com.google.guava:guava:29.0-android"
-        const val gradlePlugin = "com.google.gms:google-services:4.3.8"
+        const val perfPlugin = "com.google.firebase:perf-plugin:1.4.1"
+        const val gradlePlugin = "com.google.gms:google-services:4.3.10"
     }
 
     object glide {
@@ -91,6 +92,7 @@ object deps {
             const val tables = "$ext_package_prefix-tables:$markwon_version"
             const val tasklist = "$ext_package_prefix-tasklist:$markwon_version"
         }
+
         const val html = "$package_prefix:html:$markwon_version"
         const val imageGlide = "$package_prefix:image-glide:$markwon_version"
         const val linkify = "$package_prefix:linkify:$markwon_version"
@@ -102,18 +104,17 @@ object deps {
         const val appUpdater = "com.github.javiersantos:AppUpdater:2.7"
         const val gson = "com.google.code.gson:gson:2.8.6"
         const val imagePicker = "com.github.esafirm.android-image-picker:imagepicker:2.4.0"
-        const val streamSupport = "net.sourceforge.streamsupport:streamsupport:1.7.3"
         const val takisoftPreferencex = "com.takisoft.preferencex:preferencex:1.1.0"
     }
 
     object test {
         object android {
-            const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0-alpha04"
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.5.0-alpha03"
             const val testCore = "androidx.test:core:${versions.androidxTest}"
             const val runner = "androidx.test:runner:${versions.androidxTest}"
             const val rules = "androidx.test:rules:${versions.androidxTest}"
         }
         const val junit = "junit:junit:4.13.2"
-        const val mockitoCore = "org.mockito:mockito-core:3.10.0"
+        const val mockitoCore = "org.mockito:mockito-core:4.2.0"
     }
 }

commit bf9db70ba3a42ef48d24135ab3c5c2708023b7fd

Fix build errors introduced in 921a8829120931cef283b868f1ae9caf92c7f0d3
- Update compileSdk to 31
- Update usage of the image picker library to use the new APIs

app/build.gradle.kts
@@ -127,7 +127,7 @@ fun generateVersionName(): String {
 }
 
 android {
-    compileSdk = 30
+    compileSdk = 31
     defaultConfig {
         applicationId = "com.edricchan.studybuddy"
         minSdk = 21

app/src/main/java/com/edricchan/studybuddy/ui/modules/chat/NewChatActivity.kt
@@ -22,7 +22,9 @@ import com.edricchan.studybuddy.interfaces.Visibility
 import com.edricchan.studybuddy.interfaces.chat.Chat
 import com.edricchan.studybuddy.ui.modules.auth.LoginActivity
 import com.edricchan.studybuddy.ui.widget.iconpicker.IconPickerActivity
-import com.esafirm.imagepicker.features.ImagePicker
+import com.esafirm.imagepicker.features.ImagePickerConfig
+import com.esafirm.imagepicker.features.ImagePickerMode
+import com.esafirm.imagepicker.features.registerImagePicker
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -40,6 +42,11 @@ class NewChatActivity : AppCompatActivity(R.layout.activity_new_chat) {
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
     private val ICON_PICKER_RESULT = 100
+
+    private val imagePickerLauncher = registerImagePicker {
+        // TODO: Add proper image picker support
+        Log.d(TAG, "Selected images: $it")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -84,11 +91,11 @@ class NewChatActivity : AppCompatActivity(R.layout.activity_new_chat) {
                         }
                         1 -> {
                             // Upload
-                            ImagePicker.create(this@NewChatActivity)
-                                .folderMode(true)
-                                .single()
-                                .theme(R.style.Theme_App_ImagePicker)
-                                .start()
+                            imagePickerLauncher.launch(ImagePickerConfig {
+                                theme = R.style.Theme_App_ImagePicker
+                                mode = ImagePickerMode.SINGLE
+                                isFolderMode = true
+                            })
                         }
                         2 -> {
                             // Default
@@ -195,11 +202,7 @@ class NewChatActivity : AppCompatActivity(R.layout.activity_new_chat) {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
-            // TODO: Add actual functionality
-            val images = ImagePicker.getImages(data)
-            Log.d(TAG, "Selected images: ${images.joinToString()}")
-        } else if (requestCode == ICON_PICKER_RESULT) {
+        if (requestCode == ICON_PICKER_RESULT) {
             Log.d(TAG, "Icon picker returned!")
         }
         super.onActivityResult(requestCode, resultCode, data)