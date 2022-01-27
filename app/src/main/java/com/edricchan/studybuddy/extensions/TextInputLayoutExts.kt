35a9b353436c3d055c299f1e5a394aa4394a27f9
Edric Chan <edric.chan.1997@gmail.com>
2022-01-28 00:30:16 +0800

    TextInputLayoutExts: Remove SpannableStringBuilder usage
    Switches the code to use the `setText` method directly instead
    
    Also fixes an issue where the `EditText` class wasn't resolved in the
    KDoc

--- app/src/main/java/com/edricchan/studybuddy/extensions/TextInputLayoutExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/TextInputLayoutExts.kt
@@ -1,11 +1,10 @@
 package com.edricchan.studybuddy.extensions
 
 import android.text.Editable
-import android.text.SpannableStringBuilder
 import com.google.android.material.textfield.TextInputLayout
 
 /**
- * Getter/setter for the [TextInputLayout]'s [EditText] value
+ * Getter/setter for the [TextInputLayout]'s [android.widget.EditText] value
  * Returns/requires an [Editable]
  */
 var TextInputLayout.editTextValue: Editable?
@@ -15,11 +14,11 @@ var TextInputLayout.editTextValue: Editable?
     }
 
 /**
- * Getter/setter for the [TextInputLayout]'s [EdiText] value
+ * Getter/setter for the [TextInputLayout]'s [android.widget.EditText] value
  * Returns/requires a [String]
  */
 var TextInputLayout.editTextStrValue: String
     get() = this.editText?.text.toString()
     set(value) {
-        this.editText?.text = SpannableStringBuilder(value)
+        this.editText?.setText(value)
     }
