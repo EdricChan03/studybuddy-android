45cb9d0c2cbd12f395452103879b7946e295f1af
Edric Chan <edric.chan.1997@gmail.com>
2022-02-01 23:38:36 +0800

    Switch to Gradle 7's Version Catalogs
    For more info, see https://docs.gradle.org/current/userguide/platforms.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ buildscript {
         google()
     }
     dependencies {
-        classpath(deps.android.gradlePlugin)
-        classpath(deps.kotlin.gradlePlugin)
-        classpath(deps.firebase.gradlePlugin)
-        classpath(deps.firebase.crashlyticsPlugin)
-        classpath(deps.firebase.perfPlugin)
-        classpath(deps.licenses.gradlePlugin)
+        classpath(libs.android.gradle)
+        classpath(libs.kotlin.gradle)
+        classpath(libs.googleServices.gradle)
+        classpath(libs.firebase.crashlytics.gradle)
+        classpath(libs.firebase.perf.gradle)
+        classpath(libs.playServices.ossLicenses.gradle)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
