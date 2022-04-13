0c3e91f24bad0886ed318f4014900ec589fd77a4
Edric Chan <edric.chan.1997@gmail.com>
2022-04-14 00:10:28 +0800

    WebUtils: Fix deprecated usages

--- app/src/main/java/com/edricchan/studybuddy/utils/WebUtils.kt
+++ app/src/main/java/com/edricchan/studybuddy/utils/WebUtils.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
@@ -63,9 +64,14 @@ class WebUtils(val context: Context) {
                     }
                     setColorScheme(colorScheme)
                 }
-                setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary))
-                setSecondaryToolbarColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
-                setNavigationBarColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                setDefaultColorSchemeParams(CustomTabColorSchemeParams.Builder().apply {
+                    setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                    setSecondaryToolbarColor(
+                        ContextCompat.getColor(context, R.color.colorPrimaryDark)
+                    )
+                    setNavigationBarColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                }.build())
+
                 setShareState(CustomTabsIntent.SHARE_STATE_ON)
                 setShowTitle(true)
             }.build()
