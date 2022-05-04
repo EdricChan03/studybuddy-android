665fd08c3298484517f2ec45c920e8743b2ea88c
Edric Chan <edric.chan.1997@gmail.com>
2022-05-05 02:24:11 +0800

    Add Markwon plugin extensions

---
+++ app/src/main/java/com/edricchan/studybuddy/extensions/markwon/MarkwonPluginExts.kt
@@ -0,0 +1,31 @@
+package com.edricchan.studybuddy.extensions.markwon
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
+import coil.ImageLoader
+import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
+import io.noties.markwon.ext.tasklist.TaskListPlugin
+import io.noties.markwon.image.coil.CoilImagesPlugin
+import io.noties.markwon.linkify.LinkifyPlugin
+
+val Context.coilImagesPlugin get() = CoilImagesPlugin.create(this)
+
+fun Context.coilImagesPlugin(imageLoader: ImageLoader) =
+    CoilImagesPlugin.create(this, imageLoader)
+
+val linkifyPlugin = LinkifyPlugin.create()
+
+fun linkifyPlugin(useCompat: Boolean) = LinkifyPlugin.create(useCompat)
+
+val Context.taskListPlugin get() = TaskListPlugin.create(this)
+
+fun Context.taskListPlugin(drawable: Drawable) = TaskListPlugin.create(drawable)
+
+fun Context.taskListPlugin(
+    @ColorInt checkedFillColor: Int,
+    @ColorInt normalOutlineColor: Int,
+    @ColorInt checkMarkColor: Int
+) = TaskListPlugin.create(checkedFillColor, normalOutlineColor, checkMarkColor)
+
+val strikethroughPlugin = StrikethroughPlugin.create()
