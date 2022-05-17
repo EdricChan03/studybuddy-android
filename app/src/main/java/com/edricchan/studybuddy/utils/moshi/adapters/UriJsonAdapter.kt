487c728890db455537c47caf18d0c6e0e3b2f076
Edric Chan <edric.chan.1997@gmail.com>
2022-05-18 04:57:31 +0800

    UriJsonAdapter: Code improvements
    * Add `Suppress` annotation for unused inner methods
    * Add KDoc
    * Use `toUri` extension function

--- app/src/main/java/com/edricchan/studybuddy/utils/moshi/adapters/UriJsonAdapter.kt
+++ app/src/main/java/com/edricchan/studybuddy/utils/moshi/adapters/UriJsonAdapter.kt
@@ -1,11 +1,15 @@
+@file:Suppress("unused")
+
 package com.edricchan.studybuddy.utils.moshi.adapters
 
 import android.net.Uri
+import androidx.core.net.toUri
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 
+/** Moshi adapter to convert a [Uri] to and from its JSON representation. */
 object UriJsonAdapter {
     @ToJson fun toJson(uri: Uri) = uri.toString()
 
-    @FromJson fun fromJson(uri: String) = Uri.parse(uri)
+    @FromJson fun fromJson(uri: String) = uri.toUri()
 }
