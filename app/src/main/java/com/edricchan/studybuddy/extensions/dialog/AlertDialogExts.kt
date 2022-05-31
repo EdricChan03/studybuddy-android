96610a68688e347256ce03e273e5dd8e91a3f957
Edric Chan <edric.chan.1997@gmail.com>
2022-05-31 23:36:26 +0800

    Add extension functions to create/show a Material Design alert dialog

---
+++ app/src/main/java/com/edricchan/studybuddy/extensions/dialog/AlertDialogExts.kt
@@ -0,0 +1,46 @@
+package com.edricchan.studybuddy.extensions.dialog
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+/**
+ * Creates an instance of [MaterialAlertDialogBuilder] using the specified [configuration][init].
+ *
+ * To create an alert dialog as a [androidx.appcompat.app.AlertDialog], use [createMaterialAlertDialog].
+ * To show the dialog, use [showMaterialAlertDialog] instead.
+ * @param init Configuration to be passed to [MaterialAlertDialogBuilder].
+ * @return The created [MaterialAlertDialogBuilder].
+ * @see MaterialAlertDialogBuilder
+ */
+fun Context.materialAlertDialogBuilder(init: MaterialAlertDialogBuilder.() -> Unit) =
+    MaterialAlertDialogBuilder(this).apply(init)
+
+/**
+ * Creates a Material Design alert dialog using the specified [configuration][init].
+ *
+ * To show the dialog, use [showMaterialAlertDialog] instead.
+ * @param init Configuration to be passed to [MaterialAlertDialogBuilder].
+ * @return The created [AlertDialog].
+ * @see MaterialAlertDialogBuilder
+ */
+fun Context.createMaterialAlertDialog(init: MaterialAlertDialogBuilder.() -> Unit) =
+    materialAlertDialogBuilder(init).create()
+
+/**
+ * Creates and shows a Material Design alert dialog using the specified
+ * [dialog builder configuration][builderInit], and [dialog configuration][dialogInit], if any.
+ * Optionally, additional [dialog configuration][postDialogInit] can be set after the dialog is
+ * shown.
+ * @param builderInit Configuration to be passed to [MaterialAlertDialogBuilder].
+ * @param dialogInit Configuration to be passed to [AlertDialog] before it is shown.
+ * @param postDialogInit Configuration to be passed to [AlertDialog] after it is shown, if any.
+ * @return The shown [AlertDialog].
+ * @see MaterialAlertDialogBuilder
+ */
+fun Context.showMaterialAlertDialog(
+    builderInit: MaterialAlertDialogBuilder.() -> Unit,
+    dialogInit: AlertDialog.() -> Unit = {},
+    postDialogInit: AlertDialog.() -> Unit = {}
+) =
+    createMaterialAlertDialog(builderInit).apply(dialogInit).apply { show() }.apply(postDialogInit)
