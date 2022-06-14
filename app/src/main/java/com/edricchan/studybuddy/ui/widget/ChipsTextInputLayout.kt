fe15c45eca89efc5832dae7d1db7c23fa070076d
Edric Chan <edric.chan.1997@gmail.com>
2022-06-15 04:32:14 +0800

    ChipsTextInputLayout: Code improvements
    * Use androidx.core extension functions where applicable
    * Use single constructor
    * Unexpose the inner `TextInputEditText`
    * Mark exposed views as non-nullable
    * Use View Binding

--- app/src/main/java/com/edricchan/studybuddy/ui/widget/ChipsTextInputLayout.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/widget/ChipsTextInputLayout.kt
@@ -1,86 +1,75 @@
 package com.edricchan.studybuddy.ui.widget
 
 import android.content.Context
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.widget.LinearLayout
-import androidx.core.content.getSystemService
+import androidx.core.content.withStyledAttributes
+import androidx.core.view.children
+import androidx.core.view.minusAssign
+import androidx.core.view.plusAssign
+import androidx.core.widget.doAfterTextChanged
 import com.edricchan.studybuddy.R
+import com.edricchan.studybuddy.databinding.ViewChipsTextInputLayoutBinding
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
-import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
-class ChipsTextInputLayout : LinearLayout {
+class ChipsTextInputLayout @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+    private var binding: ViewChipsTextInputLayoutBinding
+
     /**
      * The internal [TextInputLayout]
      */
-    var textInputLayout: TextInputLayout?
-    /**
-     * The internal [TextInputEditText]
-     */
-    var textInputEditText: TextInputEditText?
+    var textInputLayout: TextInputLayout
+        private set
+
     /**
      * The internal [ChipGroup]
      */
-    var chipGroup: ChipGroup?
+    var chipGroup: ChipGroup
+        private set
 
-    constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        val styledAttrs =
-            context.obtainStyledAttributes(attrs, R.styleable.ChipsTextInputLayout, 0, 0)
-        val textInputLayoutHint =
-            styledAttrs.getString(R.styleable.ChipsTextInputLayout_textInputLayoutHint)
-                ?: context.getString(R.string.chips_text_input_layout_default_hint)
-        val textInputLayoutHelperText =
-            styledAttrs.getString(R.styleable.ChipsTextInputLayout_textInputLayoutHelperText)
+    init {
+        binding =
+            ViewChipsTextInputLayoutBinding.inflate(LayoutInflater.from(context), this, true).also {
+                textInputLayout = it.textInputLayout
+                chipGroup = it.chipGroup
+            }
 
-        val inflater = context.getSystemService<LayoutInflater>()
-        val view = inflater?.inflate(R.layout.view_chips_text_input_layout, this, true)
-
-        textInputLayout = view?.findViewById(R.id.textInputLayout)
-        textInputEditText = view?.findViewById(R.id.textInputEditText)
-        chipGroup = view?.findViewById(R.id.chipGroup)
-
-        textInputLayout?.apply {
-            hint = textInputLayoutHint
-            helperText = textInputLayoutHelperText
+        context.withStyledAttributes(attrs, R.styleable.ChipsTextInputLayout) {
+            textInputLayout.apply {
+                hint = getString(R.styleable.ChipsTextInputLayout_textInputLayoutHint)
+                    ?: context.getString(R.string.chips_text_input_layout_default_hint)
+                helperText = getString(R.styleable.ChipsTextInputLayout_textInputLayoutHelperText)
+            }
         }
 
-        textInputEditText?.addTextChangedListener(object : TextWatcher {
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun afterTextChanged(editable: Editable?) {
-                val trimmed = editable.toString().trim { it <= ' ' }
+        textInputLayout.editText?.apply {
+            doAfterTextChanged { editable ->
+                val trimmed = editable?.toString().orEmpty().trim()
                 if (trimmed.length > 1 && trimmed.endsWith(",")) {
-                    val chip = Chip(context)
-                    chip.text = trimmed.substring(0, trimmed.length - 1)
-                    chip.isCloseIconVisible = true
-
-                    //Callback fired when chip close icon is clicked
-                    chip.setOnCloseIconClickListener {
-                        chipGroup?.removeView(chip)
+                    chipGroup += Chip(context).apply {
+                        text = trimmed.substring(0, trimmed.length - 1)
+                        setOnCloseIconClickListener {
+                            chipGroup -= this
+                        }
                     }
-
-                    chipGroup?.addView(chip)
                     editable?.clear()
                 }
             }
-        })
-        textInputEditText?.setOnKeyListener { _, _, event ->
-            if (event != null && event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_DEL) {
-                if (textInputEditText?.length() == 0 && chipGroup?.childCount!! > 0) {
-                    val chip =
-                        chipGroup?.childCount?.minus(1)?.let { chipGroup?.getChildAt(it) } as Chip
-                    chipGroup?.removeView(chip)
+
+            setOnKeyListener { _, _, event ->
+                if (event != null && event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_DEL) {
+                    if (text.isNullOrEmpty() && chipGroup.childCount > 0) {
+                        chipGroup -= chipGroup.children.last() as Chip
+                    }
                 }
+                false
             }
-            false
         }
     }
 }
