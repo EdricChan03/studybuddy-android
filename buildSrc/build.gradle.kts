e211189b66594068519f18f89ed58d7ed9a20c9e
Edric Chan <edric.chan.1997@gmail.com>
2022-06-17 23:30:15 +0800

    MarkdownReporter: Opt into `OptIn` annotation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
 }
@@ -10,4 +12,12 @@ repositories {
 dependencies {
     // Used for the custom reporter
     implementation(libs.gradleVersions.gradle)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        freeCompilerArgs += listOf(
+            "-opt-in=kotlin.RequiresOptIn"
+        )
+    }
 }
