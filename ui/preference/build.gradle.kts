a41831c7c11fdb408a28923d9e0f5a07af1555ec
Edric Chan <edric.chan.1997@gmail.com>
2022-06-18 21:23:33 +0800

    ui/preference: Remove `buildTypes` config for now

--- ui/preference/build.gradle.kts
+++ ui/preference/build.gradle.kts
@@ -16,16 +16,6 @@ android {
         consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
     buildFeatures {
         viewBinding = true
     }
