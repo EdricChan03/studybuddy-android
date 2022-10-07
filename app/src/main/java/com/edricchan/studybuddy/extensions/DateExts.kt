719ad1aefdfaa9dd37dbc4d4a66991694f7f1bc1
Edric <edric.chan.1997@gmail.com>
2022-10-07 18:38:00 +0800

    [2/2] Refactor to java.time APIs where applicable
    * Remove extension functions for the old
      `java.util.Date`/`java.util.SimpleDateFormat` classes
    * Add extension functions to format a Firestore `Timestamp` using a
      `DateTimeFormatter`
    * Update usages

--- app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
@@ -1,29 +1,9 @@
 package com.edricchan.studybuddy.extensions
 
-import com.google.firebase.Timestamp
-import java.text.SimpleDateFormat
 import java.time.Duration
 import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalAccessor
 import java.time.temporal.TemporalUnit
-import java.util.*
-
-/**
- * Converts a [Date] to its [Timestamp] equivalent
- * @return A [Timestamp]
- */
-fun Date.toTimestamp() = Timestamp(this)
-
-/**
- * Converts a date to the specified [format]
- *
- * See the [`SimpleDateFormat` docs](https://docs.oracle.com/javase/8/docs/api/java/text/SimpleDateFormat.html) for more info
- * @param format A format that [SimpleDateFormat] supports
- * @param locale The locale to be used to convert the date
- */
-fun Date.toDateFormat(format: String, locale: Locale = Locale.getDefault()): String =
-    SimpleDateFormat(format, locale)
-        .format(this)
 
 /** Converts the [Long] to a [Duration] with the specified [unit]. */
 fun Long.toDuration(unit: TemporalUnit) = Duration.of(this, unit)
