54c98f5d8119063aaf6cb308342e3e78368b376f
Edric <edric.chan.1997@gmail.com>
2022-10-07 22:25:42 +0800

    DateExts: Add explicit return type to `Long.toInstant()`

--- app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
@@ -6,7 +6,7 @@ import java.time.temporal.TemporalAccessor
 import java.time.temporal.TemporalUnit
 
 /** Converts the [Long] to a [Duration] with the specified [unit]. */
-fun Long.toDuration(unit: TemporalUnit) = Duration.of(this, unit)
+fun Long.toDuration(unit: TemporalUnit): Duration = Duration.of(this, unit)
 
 /** Formats the temporal object using the given [formatter]. */
 fun TemporalAccessor.format(formatter: DateTimeFormatter): String = formatter.format(this)
