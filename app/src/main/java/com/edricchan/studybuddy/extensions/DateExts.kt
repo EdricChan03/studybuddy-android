0cce424684cd5da93f34572d61eb6b1ecbf1afd2
Edric <edric.chan.1997@gmail.com>
2022-10-13 23:46:36 +0800

    DateExts: Add extension function to convert `Instant`s to `LocalDateTime`

--- app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/DateExts.kt
@@ -1,12 +1,19 @@
 package com.edricchan.studybuddy.extensions
 
 import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalAccessor
 import java.time.temporal.TemporalUnit
 
 /** Converts the [Long] to a [Duration] with the specified [unit]. */
 fun Long.toDuration(unit: TemporalUnit): Duration = Duration.of(this, unit)
+
+/** Converts the [Instant] to a [LocalDateTime] given the [zoneId]. */
+fun Instant.toLocalDateTime(zoneId: ZoneId = ZoneId.systemDefault()): LocalDateTime =
+    LocalDateTime.ofInstant(this, zoneId)
 
 /** Formats the temporal object using the given [formatter]. */
 fun TemporalAccessor.format(formatter: DateTimeFormatter): String = formatter.format(this)
