08122cdda9cb81cf3f80283e53af3916808492cd
Edric <edric.chan.1997@gmail.com>
2022-10-17 19:03:42 +0800

    Add explicit JavaPoet dep

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,6 +14,10 @@ dependencies {
     // Used for the custom reporter
     implementation(libs.gradleVersions.gradle)
     implementation(libs.android.gradle.library)
+    // See https://github.com/google/dagger/issues/3068
+    implementation("com.squareup:javapoet:1.13.0") {
+        because("AGP DataBinding uses an older version of Javapoet which breaks Hilt")
+    }
     implementation(libs.kotlin.gradle)
 }
 
