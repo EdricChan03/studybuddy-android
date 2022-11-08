04f4b110b6251149b47bb142b1566eadefbeb937
Edric <edric.chan.1997@gmail.com>
2022-11-08 18:16:21 +0800

    StringExts: Use Kotlin `Regex` for email check

--- app/src/main/java/com/edricchan/studybuddy/extensions/StringExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/StringExts.kt
@@ -2,11 +2,13 @@ package com.edricchan.studybuddy.extensions
 
 import androidx.core.util.PatternsCompat
 
+private val emailAddressRegex = PatternsCompat.EMAIL_ADDRESS.toRegex()
+
 /**
  * Checks if the specified [String] is a valid email address
  * @return `true` if it is a valid email address, `false` otherwise
  */
-fun String.isValidEmail() = PatternsCompat.EMAIL_ADDRESS.matcher(this).matches()
+fun String.isValidEmail() = emailAddressRegex matches this
 
 /**
  * Checks if the specified [String] is not a valid email address
