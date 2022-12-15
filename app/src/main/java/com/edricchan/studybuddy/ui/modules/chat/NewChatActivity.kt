a378f3b2c4852e582057df340c2a311ca4e8d1cb
Edric <edric.chan.1997@gmail.com>
2022-12-15 17:45:56 +0800

    NewChatActivity: Code clean-up
    - Switch to View Binding
    - Use extension funs where applicable
    - Remove redundant `isErrorEnabled` calls
    - Other minor improvements

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/chat/NewChatActivity.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/chat/NewChatActivity.kt
@@ -7,14 +7,16 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import com.edricchan.studybuddy.R
 import com.edricchan.studybuddy.core.deeplink.AppDeepLink
 import com.edricchan.studybuddy.core.deeplink.WebDeepLink
+import com.edricchan.studybuddy.databinding.ActivityNewChatBinding
 import com.edricchan.studybuddy.extensions.TAG
+import com.edricchan.studybuddy.extensions.dialog.showMaterialAlertDialog
 import com.edricchan.studybuddy.extensions.editTextStrValue
 import com.edricchan.studybuddy.extensions.firebase.auth.getUserDocument
+import com.edricchan.studybuddy.extensions.showSnackbar
 import com.edricchan.studybuddy.extensions.startActivity
 import com.edricchan.studybuddy.interfaces.Visibility
 import com.edricchan.studybuddy.interfaces.chat.Chat
@@ -24,10 +26,7 @@ import com.edricchan.studybuddy.ui.widget.iconpicker.IconPickerActivity
 import com.esafirm.imagepicker.features.ImagePickerConfig
 import com.esafirm.imagepicker.features.ImagePickerMode
 import com.esafirm.imagepicker.features.registerImagePicker
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -37,11 +36,14 @@ import java.util.Locale
 
 @WebDeepLink(["/chats/new"])
 @AppDeepLink(["/chats/new"])
-class NewChatActivity : BaseActivity(R.layout.activity_new_chat) {
-
+class NewChatActivity : BaseActivity() {
+    private lateinit var binding: ActivityNewChatBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
-    private val ICON_PICKER_RESULT = 100
+
+    companion object {
+        private val ICON_PICKER_RESULT = 100
+    }
 
     private val imagePickerLauncher = registerImagePicker {
         // TODO: Add proper image picker support
@@ -51,6 +53,9 @@ class NewChatActivity : BaseActivity(R.layout.activity_new_chat) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding = ActivityNewChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         auth = Firebase.auth
         firestore = Firebase.firestore
@@ -62,58 +67,57 @@ class NewChatActivity : BaseActivity(R.layout.activity_new_chat) {
         }
 
         @SuppressLint("DefaultLocale")
-        val visibilityValues =
-            listOf(Visibility.PUBLIC, Visibility.UNLISTED, Visibility.PRIVATE).map {
-                it.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+        val visibilityValues = Visibility.values.map { value ->
+            value.replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
             }
+        }
 
-        findViewById<AutoCompleteTextView>(R.id.chatVisibilityAutoCompleteTextView).apply {
-            setAdapter(
+        binding.apply {
+            chatVisibilityAutoCompleteTextView.setAdapter(
                 ArrayAdapter(
-                    context,
+                    this@NewChatActivity,
                     R.layout.dropdown_menu_popup_item,
                     visibilityValues
                 )
             )
-        }
 
-        val chatIconImageButton = findViewById<FloatingActionButton>(R.id.imageButtonChatIconPicker)
-        chatIconImageButton.setOnClickListener {
-            // TODO: Show a dialog with options to choose from library or upload
-            MaterialAlertDialogBuilder(this@NewChatActivity).apply {
-                setTitle(R.string.new_chat_activity_select_chat_icon_dialog_title)
-                setItems(R.array.new_chat_activity_select_chat_icon_dialog_items) { dialog, i ->
-                    when (i) {
-                        0 -> {
-                            // Choose from library
-                            // TODO: Add activity for this functionality
-                            startActivityForResult(
-                                Intent(
-                                    this@NewChatActivity,
-                                    IconPickerActivity::class.java
-                                ), ICON_PICKER_RESULT
-                            )
-                        }
+            imageButtonChatIconPicker.setOnClickListener {
+                // TODO: Show a dialog with options to choose from library or upload
+                showMaterialAlertDialog {
+                    setTitle(R.string.new_chat_activity_select_chat_icon_dialog_title)
+                    setItems(R.array.new_chat_activity_select_chat_icon_dialog_items) { dialog, i ->
+                        when (i) {
+                            0 -> {
+                                // Choose from library
+                                // TODO: Add activity for this functionality
+                                startActivityForResult(
+                                    Intent(
+                                        this@NewChatActivity,
+                                        IconPickerActivity::class.java
+                                    ), ICON_PICKER_RESULT
+                                )
+                            }
 
-                        1 -> {
-                            // Upload
-                            imagePickerLauncher.launch(ImagePickerConfig {
-                                theme = R.style.Theme_App_ImagePicker
-                                mode = ImagePickerMode.SINGLE
-                                isFolderMode = true
-                            })
-                        }
+                            1 -> {
+                                // Upload
+                                imagePickerLauncher.launch(ImagePickerConfig {
+                                    theme = R.style.Theme_App_ImagePicker
+                                    mode = ImagePickerMode.SINGLE
+                                    isFolderMode = true
+                                })
+                            }
 
-                        2 -> {
-                            // Default
-                            // TODO: Add functionality
+                            2 -> {
+                                // Default
+                                // TODO: Add functionality
+                            }
                         }
                     }
+                    setNegativeButton(R.string.dialog_action_cancel, null)
                 }
-                setNegativeButton(R.string.dialog_action_cancel, null)
-            }.show()
+            }
         }
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -130,10 +134,10 @@ class NewChatActivity : BaseActivity(R.layout.activity_new_chat) {
 
             R.id.action_submit -> {
                 // NOTE: TIL means TextInputLayout here
-                val chatNameTIL = findViewById<TextInputLayout>(R.id.chatNameTextInputLayout)
-                val chatDescTIL = findViewById<TextInputLayout>(R.id.chatDescTextInputLayout)
+                val chatNameTIL = binding.chatNameTextInputLayout
+                val chatDescTIL = binding.chatDescTextInputLayout
                 val chatVisibilityAutoCompleteTextView =
-                    findViewById<AutoCompleteTextView>(R.id.chatVisibilityAutoCompleteTextView)
+                    binding.chatVisibilityAutoCompleteTextView
 
                 val chatName = chatNameTIL.editTextStrValue
                 val chatDescription = chatDescTIL.editTextStrValue
@@ -144,44 +148,32 @@ class NewChatActivity : BaseActivity(R.layout.activity_new_chat) {
                         .replaceFirstChar { it.lowercase(Locale.getDefault()) }
 
                 if (chatName.isEmpty() || chatName.length > 100 || chatDescription.length > 300) {
-                    if ((chatName.isEmpty() || chatName.length > 100) && !chatNameTIL.isErrorEnabled) chatNameTIL.isErrorEnabled =
-                        true
                     if (chatName.isEmpty()) {
                         chatNameTIL.error = "Please specify a chat name!"
                     } else if (chatName.length > 100) {
                         chatNameTIL.error = "Only 100 characters are allowed."
                     }
 
-                    if ((chatDescription.length > 300) && !chatDescTIL.isErrorEnabled) chatDescTIL.isErrorEnabled =
-                        true
                     if (chatDescription.length > 300) {
                         chatDescTIL.error = "Only 300 characters are allowed."
                     }
 
-                    Snackbar.make(
-                        findViewById(R.id.mainView),
+                    showSnackbar(
+                        binding.mainView,
                         "Some errors occurred while attempting to submit the form.",
                         Snackbar.LENGTH_SHORT
-                    ).show()
+                    )
                 } else {
-                    if (chatNameTIL.isErrorEnabled) {
-                        // Disable the error
-                        chatNameTIL.isErrorEnabled = false
-                        chatNameTIL.error = null
-                    }
-                    if (chatDescTIL.isErrorEnabled) {
-                        // Disable the error
-                        chatDescTIL.isErrorEnabled = false
-                        chatDescTIL.error = null
-                    }
+                    chatNameTIL.error = null
+                    chatDescTIL.error = null
 
                     val chat = Chat.build {
                         name = chatName
                         description = chatDescription
                         visibility = chatVisibility
                         owner = auth.currentUser?.getUserDocument(firestore)
-                        admins = mutableListOf(auth.currentUser?.getUserDocument(firestore))
-                        members = mutableListOf(auth.currentUser?.getUserDocument(firestore))
+                        admins?.add(auth.currentUser?.getUserDocument(firestore))
+                        members?.add(auth.currentUser?.getUserDocument(firestore))
                     }
 
                     firestore.collection("chats").add(chat).addOnCompleteListener { task ->
