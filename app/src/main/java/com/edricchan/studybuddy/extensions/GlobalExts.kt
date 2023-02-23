c4ed719766cb3ab632552c246446e5151dabe765
Edric <edric.chan.1997@gmail.com>
2023-02-23 22:04:39 +0800

    GlobalExts: Remove unused extension functions

--- app/src/main/java/com/edricchan/studybuddy/extensions/GlobalExts.kt
+++ app/src/main/java/com/edricchan/studybuddy/extensions/GlobalExts.kt
@@ -3,18 +3,6 @@ package com.edricchan.studybuddy.extensions
 // This file contains the definitions for all global extensions
 
 /**
- * Checks if a value is `null`
- * @return `true` if the value is `null`, `false` otherwise
- */
-fun Any?.isNull() = this == null
-
-/**
- * Checks if a value is not `null`
- * @return `true` if the value is not `null`, `false` otherwise
- */
-fun Any?.isNotNull() = this != null
-
-/**
  * The TAG constant which should ideally be used with one of the [android.util.Log] methods.
  *
  * See https://stackoverflow.com/a/53510106/6782707 for more info
