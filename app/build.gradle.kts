b9f0be088c781d39ab5c25b9156171d5b1437d14
Edric <edric.chan.1997@gmail.com>
2023-02-28 01:50:53 +0800

    app: Bump Java source + target compatibility to Java 17
    Whoops

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,8 +111,8 @@ android {
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
 
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     testOptions {
