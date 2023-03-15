cd79b8f81a616ed6bd439594ba8b6c520c507e74
Edric <edric.chan.1997@gmail.com>
2023-03-16 02:50:27 +0800

    Initial precompiled script for Compose libs

---
+++ buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/android-compose.gradle.kts
@@ -0,0 +1,11 @@
+/** Convention plugin for Android libraries made with Jetpack Compose. */
+package com.edricchan.studybuddy.library
+
+plugins {
+    id("com.edricchan.studybuddy.library.android")
+}
+
+android {
+    buildFeatures.compose = true
+    composeOptions.kotlinCompilerExtensionVersion = "1.4.3"
+}
