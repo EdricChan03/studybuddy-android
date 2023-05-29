963af436f2a91950edd798da5c8df8ad1925662f
Edric <edric.chan.1997@gmail.com>
2023-05-30 01:06:24 +0800

    Theme: Use property getter instead for dynamic colour check

--- ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/theme/Theme.kt
+++ ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/theme/Theme.kt
@@ -4,5 +4,5 @@ import android.os.Build
 import androidx.annotation.ChecksSdkIntAtLeast
 
 /** Whether dynamic colour theming is supported. */
-@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
-val supportsDynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+@get:ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+val supportsDynamicColor get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
