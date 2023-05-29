c8e76f534b35e23995297014860574f6362cf108
Edric <edric.chan.1997@gmail.com>
2023-05-30 01:33:33 +0800

    StudyBuddyCompatTheme: Mark as deprecated
    The `StudyBuddyTheme` composable should be used instead

--- ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/StudyBuddyCompatTheme.kt
+++ ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/StudyBuddyCompatTheme.kt
@@ -3,6 +3,15 @@ package com.edricchan.studybuddy.ui.theming.compose
 import androidx.compose.runtime.Composable
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
 
+@Deprecated(
+    "This composable is used for backwards-compatibility with the existing theming " +
+        "system. Once the theming system has been migrated to Compose, use the " +
+        "StudyBuddyTheme composable instead",
+    ReplaceWith(
+        "StudyBuddyTheme(content)",
+        "com.edricchan.studybuddy.ui.theming.compose.StudyBuddyTheme"
+    )
+)
 @Composable
 fun StudyBuddyCompatTheme(content: @Composable () -> Unit) =
     Mdc3Theme(content = content, setDefaultFontFamily = true)
