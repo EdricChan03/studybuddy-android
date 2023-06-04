74cca519492c7720d43734866bff5f94a009cc33
Edric <edric.chan.1997@gmail.com>
2023-06-04 14:30:57 +0800

    TipsFragment: Code clean-up
    * Migrate to `MenuProvider` API (#379)
    * Add missing `onCreateView` implementation

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/tips/fragment/TipsFragment.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/tips/fragment/TipsFragment.kt
@@ -2,10 +2,13 @@ package com.edricchan.studybuddy.ui.modules.tips.fragment
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.edricchan.studybuddy.BuildConfig
 import com.edricchan.studybuddy.R
@@ -22,35 +25,20 @@ class TipsFragment : Fragment() {
     private var _binding: FragTipsBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-        preferences = requireContext().defaultSharedPreferences
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.tipsEmptyStateCta.setOnClickListener {
-            requireContext().launchUri(Constants.uriSubmitTip)
+    private val menuProvider = object : MenuProvider {
+        override fun onPrepareMenu(menu: Menu) {
+            if (!BuildConfig.DEBUG) {
+                menu.removeItem(R.id.action_debug)
+            }
         }
-    }
 
-
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        if (!BuildConfig.DEBUG) {
-            menu.removeItem(R.id.action_debug)
+        override fun onCreateMenu(menu: Menu, inflater: MenuInflater) {
+            // Clear the main activity's menu before inflating the fragment's menu
+            menu.clear()
+            inflater.inflate(R.menu.menu_frag_tips, menu)
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        // Clear the main activity's menu before inflating the fragment's menu
-        menu.clear()
-        inflater.inflate(R.menu.menu_frag_tips, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
+        override fun onMenuItemSelected(item: MenuItem) = when (item.itemId) {
             R.id.action_debug -> {
                 startActivity<DebugActivity>()
                 true
@@ -61,7 +49,28 @@ class TipsFragment : Fragment() {
                 true
             }
 
-            else -> super.onOptionsItemSelected(item)
+            else -> false
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        preferences = requireContext().defaultSharedPreferences
+        activity?.addMenuProvider(menuProvider)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragTipsBinding
+        .inflate(inflater, container, false).also { _binding = it }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tipsEmptyStateCta.setOnClickListener {
+            requireContext().launchUri(Constants.uriSubmitTip)
         }
     }
 
