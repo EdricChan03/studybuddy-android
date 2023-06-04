77bd2096dedde5a077b0b6e6dfa17cea40c09edf
Edric <edric.chan.1997@gmail.com>
2023-06-04 11:26:04 +0800

    PermissionUtils: Move methods to extension functions

--- app/src/main/java/com/edricchan/studybuddy/utils/PermissionUtils.kt
+++ app/src/main/java/com/edricchan/studybuddy/utils/PermissionUtils.kt
@@ -8,24 +8,51 @@ import androidx.core.content.ContextCompat
  * Utility class for permission-related functions.
  * @property context The context to be used.
  */
+@Deprecated("Use the appropriate extension functions instead")
 class PermissionUtils(
-    val context: Context
+    private val context: Context
 ) {
     /**
      * Checks if the specified [permission] is granted.
      * @param permission The permission to check.
      * @return `true` if the specified permission is granted, `false` otherwise
      */
-    fun checkPermissionGranted(permission: String) = ContextCompat.checkSelfPermission(
-        context,
-        permission
-    ) == PackageManager.PERMISSION_GRANTED
+    @Deprecated(
+        "Use the extension function instead",
+        ReplaceWith(
+            "context.checkPermissionGranted(permission)",
+            "com.edricchan.studybuddy.utils.checkPermissionGranted"
+        )
+    )
+    fun checkPermissionGranted(permission: String) = context.checkPermissionGranted(permission)
 
     companion object {
         /**
          * Creates a new instance of the [PermissionUtils] class with the specified arguments.
          * @param context The context to be used.
          */
+        @Suppress("DeprecatedCallableAddReplaceWith")
+        @Deprecated("Use the appropriate extension functions instead")
         fun getInstance(context: Context) = PermissionUtils(context)
     }
 }
+
+/**
+ * Checks if the specified [permission] is granted.
+ * @param permission The permission to check.
+ * @return `true` if the specified permission is granted, `false` otherwise
+ */
+fun Context.checkPermissionGranted(permission: String) = ContextCompat.checkSelfPermission(
+    this,
+    permission
+) == PackageManager.PERMISSION_GRANTED
+
+/**
+ * Checks if the specified [permission] is denied.
+ * @param permission The permission to check.
+ * @return `true` if the specified permission is denied, `false` otherwise
+ */
+fun Context.checkPermissionDenied(permission: String) = ContextCompat.checkSelfPermission(
+    this,
+    permission
+) == PackageManager.PERMISSION_DENIED
