ddfb427ab49d66c1594da3c6948670b47e76ec25
Edric <edric.chan.1997@gmail.com>
2023-06-22 15:43:08 +0800

    kotlin.gradle.kts: Set JVM toolchain via Kotlin plugin instead
    Migrate to type-safe plugin accessors as well

--- buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/kotlin.gradle.kts
@@ -1,20 +1,11 @@
 /** Convention plugin for pure-Kotlin libraries. */
 package com.edricchan.studybuddy.library
 
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
 plugins {
     `java-library`
-    kotlin("jvm")
+    org.jetbrains.kotlin.jvm
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-tasks.withType<KotlinJvmCompile>().configureEach {
-    compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+kotlin {
+    jvmToolchain(17)
 }
