ef6bb52780c2b1d41a342ec9cd80173a86904c63
Edric <edric.chan.1997@gmail.com>
2023-07-02 22:05:28 +0800

    Bump AGP to `8.2.0-alpha10`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ plugins {
     // https://docs.gradle.org/8.0-rc-2/userguide/toolchains.html#sub:download_repositories
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 
-    id("com.android.settings") version "8.2.0-alpha09"
+    id("com.android.settings") version "8.2.0-alpha10"
 }
 
 // Configure the Gradle Enterprise Plugin
