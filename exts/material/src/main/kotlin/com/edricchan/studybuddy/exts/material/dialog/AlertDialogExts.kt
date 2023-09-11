fe39867b817b31c44ad67bc7f596a4fe6133aab8
Edric <edric.chan.1997@gmail.com>
2023-09-11 23:42:58 +0800

    AlertDialogExts: Add theme resource support

--- exts/material/src/main/kotlin/com/edricchan/studybuddy/exts/material/dialog/AlertDialogExts.kt
+++ exts/material/src/main/kotlin/com/edricchan/studybuddy/exts/material/dialog/AlertDialogExts.kt
@@ -1,37 +1,47 @@
 package com.edricchan.studybuddy.exts.material.dialog
 
 import android.content.Context
+import androidx.annotation.StyleRes
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 /**
  * Creates an instance of [MaterialAlertDialogBuilder] using the specified [configuration][init].
  *
- * To create an alert dialog as a [androidx.appcompat.app.AlertDialog], use [createMaterialAlertDialog].
+ * To create an alert dialog as a [androidx.appcompat.app.AlertDialog], use
+ * [createMaterialAlertDialog].
  * To show the dialog, use [showMaterialAlertDialog] instead.
+ * @param themeResId The theme resource to use, if any.
  * @param init Configuration to be passed to [MaterialAlertDialogBuilder].
  * @return The created [MaterialAlertDialogBuilder].
  * @see MaterialAlertDialogBuilder
  */
-fun Context.materialAlertDialogBuilder(init: MaterialAlertDialogBuilder.() -> Unit) =
-    MaterialAlertDialogBuilder(this).apply(init)
+fun Context.materialAlertDialogBuilder(
+    @StyleRes themeResId: Int = 0,
+    init: MaterialAlertDialogBuilder.() -> Unit
+) = MaterialAlertDialogBuilder(this, themeResId).apply(init)
 
 /**
  * Creates a Material Design alert dialog using the specified [configuration][init].
  *
  * To show the dialog, use [showMaterialAlertDialog] instead.
+ * @param themeResId The theme resource to use, if any.
  * @param init Configuration to be passed to [MaterialAlertDialogBuilder].
  * @return The created [AlertDialog].
  * @see MaterialAlertDialogBuilder
  */
-fun Context.createMaterialAlertDialog(init: MaterialAlertDialogBuilder.() -> Unit) =
-    materialAlertDialogBuilder(init).create()
+fun Context.createMaterialAlertDialog(
+    @StyleRes themeResId: Int = 0,
+    init: MaterialAlertDialogBuilder.() -> Unit
+) =
+    materialAlertDialogBuilder(themeResId, init).create()
 
 /**
  * Creates and shows a Material Design alert dialog using the specified
  * [dialog builder configuration][builderInit], and [dialog configuration][dialogInit], if any.
  * Optionally, additional [dialog configuration][postDialogInit] can be set after the dialog is
  * shown.
+ * @param themeResId The theme resource to use, if any.
  * @param dialogInit Configuration to be passed to [AlertDialog] before it is shown.
  * @param postDialogInit Configuration to be passed to [AlertDialog] after it is shown, if any.
  * @param builderInit Configuration to be passed to [MaterialAlertDialogBuilder].
@@ -39,8 +49,11 @@ fun Context.createMaterialAlertDialog(init: MaterialAlertDialogBuilder.() -> Uni
  * @see MaterialAlertDialogBuilder
  */
 fun Context.showMaterialAlertDialog(
+    @StyleRes themeResId: Int = 0,
     dialogInit: AlertDialog.() -> Unit = {},
     postDialogInit: AlertDialog.() -> Unit = {},
     builderInit: MaterialAlertDialogBuilder.() -> Unit
-) =
-    createMaterialAlertDialog(builderInit).apply(dialogInit).apply { show() }.apply(postDialogInit)
+) = createMaterialAlertDialog(themeResId, builderInit)
+    .apply(dialogInit)
+    .apply { show() }
+    .apply(postDialogInit)
