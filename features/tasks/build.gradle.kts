e571722375d558748e5dd0edda7bee51f516b521
Edric <edric.chan.1997@gmail.com>
2023-09-22 01:17:04 +0800

    :features:tasks: Add missing deps

--- features/tasks/build.gradle.kts
+++ features/tasks/build.gradle.kts
@@ -18,10 +18,23 @@ android {
             )
         }
     }
+
+    compileOptions.isCoreLibraryDesugaringEnabled = true
 }
 
 
 dependencies {
+    implementation(projects.exts.androidx.preferenceFiles)
+    implementation(projects.exts.common)
+
+    // Kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Migration
+    implementation(libs.migration)
+
+    coreLibraryDesugaring(libs.android.desugar.nio)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext)
     androidTestImplementation(libs.androidx.test.espresso.core)
