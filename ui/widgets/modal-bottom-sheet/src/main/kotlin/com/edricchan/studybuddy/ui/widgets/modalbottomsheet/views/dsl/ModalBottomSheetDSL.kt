1471123b42310b9e1a1b2badc3271b32cf3e8cef
Edric <edric.chan.1997@gmail.com>
2023-10-07 17:18:39 +0800

    ModalBottomSheetDSL: Rename DSLs to `Builder`s
    Something went wrong with the local cache for whatever reason when using
    `DSL` as a suffix, so rename the classes to `Builder`s instead (it also
    sounds more clear so)

--- ui/widgets/modal-bottom-sheet/src/main/kotlin/com/edricchan/studybuddy/ui/widgets/modalbottomsheet/views/dsl/ModalBottomSheetDSL.kt
+++ ui/widgets/modal-bottom-sheet/src/main/kotlin/com/edricchan/studybuddy/ui/widgets/modalbottomsheet/views/dsl/ModalBottomSheetDSL.kt
@@ -12,20 +12,20 @@ import com.edricchan.studybuddy.ui.widgets.modalbottomsheet.views.interfaces.Mod
 annotation class ModalBottomSheetDsl
 
 @ModalBottomSheetDsl
-class ModalBottomSheetDSL {
+class ModalBottomSheetBuilder {
     private var items = mutableListOf<ModalBottomSheetItem>()
 
-    private var groupItems = mutableListOf<ModalBottomSheetGroupItemsDSL>()
+    private var groupItems = mutableListOf<ModalBottomSheetGroupItemsBuilder>()
 
     /** Adds a new item to the bottom sheet and returns the created item. */
     fun item(item: ModalBottomSheetItem) = item.also { items += it }
 
     /** Adds a new item to the bottom sheet and returns the created item. */
-    private fun item(init: ModalBottomSheetItemDSL.() -> Unit) =
-        ModalBottomSheetItemDSL().apply(init).build().also { items += it }
+    private fun item(init: ModalBottomSheetItemBuilder.() -> Unit) =
+        ModalBottomSheetItemBuilder().apply(init).build().also { items += it }
 
     /** Adds a new item to the bottom sheet and returns the created item. */
-    fun item(title: String, init: ModalBottomSheetItemDSL.() -> Unit = {}) = item {
+    fun item(title: String, init: ModalBottomSheetItemBuilder.() -> Unit = {}) = item {
         this.title = title
         init()
     }
@@ -36,13 +36,13 @@ class ModalBottomSheetDSL {
     }
 
     /** Adds a new item to the bottom sheet using the `+=` operator. */
-    operator fun plusAssign(init: ModalBottomSheetItemDSL.() -> Unit) {
+    operator fun plusAssign(init: ModalBottomSheetItemBuilder.() -> Unit) {
         item(init)
     }
 
     /** Adds the specified list of items to the bottom sheet and returns the created items. */
-    fun items(vararg init: ModalBottomSheetItemDSL.() -> Unit) =
-        init.map { ModalBottomSheetItemDSL().apply(it).build() }
+    fun items(vararg init: ModalBottomSheetItemBuilder.() -> Unit) =
+        init.map { ModalBottomSheetItemBuilder().apply(it).build() }
             .also { this.items = it.toMutableList() }
 
     /** Adds the specified list of items to the bottom sheet. */
@@ -58,35 +58,35 @@ class ModalBottomSheetDSL {
 
     /** Creates a new group. */
     fun group(group: ModalBottomSheetGroup) =
-        ModalBottomSheetGroupItemsDSL(group).also { groupItems += it }
+        ModalBottomSheetGroupItemsBuilder(group).also { groupItems += it }
 
     /** Creates a new group with the specified list of items and returns the created items. */
     fun group(
         group: ModalBottomSheetGroup,
-        groupItemsInit: ModalBottomSheetGroupItemsDSL.() -> Unit
+        groupItemsInit: ModalBottomSheetGroupItemsBuilder.() -> Unit
     ) =
-        ModalBottomSheetGroupItemsDSL(group)
+        ModalBottomSheetGroupItemsBuilder(group)
             .apply(groupItemsInit)
             .also { groupItems += it }
             .build()
 
     /** Creates a new group. */
-    private fun group(init: ModalBottomSheetGroupDSL.() -> Unit) =
-        ModalBottomSheetGroupItemsDSL(ModalBottomSheetGroupDSL().apply(init).build())
+    private fun group(init: ModalBottomSheetGroupBuilder.() -> Unit) =
+        ModalBottomSheetGroupItemsBuilder(ModalBottomSheetGroupBuilder().apply(init).build())
             .also { groupItems += it }
 
     /** Creates a new group. */
-    fun group(id: Int, init: ModalBottomSheetGroupDSL.() -> Unit) = group {
+    fun group(id: Int, init: ModalBottomSheetGroupBuilder.() -> Unit) = group {
         this.id = id
         init()
     }
 
     /** Creates a new group with the specified list of items and returns the created items. */
     private fun group(
-        init: ModalBottomSheetGroupDSL.() -> Unit,
-        groupItemsInit: ModalBottomSheetGroupItemsDSL.() -> Unit
+        init: ModalBottomSheetGroupBuilder.() -> Unit,
+        groupItemsInit: ModalBottomSheetGroupItemsBuilder.() -> Unit
     ) =
-        ModalBottomSheetGroupItemsDSL(ModalBottomSheetGroupDSL().apply(init).build())
+        ModalBottomSheetGroupItemsBuilder(ModalBottomSheetGroupBuilder().apply(init).build())
             .apply(groupItemsInit)
             .also { groupItems += it }
             .build()
@@ -94,8 +94,8 @@ class ModalBottomSheetDSL {
     /** Creates a new group with the specified list of items and returns the created items. */
     fun group(
         id: Int,
-        init: ModalBottomSheetGroupDSL.() -> Unit,
-        groupItemsInit: ModalBottomSheetGroupItemsDSL.() -> Unit
+        init: ModalBottomSheetGroupBuilder.() -> Unit,
+        groupItemsInit: ModalBottomSheetGroupItemsBuilder.() -> Unit
     ) = group({
         this.id = id
         init()
@@ -106,7 +106,7 @@ class ModalBottomSheetDSL {
 }
 
 @ModalBottomSheetDsl
-class ModalBottomSheetGroupDSL(
+class ModalBottomSheetGroupBuilder(
     private val group: ModalBottomSheetGroup = ModalBottomSheetGroup(id = Integer.MIN_VALUE),
     groupOptions: ModalBottomSheetGroup.() -> Unit = {}
 ) {
@@ -132,16 +132,16 @@ class ModalBottomSheetGroupDSL(
 }
 
 @ModalBottomSheetDsl
-class ModalBottomSheetGroupItemsDSL(private val group: ModalBottomSheetGroup) {
+class ModalBottomSheetGroupItemsBuilder(private val group: ModalBottomSheetGroup) {
     private var items = mutableListOf<ModalBottomSheetItem>()
 
     /** Adds the specified item to the group and returns the created item. */
-    private fun item(init: ModalBottomSheetItemDSL.() -> Unit) =
-        ModalBottomSheetItemDSL().apply(init).build()
+    private fun item(init: ModalBottomSheetItemBuilder.() -> Unit) =
+        ModalBottomSheetItemBuilder().apply(init).build()
             .also { items += it }
 
     /** Adds the specified item to the group and returns the created item. */
-    fun item(title: String, init: ModalBottomSheetItemDSL.() -> Unit) = item {
+    fun item(title: String, init: ModalBottomSheetItemBuilder.() -> Unit) = item {
         this.title = title
         init()
     }
@@ -151,8 +151,8 @@ class ModalBottomSheetGroupItemsDSL(private val group: ModalBottomSheetGroup) {
         item.also { items += it }
 
     /** Adds the specified list of items to the group and returns the created items. */
-    fun items(vararg init: ModalBottomSheetItemDSL.() -> Unit) =
-        init.map { ModalBottomSheetItemDSL().apply(it).build() }
+    fun items(vararg init: ModalBottomSheetItemBuilder.() -> Unit) =
+        init.map { ModalBottomSheetItemBuilder().apply(it).build() }
             .also { items += it }
 
     /** Adds the specified list of items to the group and returns the created items. */
@@ -167,11 +167,11 @@ class ModalBottomSheetGroupItemsDSL(private val group: ModalBottomSheetGroup) {
         items(buildList(itemsInit))
 
     /** Returns the added list of items with the group assigned to it. */
-    fun build() = items.map { it.apply { group = this@ModalBottomSheetGroupItemsDSL.group } }
+    fun build() = items.map { it.apply { group = this@ModalBottomSheetGroupItemsBuilder.group } }
 }
 
 @ModalBottomSheetDsl
-class ModalBottomSheetItemDSL(
+class ModalBottomSheetItemBuilder(
     private val item: ModalBottomSheetItem = ModalBottomSheetItem(title = ""),
     itemOptions: ModalBottomSheetItem.() -> Unit = {}
 ) {
@@ -209,15 +209,15 @@ class ModalBottomSheetItemDSL(
 
 /** Creates a [ModalBottomSheetGroup] using DSL syntax. */
 @ModalBottomSheetDsl
-inline fun group(init: ModalBottomSheetGroupDSL.() -> Unit) =
-    ModalBottomSheetGroupDSL().apply(init).build()
+inline fun group(init: ModalBottomSheetGroupBuilder.() -> Unit) =
+    ModalBottomSheetGroupBuilder().apply(init).build()
 
 /** Creates a [ModalBottomSheetItem] using DSL syntax. */
 @ModalBottomSheetDsl
-inline fun item(init: ModalBottomSheetItemDSL.() -> Unit) =
-    ModalBottomSheetItemDSL().apply(init).build()
+inline fun item(init: ModalBottomSheetItemBuilder.() -> Unit) =
+    ModalBottomSheetItemBuilder().apply(init).build()
 
 /** Creates a list of [ModalBottomSheetItem]s using DSL syntax. */
 @ModalBottomSheetDsl
-inline fun items(init: ModalBottomSheetDSL.() -> Unit) =
-    ModalBottomSheetDSL().apply(init).build()
+inline fun items(init: ModalBottomSheetBuilder.() -> Unit) =
+    ModalBottomSheetBuilder().apply(init).build()
