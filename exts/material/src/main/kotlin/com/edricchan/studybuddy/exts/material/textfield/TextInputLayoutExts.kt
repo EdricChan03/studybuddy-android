37483f90c062bd6a90f9fb5f04e97d1996496981
Edric <edric.chan.1997@gmail.com>
2023-10-10 14:17:15 +0800

    TextInputLayoutExts: Use `orEmpty` insead of `Any?.toString()`
    `Any?.toString()` returns the _string_ `"null"` if the receiver's value
    is `null`, so we should instead use `String?.orEmpty()` to get an empty
    string.
    
    Add a TODO to migrate to nullable `String` at some point :\

--- exts/material/src/main/kotlin/com/edricchan/studybuddy/exts/material/textfield/TextInputLayoutExts.kt
+++ exts/material/src/main/kotlin/com/edricchan/studybuddy/exts/material/textfield/TextInputLayoutExts.kt
@@ -3,22 +3,17 @@ package com.edricchan.studybuddy.exts.material.textfield
 import android.text.Editable
 import com.google.android.material.textfield.TextInputLayout
 
-/**
- * Getter/setter for the [TextInputLayout]'s [android.widget.EditText] value
- * Returns/requires an [Editable]
- */
+/** The [TextInputLayout]'s [android.widget.EditText] value as an [Editable]. */
 var TextInputLayout.editTextValue: Editable?
     get() = this.editText?.text
     set(value) {
         this.editText?.text = value
     }
 
-/**
- * Getter/setter for the [TextInputLayout]'s [android.widget.EditText] value
- * Returns/requires a [String]
- */
+/** The [TextInputLayout]'s [android.widget.EditText] value as a [String]. */
 var TextInputLayout.editTextStrValue: String
-    get() = this.editText?.text.toString()
+    // TODO: Remove orEmpty() when usages have migrated to nullable String
+    get() = this.editText?.text?.toString().orEmpty()
     set(value) {
         this.editText?.setText(value)
     }
