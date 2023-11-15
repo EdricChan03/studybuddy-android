e08dc45b621d6b9f116c13ed2fc3d923a1b633bd
Edric <edric.chan.1997@gmail.com>
2023-11-15 19:28:48 +0800

    ViewTaskActivity: Enable edge-to-edge behaviour
    * Use a custom `AppBarLayout` that sets the relevant M3 styling
    * Update manifest entry to use the `NoActionBar` theme variant
    * Set `android:fitsSystemBars=true` for `CoordinatorLayout` and
      `BottomAppBar`
    * Set the toolbar as the support action-bar
    * Set scrolling behaviour for content + hide `BottomAppBar` on scroll

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/task/ViewTaskActivity.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/task/ViewTaskActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
 import androidx.core.view.isVisible
 import androidx.core.view.plusAssign
@@ -49,8 +50,11 @@ class ViewTaskActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityViewTaskBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityViewTaskBinding.inflate(layoutInflater).apply {
+            setSupportActionBar(toolbar)
+        }.also { setContentView(it.root) }
+
+        enableEdgeToEdge()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
