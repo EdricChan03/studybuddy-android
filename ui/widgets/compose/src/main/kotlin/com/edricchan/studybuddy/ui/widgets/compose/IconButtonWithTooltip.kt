5cb513799625e821bebe693e65cd68de1cb73c6d
Edric <edric.chan.1997@gmail.com>
2024-01-15 00:57:21 +0800

    IconButtonWithTooltip: Expose `CaretScope`
    This change was introduced in `1.2.0-beta02` of Jetpack Compose
    Material3, see https://developer.android.com/jetpack/androidx/releases/compose-material3#1.2.0-beta02

--- ui/widgets/compose/src/main/kotlin/com/edricchan/studybuddy/ui/widgets/compose/IconButtonWithTooltip.kt
+++ ui/widgets/compose/src/main/kotlin/com/edricchan/studybuddy/ui/widgets/compose/IconButtonWithTooltip.kt
@@ -1,5 +1,6 @@
 package com.edricchan.studybuddy.ui.widgets.compose
 
+import androidx.compose.material3.CaretScope
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TooltipBox
@@ -28,7 +29,7 @@ fun IconButtonWithTooltip(
     tooltipModifier: Modifier = Modifier,
     positionProvider: PopupPositionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
     state: TooltipState = rememberTooltipState(),
-    tooltip: @Composable () -> Unit,
+    tooltip: @Composable CaretScope.() -> Unit,
     icon: @Composable () -> Unit,
     enabled: Boolean = true,
     onClick: () -> Unit
