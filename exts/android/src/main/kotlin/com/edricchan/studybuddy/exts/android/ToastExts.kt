3ddf95fd6fe31339567df845015ecba6e56ed8ce
Edric <edric.chan.1997@gmail.com>
2024-01-29 17:56:22 +0800

    ToastExts: Remove redundant `Activity` receiver extension functions
    These are already covered by their `Context` receiver counterparts as
    `Activity`s extend `Context`

--- exts/android/src/main/kotlin/com/edricchan/studybuddy/exts/android/ToastExts.kt
+++ exts/android/src/main/kotlin/com/edricchan/studybuddy/exts/android/ToastExts.kt
@@ -1,6 +1,5 @@
 package com.edricchan.studybuddy.exts.android
 
-import android.app.Activity
 import android.content.Context
 import android.widget.Toast
 import androidx.annotation.StringRes
@@ -37,41 +36,6 @@ fun Context.showToast(@StringRes messageResId: Int, duration: Int) {
  * @param toastOptions Options to be passed to the [Toast]
  */
 fun Context.showToast(toastOptions: Toast.() -> Unit) {
-    Toast(this).apply(toastOptions)
-        .show()
-}
-
-/**
- * Shows a [Toast] with the specified options.
- * @param message The message to be shown to the user as a [CharSequence]
- * @param duration The length of time to show the toast for
- * @see [Toast.makeText]
- * @see [Toast.setText]
- * @see [Toast.setDuration]
- */
-fun Activity.showToast(message: CharSequence, duration: Int) {
-    Toast.makeText(this, message, duration).show()
-}
-
-/**
- * Shows a [Toast] with the specified options.
- * @param messageResId The message to be shown to the user as a string resource ID
- * @param duration The length of time to show the toast for
- * @see [Toast.makeText]
- * @see [Toast.setText]
- * @see [Toast.setDuration]
- */
-fun Activity.showToast(@StringRes messageResId: Int, duration: Int) {
-    Toast.makeText(this, messageResId, duration).show()
-}
-
-/**
- * Shows a toast with the specified [toastOptions]. Note that this method will
- * automatically call [Toast.show] for you, so there's no need to pass that with
- * the options.
- * @param toastOptions Options to be passed to the [Toast]
- */
-fun Activity.showToast(toastOptions: Toast.() -> Unit) {
     Toast(this).apply(toastOptions)
         .show()
 }
