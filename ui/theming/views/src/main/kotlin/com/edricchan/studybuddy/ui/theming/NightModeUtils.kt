2f9ed376e138e5fd7735e512dca71129471766d1
Edric <edric.chan.1997@gmail.com>
2024-03-13 22:16:12 +0800

    NightModeUtils.kt: Migrate to `DarkThemeValue` enum

--- ui/theming/views/src/main/kotlin/com/edricchan/studybuddy/ui/theming/NightModeUtils.kt
+++ ui/theming/views/src/main/kotlin/com/edricchan/studybuddy/ui/theming/NightModeUtils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.edit
 import com.edricchan.studybuddy.core.settings.appearance.DarkThemePrefValues
+import com.edricchan.studybuddy.core.settings.appearance.DarkThemeValue
 import com.edricchan.studybuddy.exts.androidx.preference.defaultSharedPreferences
 import androidx.appcompat.app.AppCompatDelegate.NightMode as NightModeAnnotation
 
@@ -36,10 +37,10 @@ enum class NightMode(@NightModeAnnotation val modeId: Int) {
 }
 
 /** An enum-like class used to represent a theme setting value and its corresponding night mode value. */
-enum class DarkThemeValue(val value: String?, val mode: NightMode) {
+enum class DarkThemeOption(val prefValue: DarkThemeValue, val mode: NightMode) {
 
     V1Always(
-        DarkThemePrefValues.V1_ALWAYS,
+        DarkThemeValue.V1Always,
         NightMode.Yes
     ),
 
@@ -49,37 +50,37 @@ enum class DarkThemeValue(val value: String?, val mode: NightMode) {
             "Considering using an explicit setting, or AutoBattery.",
         ReplaceWith(
             "V2AutoBatterySaver",
-            "com.edricchan.studybuddy.ui.theming.DarkThemeValue.V2AutoBatterySaver"
+            "com.edricchan.studybuddy.ui.theming.DarkThemeOption.V2AutoBatterySaver"
         )
     )
     V1AutoTime(
-        DarkThemePrefValues.V1_AUTO_TIME,
+        DarkThemeValue.V1AutoTime,
         NightMode.AutoTime
     ),
     V1Never(
-        DarkThemePrefValues.V1_NEVER,
+        DarkThemeValue.V1Never,
         NightMode.No
     ),
     V2Always(
-        DarkThemePrefValues.V2_ALWAYS,
+        DarkThemeValue.V2Always,
         NightMode.Yes
     ),
     V2AutoBatterySaver(
-        DarkThemePrefValues.V2_AUTO_BATTERY,
+        DarkThemeValue.V2AutoBattery,
         NightMode.AutoBattery
     ),
     V2FollowSystem(
-        DarkThemePrefValues.V2_FOLLOW_SYSTEM,
+        DarkThemeValue.V2FollowSystem,
         NightMode.FollowSystem
     ),
     V2Never(
-        DarkThemePrefValues.V2_NEVER,
+        DarkThemeValue.V2Never,
         NightMode.No
     ),
-    Default(null, NightMode.Unspecified);
+    Default(DarkThemeValue.V2FollowSystem, NightMode.Unspecified);
 
     companion object {
-        /** Retrieves the [DarkThemeValue] given the [value], or [Default] if invalid. */
+        /** Retrieves the [DarkThemeOption] given the [value], or [Default] if invalid. */
         @Suppress("DEPRECATION")
         fun fromValue(value: String?) = when (value) {
             DarkThemePrefValues.V1_ALWAYS -> V1Always
@@ -92,16 +93,13 @@ enum class DarkThemeValue(val value: String?, val mode: NightMode) {
             else -> Default
         }
 
-        /** Retrieves the [DarkThemeValue] given the [mode], or [Default] if invalid. */
-        @Suppress("DEPRECATION")
-        fun fromMode(mode: NightMode) = when (mode) {
-            NightMode.AutoTime -> V1AutoTime
-            NightMode.Yes -> V2Always
-            NightMode.No -> V2Never
-            NightMode.FollowSystem -> V2FollowSystem
-            NightMode.AutoBattery -> V2AutoBatterySaver
-            else -> Default
-        }
+        /** Retrieves the [DarkThemeOption] given the [value], or [Default] if invalid. */
+        fun fromValue(value: DarkThemeValue) =
+            entries.find { it.prefValue == value } ?: Default
+
+        /** Retrieves the [DarkThemeOption] given the [mode], or [Default] if invalid. */
+        fun fromMode(mode: NightMode) =
+            entries.find { it.mode == mode } ?: Default
     }
 }
 
@@ -111,8 +109,8 @@ var defaultNightMode: NightMode
     set(value) = AppCompatDelegate.setDefaultNightMode(value.modeId)
 
 /** The currently selected dark theme. */
-var Context.prefDarkTheme: DarkThemeValue
-    get() = DarkThemeValue.fromValue(
+var Context.prefDarkTheme: DarkThemeOption
+    get() = DarkThemeOption.fromValue(
         defaultSharedPreferences.getString(
             PREF_DARK_THEME,
             DarkThemePrefValues.V2_AUTO_BATTERY
@@ -120,12 +118,12 @@ var Context.prefDarkTheme: DarkThemeValue
     )
     set(theme) {
         defaultSharedPreferences.edit {
-            putString(PREF_DARK_THEME, theme.value)
+            putString(PREF_DARK_THEME, theme.prefValue.value)
         }
     }
 
 /** Applies the dark theme given the [newTheme]. */
-fun Context.applyDarkTheme(newTheme: DarkThemeValue = prefDarkTheme) {
+fun Context.applyDarkTheme(newTheme: DarkThemeOption = prefDarkTheme) {
     // Update the preference value if they don't match
     if (newTheme != prefDarkTheme) prefDarkTheme = newTheme
     defaultNightMode = newTheme.mode
