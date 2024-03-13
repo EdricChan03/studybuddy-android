4f241c844cbdce57346410ad701ed85f22c5d9af
Edric <edric.chan.1997@gmail.com>
2024-03-13 15:51:21 +0800

    Mark compat colours as deprecated

--- ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/theme/compat/Color.kt
+++ ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/theme/compat/Color.kt
@@ -4,6 +4,13 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
+@Deprecated(
+    "Use the M3 variant (StudyBuddyM3LightColors) where applicable",
+    ReplaceWith(
+        "StudyBuddyM3LightColors",
+        "com.edricchan.studybuddy.ui.theming.compose.theme.m3.StudyBuddyM3LightColors"
+    )
+)
 val StudyBuddyCompatLightColors = lightColorScheme(
     // TODO: Add colours
     primary = Color(0xff3948ab),
@@ -11,6 +18,13 @@ val StudyBuddyCompatLightColors = lightColorScheme(
     secondary = Color(0xffff4081)
 )
 
+@Deprecated(
+    "Use the M3 variant (StudyBuddyM3DarkColors) where applicable",
+    ReplaceWith(
+        "StudyBuddyM3DarkColors",
+        "com.edricchan.studybuddy.ui.theming.compose.theme.m3.StudyBuddyM3DarkColors"
+    )
+)
 val StudyBuddyCompatDarkColors = darkColorScheme(
     primary = Color(0xff8e24aa),
     primaryContainer = Color(0xff5c007a),
