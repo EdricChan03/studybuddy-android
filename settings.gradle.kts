2dbc743d76a8fb20f94b955f5904e6640f0b9dc4
Edric <edric.chan.1997@gmail.com>
2024-03-13 16:57:41 +0800

    Remove AboutLibraries fork

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -106,7 +106,6 @@ include(
     ":ui:theming:views",
     ":ui:widgets:compose",
     ":ui:widgets:views",
-    ":ui:widgets:compose:about-libraries",
     ":ui:widgets:compose:navigation",
     ":ui:widgets:compose:option-bottom-sheet",
     ":ui:widgets:modal-bottom-sheet",
