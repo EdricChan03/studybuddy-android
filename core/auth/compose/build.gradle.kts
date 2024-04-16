ae690e09ca8e6d224e2ea213b2f852d09163a8f0
Edric <edric.chan.1997@gmail.com>
2024-04-16 16:52:57 +0800

    Add CompositionLocals for Firebase auth user data
    See #368 and #585
    
    * Scaffold `:core:auth:compose` module
    * Include module in settings script
    * Add composable to provide CompositionLocals

---
+++ core/auth/compose/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    com.edricchan.studybuddy.library.`android-compose`
+}
+
+android {
+    namespace = "com.edricchan.studybuddy.core.auth"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.ui.theming.compose)
+
+    // Firebase dependencies
+    api(platform(libs.firebase.bom))
+    api(libs.firebase.auth.ktx)
+    implementation(libs.playServices.auth)
+
+    // Compose dependencies
+    implementation(libs.bundles.androidx.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    debugImplementation(libs.bundles.androidx.compose.tooling)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+
+    // Compose rule support
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+}
---
+++ core/auth/compose/src/main/kotlin/com/edricchan/studybuddy/core/auth/UserLocals.kt
@@ -0,0 +1,42 @@
+package com.edricchan.studybuddy.core.auth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocal
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.flow.Flow
+
+/** [CompositionLocal] of the currently signed-in [FirebaseUser], or `null`. */
+val LocalCurrentUser: ProvidableCompositionLocal<FirebaseUser?> =
+    compositionLocalOf { Firebase.auth.currentUser }
+
+/** [CompositionLocal] of whether there's a currently signed-in [FirebaseUser]. */
+val LocalIsSignedIn: ProvidableCompositionLocal<Boolean> =
+    compositionLocalOf { Firebase.auth.currentUser != null }
+
+/**
+ * Provides the [LocalCurrentUser] and [LocalIsSignedIn] composition locals with the
+ * given [userFlow].
+ *
+ * The [userFlow] will be collected as a state and used for [LocalCurrentUser].
+ */
+@Composable
+fun ProvideCurrentUser(
+    initialUser: FirebaseUser? = Firebase.auth.currentUser,
+    userFlow: Flow<FirebaseUser?>,
+    content: @Composable () -> Unit
+) {
+    val user by userFlow.collectAsStateWithLifecycle(initialValue = initialUser)
+
+    CompositionLocalProvider(
+        LocalCurrentUser provides user,
+        LocalIsSignedIn provides (user != null),
+        content = content
+    )
+}
