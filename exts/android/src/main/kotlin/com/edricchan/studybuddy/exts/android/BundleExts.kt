741c851c84c12447aff95138ef6fc16676deec4d
Edric <edric.chan.1997@gmail.com>
2024-04-22 19:27:02 +0800

    BundleExts: Use `BundleCompat` impl for `getSerializableCompat`

--- exts/android/src/main/kotlin/com/edricchan/studybuddy/exts/android/BundleExts.kt
+++ exts/android/src/main/kotlin/com/edricchan/studybuddy/exts/android/BundleExts.kt
@@ -1,15 +1,12 @@
 package com.edricchan.studybuddy.exts.android
 
-import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import androidx.core.os.BundleCompat
 import java.io.Serializable
 
-@Suppress("DEPRECATION")
 inline fun <reified T : Serializable> Bundle.getSerializableCompat(key: String?) =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) getSerializable(key, T::class.java)
-    else getSerializable(key) as? T
+    BundleCompat.getSerializable(this, key, T::class.java)
 
 inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String?) =
     BundleCompat.getParcelable(this, key, T::class.java)
