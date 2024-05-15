793a76095a4bf44b582d5aa91fc0b5fd9697021a
Edric <edric.chan.1997@gmail.com>
2024-05-16 03:25:24 +0800

    HttpModule: Add caching for the HttpClient by default

--- core/di/src/main/kotlin/com/edricchan/studybuddy/core/di/HttpModule.kt
+++ core/di/src/main/kotlin/com/edricchan/studybuddy/core/di/HttpModule.kt
@@ -1,11 +1,16 @@
 package com.edricchan.studybuddy.core.di
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.cache.storage.FileStorage
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import javax.inject.Qualifier
@@ -14,12 +19,23 @@ import javax.inject.Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class ContentNegotiationJsonHttpClient
 
+val Context.defaultCacheStorage get() = FileStorage(cacheDir.resolve("ktor"))
+
+fun HttpClientConfig<*>.installBaseConfig(context: Context) {
+    install(HttpCache) {
+        privateStorage(context.defaultCacheStorage)
+    }
+}
+
 @Module
 @InstallIn(SingletonComponent::class)
 object HttpModule {
     @ContentNegotiationJsonHttpClient
     @Provides
-    fun provideContentNegotiationJsonHttpClient() = HttpClient(OkHttp) {
+    fun provideContentNegotiationJsonHttpClient(
+        @ApplicationContext context: Context
+    ) = HttpClient(OkHttp) {
+        installBaseConfig(context)
         install(ContentNegotiation) {
             json()
         }
