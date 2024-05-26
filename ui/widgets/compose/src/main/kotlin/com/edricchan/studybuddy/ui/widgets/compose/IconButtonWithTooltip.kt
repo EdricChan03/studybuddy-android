e0d07996162b434c035222cff79bd2402b8eff1c
Edric <edric.chan.1997@gmail.com>
2024-05-26 18:00:09 +0800

    Update Compose Material 3 to 1.3.0-beta01
    * Update dependency
    * Update `CaretScope` usages to `TooltipScope`

--- ui/widgets/compose/src/main/kotlin/com/edricchan/studybuddy/ui/widgets/compose/IconButtonWithTooltip.kt
+++ ui/widgets/compose/src/main/kotlin/com/edricchan/studybuddy/ui/widgets/compose/IconButtonWithTooltip.kt
@@ -1,10 +1,10 @@
 package com.edricchan.studybuddy.ui.widgets.compose
 
-import androidx.compose.material3.CaretScope
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.TooltipScope
 import androidx.compose.material3.TooltipState
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
@@ -29,7 +29,7 @@ fun IconButtonWithTooltip(
     tooltipModifier: Modifier = Modifier,
     positionProvider: PopupPositionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
     state: TooltipState = rememberTooltipState(),
-    tooltip: @Composable CaretScope.() -> Unit,
+    tooltip: @Composable TooltipScope.() -> Unit,
     icon: @Composable () -> Unit,
     enabled: Boolean = true,
     onClick: () -> Unit
@@ -39,11 +39,5 @@ fun IconButtonWithTooltip(
     state = state,
     positionProvider = positionProvider
 ) {
-    IconButton(
-        modifier = modifier,
-        onClick = onClick,
-        enabled = enabled
-    ) {
-        icon()
-    }
+    IconButton(modifier = modifier, onClick = onClick, enabled = enabled, content = icon)
 }
