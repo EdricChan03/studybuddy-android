27a13e3cbebff3d854940a2e60283dc1e56e752c
Edric <edric.chan.1997@gmail.com>
2024-06-11 02:55:44 +0800

    ListAdapterUtils: Add default `areContentsTheSame` impl
    The default implementation will do an `==` comparison

--- utils/recyclerview/src/main/kotlin/com/edricchan/studybuddy/utils/recyclerview/ListAdapterUtils.kt
+++ utils/recyclerview/src/main/kotlin/com/edricchan/studybuddy/utils/recyclerview/ListAdapterUtils.kt
@@ -9,9 +9,8 @@ import androidx.recyclerview.widget.DiffUtil
  */
 inline fun <T : Any> diffCallback(
     crossinline areItemsTheSame: (oldItem: T, newItem: T) -> Boolean,
-    crossinline areContentsTheSame: (oldItem: T, newItem: T) -> Boolean
-) =
-    object : DiffUtil.ItemCallback<T>() {
-        override fun areItemsTheSame(old: T, new: T) = areItemsTheSame(old, new)
-        override fun areContentsTheSame(old: T, new: T) = areContentsTheSame(old, new)
-    }
+    crossinline areContentsTheSame: (oldItem: T, newItem: T) -> Boolean = { old, new -> old == new }
+) = object : DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(old: T, new: T) = areItemsTheSame(old, new)
+    override fun areContentsTheSame(old: T, new: T) = areContentsTheSame(old, new)
+}
