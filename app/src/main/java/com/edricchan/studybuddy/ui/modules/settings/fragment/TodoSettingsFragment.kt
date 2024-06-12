c439fc14b2c923e728afb31b3a2d05257ef4a3df
Edric <edric.chan.1997@gmail.com>
2024-06-12 21:32:16 +0800

    TodoSettingsFragment: Use `setFragment` extension function

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/settings/fragment/TodoSettingsFragment.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/settings/fragment/TodoSettingsFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.preference.Preference
 import com.edricchan.studybuddy.R
 import com.edricchan.studybuddy.constants.Constants
+import com.edricchan.studybuddy.exts.androidx.preference.setFragment
 import com.edricchan.studybuddy.ui.modules.settings.fragment.todo.WeeklySummarySettingsFragment
 import com.edricchan.studybuddy.ui.preference.MaterialPreferenceFragment
 
 class TodoSettingsFragment : MaterialPreferenceFragment() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pref_todos, rootKey)
-        findPreference<Preference>(Constants.prefWeeklySummaryEnabled)?.fragment =
-            WeeklySummarySettingsFragment::class.java.name
+        findPreference<Preference>(Constants.prefWeeklySummaryEnabled)
+            ?.setFragment<WeeklySummarySettingsFragment>()
     }
 }
