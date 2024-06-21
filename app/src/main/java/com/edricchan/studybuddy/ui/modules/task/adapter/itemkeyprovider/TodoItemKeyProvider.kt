61a982ca66bdd8010e947c232ba82e1cde739b86
Edric <edric.chan.1997@gmail.com>
2024-06-22 01:42:40 +0800

    TodoItemKeyProvider: Code clean-up
    * Migrate away from Hungarian notation (`mList` -> `list`)
    * Use Kotlin `associate` stdlib function
    * Use one-liner functions
    * Return `NO_POSITION` instead of potentially throwing a NPE

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/task/adapter/itemkeyprovider/TodoItemKeyProvider.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/task/adapter/itemkeyprovider/TodoItemKeyProvider.kt
@@ -1,33 +1,20 @@
 package com.edricchan.studybuddy.ui.modules.task.adapter.itemkeyprovider
 
 import androidx.recyclerview.selection.ItemKeyProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.edricchan.studybuddy.features.tasks.data.model.TodoItem
 
 /**
  * Creates a new provider.
  *
- * @param mItemList The list of tasks
+ * @param itemList The list of tasks
  */
 class TodoItemKeyProvider(
-    private val mItemList: List<TodoItem>
+    private val itemList: List<TodoItem>
 ) : ItemKeyProvider<String>(SCOPE_CACHED) {
-    private val mKeyToPosition: MutableMap<String, Int>
+    private val keyToPosition = itemList.withIndex().associate { it.value.id to it.index }
 
-    init {
-        this.mKeyToPosition = HashMap(mItemList.size)
-        var i = 0
-        for ((id) in mItemList) {
-            mKeyToPosition[id] = i
-            i++
-        }
+    override fun getKey(position: Int): String = itemList[position].id
 
-    }
-
-    override fun getKey(position: Int): String {
-        return mItemList[position].id
-    }
-
-    override fun getPosition(key: String): Int {
-        return mKeyToPosition[key]!!
-    }
+    override fun getPosition(key: String): Int = keyToPosition[key] ?: RecyclerView.NO_POSITION
 }
