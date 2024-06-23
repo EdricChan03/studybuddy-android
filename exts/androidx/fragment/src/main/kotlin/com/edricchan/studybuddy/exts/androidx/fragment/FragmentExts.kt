2caa795769e80317f3c3e697690c20a1c0c17446
Edric <edric.chan.1997@gmail.com>
2024-06-24 00:30:44 +0800

    FragmentExts: Remove unused extension methods

--- exts/androidx/fragment/src/main/kotlin/com/edricchan/studybuddy/exts/androidx/fragment/FragmentExts.kt
+++ exts/androidx/fragment/src/main/kotlin/com/edricchan/studybuddy/exts/androidx/fragment/FragmentExts.kt
@@ -1,7 +1,6 @@
 package com.edricchan.studybuddy.exts.androidx.fragment
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.annotation.IdRes
@@ -14,51 +13,8 @@ import com.google.android.material.transition.MaterialFadeThrough
  * Starts an activity [T] with the fragment's context
  * @param T The activity to launch
  */
-inline fun <reified T : Activity> Fragment.startActivity() {
-    startActivity(Intent(context, T::class.java))
-}
-
-/**
- * Starts an activity [T] with the fragment's context
- * @param T The activity to launch
- * @param intentOptions Intent options
- */
-inline fun <reified T : Activity> Fragment.startActivity(intentOptions: Intent.() -> Unit) {
-    val intent = Intent(context, T::class.java).apply(intentOptions)
-    startActivity(intent)
-}
-
-/**
- * Starts an activity [T] with the specified [context]
- * @param T The activity to launch
- * @param context The context to be used to launch the activity
- */
-inline fun <reified T : Activity> Fragment.startActivity(context: Context) {
-    startActivity(Intent(context, T::class.java))
-}
-
-/**
- * Starts an activity [T] with the specified [context]
- * @param T The activity to launch
- * @param context The context to be used to launch the activity
- * @param intentOptions Options for the intent
- */
-inline fun <reified T : Activity> Fragment.startActivity(
-    context: Context,
-    intentOptions: Intent.() -> Unit
-) {
-    val intent = Intent(context, T::class.java).apply(intentOptions)
-    startActivity(intent)
-}
-
-/**
- * Starts an activity [T] with the fragment's context and [options] to be passed to the intent
- * @param T The activity to launch
- * @param options Options to be passed to the activity
- * @see Intent.getExtras This can be used in the activity launched to retrieve the [options] specified
- */
-inline fun <reified T : Activity> Fragment.startActivity(options: Bundle?) {
-    startActivity(Intent(context, T::class.java), options)
+inline fun <reified T : Activity> Fragment.startActivity(intentOptions: Intent.() -> Unit = {}) {
+    startActivity(Intent(context, T::class.java).apply(intentOptions))
 }
 
 /**
@@ -70,35 +26,7 @@ inline fun <reified T : Activity> Fragment.startActivity(options: Bundle?) {
  */
 inline fun <reified T : Activity> Fragment.startActivity(
     options: Bundle?,
-    intentOptions: Intent.() -> Unit
-) {
-    val intent = Intent(context, T::class.java).apply(intentOptions)
-    startActivity(intent, options)
-}
-
-/**
- * Starts an activity [T] with the specified [context] and [options] to be passed to the intent
- * @param T The activity to launch
- * @param context The context to be used to launch the activity
- * @param options Options to be passed to the activity
- * @see Intent.getExtras This can be used in the activity launched to retrieve the [options] specified
- */
-inline fun <reified T : Activity> Fragment.startActivity(context: Context, options: Bundle?) {
-    startActivity(Intent(context, T::class.java), options)
-}
-
-/**
- * Starts an activity [T] with the specified [context] and [options] to be passed to the intent
- * @param T The activity to launch
- * @param context The context to be used to launch the activity
- * @param options Options to be passed to the activity
- * @param intentOptions Options to be set on the [Intent]
- * @see Intent.getExtras This can be used in the activity launched to retrieve the [options] specified
- */
-inline fun <reified T : Activity> Fragment.startActivity(
-    context: Context,
-    options: Bundle?,
-    intentOptions: Intent.() -> Unit
+    intentOptions: Intent.() -> Unit = {}
 ) {
     val intent = Intent(context, T::class.java).apply(intentOptions)
     startActivity(intent, options)
