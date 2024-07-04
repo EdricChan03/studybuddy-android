70f1a1ae185ec3690dc8c1034e2393f0b8f5db1a
Edric <edric.chan.1997@gmail.com>
2024-07-04 15:43:43 +0800

    DynamicColors: Add extension function to retrieve a themed colour

--- ui/theming/views/src/main/kotlin/com/edricchan/studybuddy/ui/theming/DynamicColors.kt
+++ ui/theming/views/src/main/kotlin/com/edricchan/studybuddy/ui/theming/DynamicColors.kt
@@ -1,6 +1,8 @@
 package com.edricchan.studybuddy.ui.theming
 
 import android.content.Context
+import android.view.View
+import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import com.google.android.material.color.MaterialColors
@@ -18,3 +20,13 @@ val Context.dynamicColorPrimary
         MaterialR.attr.colorPrimary,
         ContextCompat.getColor(this, R.color.colorPrimary)
     )
+
+/**
+ * Retrieves the specified colour for the given [colorAttrResId] given the receiver
+ * [View] as a colour [Int].
+ */
+@ColorInt
+fun View.getThemedColor(@AttrRes colorAttrResId: Int): Int = MaterialColors.getColor(
+    this,
+    colorAttrResId
+)
