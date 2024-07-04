d9d899c0d8975c515e3f2affebbefd37c244958b
Edric <edric.chan.1997@gmail.com>
2024-07-04 15:43:13 +0800

    DynamicColors: Clarify KDoc

--- ui/theming/views/src/main/kotlin/com/edricchan/studybuddy/ui/theming/DynamicColors.kt
+++ ui/theming/views/src/main/kotlin/com/edricchan/studybuddy/ui/theming/DynamicColors.kt
@@ -7,8 +7,9 @@ import com.google.android.material.color.MaterialColors
 import com.google.android.material.R as MaterialR
 
 /**
- * Retrieves the `colorPrimary` dynamic colour, or the default value if the
- * receiver context does not have dynamic theming applied, as a colour [Int].
+ * Retrieves the [`colorPrimary`][MaterialR.attr.colorPrimary] dynamic colour, or the
+ * [default value][R.color.colorPrimary] if the receiver context does not have
+ * dynamic theming applied, as a colour [Int].
  */
 @get:ColorInt
 val Context.dynamicColorPrimary
