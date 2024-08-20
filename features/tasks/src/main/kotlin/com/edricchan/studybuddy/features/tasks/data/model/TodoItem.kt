2585dd3a5a5e3cab5a84ea29c49fe40ca0e01de0
Edric <edric.chan.1997@gmail.com>
2024-08-21 01:47:40 +0800

    TodoItem: Add `Field` enum for typed access to fields

--- features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/data/model/TodoItem.kt
+++ features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/data/model/TodoItem.kt
@@ -11,11 +11,11 @@ import com.google.firebase.firestore.ServerTimestamp
 
 /**
  * Specifies a task (in the old format)
+ * @property id The document's ID of this task
  * @property content The contents of this task
  * @property dueDate The due date of this task
  * @property done Whether the task is initially marked as done
  * @property archived Whether the task has been archived
- * @property id The document's ID of this task
  * @property project The project assigned to this task as a document reference
  * @property tags A list of tags assigned to this task
  * @property title The title of this task
@@ -117,5 +117,52 @@ data class TodoItem(
          * @return The created [TodoItem]
          */
         fun build() = TodoItem(this)
+    }
+
+    /** Represents a field in [TodoItem]. The field's name can be retrieved via [fieldName]. */
+    enum class Field(val fieldName: String) {
+        /** The `title` field. This field is a non-null [String] field. */
+        Title("title"),
+
+        /** The `content` field. This field is a nullable [String] field. */
+        Content("content"),
+
+        /** The `dueDate` field. This field contains a `timestamp` value. */
+        DueDate("dueDate"),
+
+        /** The `tags` field. This field consists of a list of [String]s. */
+        Tags("tags"),
+
+        /**
+         * The `project` field.
+         * This field should hold a [reference to a Firestore document][DocumentReference].
+         */
+        Project("project"),
+
+        /**
+         * The `done` field.
+         * This field is a [Boolean] value, and defaults to `false` if not specified.
+         */
+        IsDone("done"),
+
+        /**
+         * The `archived` field.
+         * This field is a [Boolean] value, and defaults to `false` if not specified.
+         */
+        IsArchived("archived"),
+
+        /**
+         * The `createdAt` field.
+         * This field is a `timestamp` value, and defaults to the server timestamp
+         * if not specified.
+         */
+        CreatedAt("createdAt"),
+
+        /**
+         * The `lastModified` field.
+         * This field is a `timestamp` value, and defaults to the server timestamp
+         * if not specified.
+         */
+        LastModified("lastModified");
     }
 }
