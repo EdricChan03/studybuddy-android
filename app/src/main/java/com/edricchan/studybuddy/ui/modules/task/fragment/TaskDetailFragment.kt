0a99a6109c6bb5cf028d2cf6f193ea98005f8dbc
Edric <edric.chan.1997@gmail.com>
2024-08-21 03:46:47 +0800

    TaskDetailFragment: Add inset handling for `NestedScrollView`

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/task/fragment/TaskDetailFragment.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/task/fragment/TaskDetailFragment.kt
@@ -8,8 +8,11 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.core.view.MenuProvider
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.core.view.plusAssign
+import androidx.core.view.updatePadding
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -104,6 +107,14 @@ class TaskDetailFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.scrollTaskView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.navigationBars())
+
+            v.updatePadding(bottom = insets.bottom)
+
+            windowInsets
+        }
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
