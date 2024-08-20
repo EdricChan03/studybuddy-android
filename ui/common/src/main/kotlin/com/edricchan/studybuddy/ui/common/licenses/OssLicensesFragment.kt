a8c0fdc64291d018ac5ebf922f6c69d6c5b95f05
Edric <edric.chan.1997@gmail.com>
2024-08-21 03:57:30 +0800

    OssLicensesFragment: Theming fixes + apply insets (#493)

--- ui/common/src/main/kotlin/com/edricchan/studybuddy/ui/common/licenses/OssLicensesFragment.kt
+++ ui/common/src/main/kotlin/com/edricchan/studybuddy/ui/common/licenses/OssLicensesFragment.kt
@@ -3,8 +3,16 @@ package com.edricchan.studybuddy.ui.common.licenses
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.fragment.app.Fragment
 import androidx.fragment.compose.content
+import com.edricchan.studybuddy.ui.theming.compose.StudyBuddyTheme
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 
 @Deprecated("Use the LibrariesContainer composable instead")
@@ -14,6 +22,15 @@ class OssLicensesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = content {
-        LibrariesContainer()
+        val nestedScrollInterop = rememberNestedScrollInteropConnection()
+
+        StudyBuddyTheme {
+            Surface {
+                LibrariesContainer(
+                    modifier = Modifier.nestedScroll(nestedScrollInterop),
+                    contentPadding = WindowInsets.navigationBars.asPaddingValues()
+                )
+            }
+        }
     }
 }
