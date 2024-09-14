6127d1339681e413bae2bf2c8a76e0fedc7243c3
Edric <edric.chan.1997@gmail.com>
2024-09-14 16:43:44 +0800

    Update AGP to 8.8.0-alpha02
    Accompanying release for https://androidstudio.googleblog.com/2024/09/android-studio-ladybug-feature-drop_12.html

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,7 +37,7 @@ plugins {
     // https://docs.gradle.org/8.7/userguide/toolchains.html#sub:download_repositories
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 
-    id("com.android.settings") version "8.8.0-alpha01"
+    id("com.android.settings") version "8.8.0-alpha02"
 }
 
 val isCi = !System.getenv("CI").isNullOrEmpty()
