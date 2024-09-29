a1a956e3fbde9d3df6cf94a43c49c10e8efb7900
Edric <edric.chan.1997@gmail.com>
2024-09-29 16:17:26 +0800

    Add missing resources

--- features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/ui/attrs/TaskCreatedAt.kt
+++ features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/ui/attrs/TaskCreatedAt.kt
@@ -1,15 +1,20 @@
 package com.edricchan.studybuddy.features.tasks.ui.attrs
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.edricchan.studybuddy.core.resources.temporal.appFormat
+import com.edricchan.studybuddy.core.resources.temporal.relative.formatRelativeTimeSpan
 import com.edricchan.studybuddy.features.tasks.R
 import com.edricchan.studybuddy.features.tasks.data.model.TodoItem
 import java.time.Instant
@@ -77,3 +82,33 @@ fun TaskCreatedAtListItem(
         TaskCreatedAtText(modifier = textModifier, text = formatDate(createdAt))
     }
 )
+
+/**
+ * Composable which displays a [TodoItem]'s [creation date][TodoItem.createdAt]
+ * using [TaskCreatedAtText]. This variant is intended to be displayed as an
+ * overline for a card.
+ * @param modifier [Modifier] to be used for the [Row].
+ * @param textModifier [Modifier] to be used for the [TaskCreatedAtText].
+ * @param createdAt The task's creation date as an [Instant].
+ */
+@Composable
+fun TaskCreatedAtOverline(
+    modifier: Modifier = Modifier,
+    textModifier: Modifier = Modifier,
+    createdAt: Instant,
+    formatDate: @Composable (Instant) -> String = {
+        LocalContext.current.formatRelativeTimeSpan(it)
+    },
+) = Row(
+    modifier = modifier,
+    horizontalArrangement = Arrangement.spacedBy(16.dp),
+    verticalAlignment = Alignment.CenterVertically
+) {
+    Icon(
+        painterResource(R.drawable.ic_access_time_outline_24dp),
+        contentDescription = stringResource(
+            R.string.view_task_created_at_content_desc
+        )
+    )
+    TaskCreatedAtText(modifier = textModifier, text = formatDate(createdAt))
+}
