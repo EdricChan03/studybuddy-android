d39e3ce955e1161d88dfb27cfff8bcd6697acf53
Edric <edric.chan.1997@gmail.com>
2024-11-29 20:35:46 +0800

    UpdateInfoPrefConstants: Clarify KDoc

--- core/settings/updates/src/main/kotlin/com/edricchan/studybuddy/core/settings/updates/UpdateInfoPrefConstants.kt
+++ core/settings/updates/src/main/kotlin/com/edricchan/studybuddy/core/settings/updates/UpdateInfoPrefConstants.kt
@@ -1,25 +1,20 @@
 package com.edricchan.studybuddy.core.settings.updates
 
-/**
- * Constants to be used for shared preferences regarding update info
- */
 object UpdateInfoPrefConstants {
-    /**
-     * File to be used for storing information about updates
-     */
+    /** File to be used for storing information about updates. */
     const val FILE_UPDATE_INFO = "update_info"
 
     /**
-     * Indicates when the app was last updated via the Update UI in Settings > Versions & About
+     * Indicates when the app was last updated.
      *
-     * This key is stored as a [Long] representation of a date (Use [java.util.Date.getTime] to convert a [java.util.Date] to a [Long])
+     * This key's value is stored as a [Long] representation of a date in milli-seconds.
      */
     const val PREF_LAST_UPDATED_DATE = "last_updated_date"
 
     /**
      * Indicates when the app last checked for updates
      *
-     * This key is stored as a [Long] representation of a date (Use [java.util.Date.getTime] to convert a [java.util.Date] to a [Long])
+     * This key's value is stored as a [Long] representation of a date in milli-seconds.
      */
     const val PREF_LAST_CHECKED_FOR_UPDATES_DATE = "last_checked_for_updates_date"
 }
