8db78335cabbf865a7320f1c418089baab8072aa
Edric <edric.chan.1997@gmail.com>
2024-12-01 14:55:23 +0800

    UpdatesCategory: Copy date value via `onClick` instead

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/debug/compose/UpdatesCategory.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/debug/compose/UpdatesCategory.kt
@@ -1,7 +1,6 @@
 package com.edricchan.studybuddy.ui.modules.debug.compose
 
 import android.util.Log
-import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Icon
@@ -14,9 +13,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.lifecycle.asFlow
 import com.edricchan.studybuddy.R
 import com.edricchan.studybuddy.exts.datetime.formatISO
@@ -34,16 +35,21 @@ private fun PreferenceCategoryScope.UpdateInfoInstantPreference(
     instant: Instant?,
     titleString: String,
     unsetString: String
-) = Preference(
-    modifier = modifier,
-    icon = { Icon(Icons.Outlined.Info, contentDescription = null) },
-    title = { Text(text = titleString) },
-    subtitle = {
-        SelectionContainer {
+) {
+    val clipboard = LocalClipboardManager.current
+
+    Preference(
+        modifier = modifier,
+        onClick = {
+            clipboard.setText(AnnotatedString(instant?.formatISO() ?: unsetString))
+        },
+        icon = { Icon(Icons.Outlined.Info, contentDescription = null) },
+        title = { Text(text = titleString) },
+        subtitle = {
             Text(text = instant?.formatISO() ?: unsetString)
         }
-    }
-)
+    )
+}
 
 @Composable
 fun UpdatesCategory(
