78b7d4e3b8a1206ceb013b98444a970090716033
Edric <edric.chan.1997@gmail.com>
2024-12-01 15:20:04 +0800

    DarkTheme: Deprecate auto battery mode + switch to `sealed class`
    It's now just follow system, always and never

--- core/settings/appearance/src/main/kotlin/com/edricchan/studybuddy/core/settings/appearance/DarkTheme.kt
+++ core/settings/appearance/src/main/kotlin/com/edricchan/studybuddy/core/settings/appearance/DarkTheme.kt
@@ -35,14 +35,46 @@ object DarkThemePrefValues {
         )
     )
     const val V1_NEVER = "3"
+
+    @Deprecated(
+        "AutoBattery is now deprecated in favour of FollowSystem.",
+        ReplaceWith(
+            "V2_FOLLOW_SYSTEM",
+            "com.edricchan.studybuddy.core.settings.appearance.DarkThemePrefValues.V2_FOLLOW_SYSTEM"
+        )
+    )
+    const val V1_AUTO_BATTERY = "automatic_battery_saver"
     const val V2_ALWAYS = "always"
-    const val V2_AUTO_BATTERY = "automatic_battery_saver"
     const val V2_FOLLOW_SYSTEM = "follow_system"
     const val V2_NEVER = "never"
 }
 
 @Suppress("DEPRECATION")
-enum class DarkThemeValue(val value: String) {
+sealed interface DarkThemeValue {
+    val value: String
+
+    sealed class Version1(override val value: String) : DarkThemeValue
+
+    sealed class Version2(override val value: String) : DarkThemeValue {
+        companion object {
+            // by lazy is needed for https://youtrack.jetbrains.com/issue/KT-59723
+            val entries by lazy {
+                listOf(
+                    V2FollowSystem,
+                    V2Always,
+                    V2Never
+                )
+            }
+
+            fun fromPrefValue(value: String?): Version2 = when (value) {
+                DarkThemePrefValues.V1_ALWAYS, DarkThemePrefValues.V2_ALWAYS -> V2Always
+                DarkThemePrefValues.V2_FOLLOW_SYSTEM -> V2FollowSystem
+                DarkThemePrefValues.V1_NEVER, DarkThemePrefValues.V2_NEVER -> V2Never
+                else -> V2FollowSystem
+            }
+        }
+    }
+
     @Deprecated(
         "This property is kept for backwards-compatibility, " +
             "use V2Always where applicable",
@@ -51,7 +83,7 @@ enum class DarkThemeValue(val value: String) {
             "com.edricchan.studybuddy.core.settings.appearance.DarkThemeValue.V2Always"
         )
     )
-    V1Always(DarkThemePrefValues.V1_ALWAYS),
+    data object V1Always : Version1(DarkThemePrefValues.V1_ALWAYS)
 
     @Deprecated(
         "Automatic switching of dark/light based on the current time is deprecated. " +
@@ -61,7 +93,7 @@ enum class DarkThemeValue(val value: String) {
             "com.edricchan.studybuddy.core.settings.appearance.DarkThemeValue.V2AutoBattery"
         )
     )
-    V1AutoTime(DarkThemePrefValues.V1_AUTO_TIME),
+    data object V1AutoTime : Version1(DarkThemePrefValues.V1_AUTO_TIME)
 
     @Deprecated(
         "This property is kept for backwards-compatibility, " +
@@ -71,19 +103,27 @@ enum class DarkThemeValue(val value: String) {
             "com.edricchan.studybuddy.core.settings.appearance.DarkThemeValue.V2Never"
         )
     )
-    V1Never(DarkThemePrefValues.V1_NEVER),
-    V2Always(DarkThemePrefValues.V2_ALWAYS),
-    V2AutoBattery(DarkThemePrefValues.V2_AUTO_BATTERY),
-    V2FollowSystem(DarkThemePrefValues.V2_FOLLOW_SYSTEM),
-    V2Never(DarkThemePrefValues.V2_NEVER);
+    data object V1Never : Version1(DarkThemePrefValues.V1_NEVER)
+
+    @Deprecated(
+        "AutoBattery is now deprecated in favour of FollowSystem.",
+        ReplaceWith(
+            "V2FollowSystem",
+            "com.edricchan.studybuddy.core.settings.appearance.DarkThemeValue.V2FollowSystem"
+        )
+    )
+    data object V1AutoBattery : Version1(DarkThemePrefValues.V1_AUTO_BATTERY)
+    data object V2Always : Version2(DarkThemePrefValues.V2_ALWAYS)
+    data object V2FollowSystem : Version2(DarkThemePrefValues.V2_FOLLOW_SYSTEM)
+    data object V2Never : Version2(DarkThemePrefValues.V2_NEVER)
 
     companion object {
         fun fromPrefValue(value: String?) = when (value) {
             DarkThemePrefValues.V1_ALWAYS -> V1Always
             DarkThemePrefValues.V1_AUTO_TIME -> V1AutoTime
             DarkThemePrefValues.V1_NEVER -> V1AutoTime
+            DarkThemePrefValues.V1_AUTO_BATTERY -> V1AutoBattery
             DarkThemePrefValues.V2_ALWAYS -> V2Always
-            DarkThemePrefValues.V2_AUTO_BATTERY -> V2AutoBattery
             DarkThemePrefValues.V2_FOLLOW_SYSTEM -> V2FollowSystem
             DarkThemePrefValues.V2_NEVER -> V2Never
             else -> V2FollowSystem
