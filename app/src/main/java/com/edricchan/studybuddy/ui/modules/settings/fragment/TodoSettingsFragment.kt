9a83ee03720aaa26e913e404abbdc9b4cb2be130
Edric <edric.chan.1997@gmail.com>
2024-12-14 15:04:36 +0800

    TodoSettingsFragment: Remove weekly summary setting

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/settings/fragment/TodoSettingsFragment.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/settings/fragment/TodoSettingsFragment.kt
@@ -1,17 +1,11 @@
 package com.edricchan.studybuddy.ui.modules.settings.fragment
 
 import android.os.Bundle
-import androidx.preference.Preference
 import com.edricchan.studybuddy.R
-import com.edricchan.studybuddy.core.settings.tasks.summary.prefWeeklySummaryEnabled
-import com.edricchan.studybuddy.exts.androidx.preference.setFragment
-import com.edricchan.studybuddy.ui.modules.settings.fragment.todo.WeeklySummarySettingsFragment
 import com.edricchan.studybuddy.ui.preference.MaterialPreferenceFragment
 
 class TodoSettingsFragment : MaterialPreferenceFragment() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pref_todos, rootKey)
-        findPreference<Preference>(prefWeeklySummaryEnabled)
-            ?.setFragment<WeeklySummarySettingsFragment>()
     }
 }
--- core/settings/tasks/src/main/kotlin/com/edricchan/studybuddy/core/settings/tasks/summary/SummarySettingsKeys.kt
+++
@@ -1,9 +0,0 @@
-package com.edricchan.studybuddy.core.settings.tasks.summary
-
-// Preferences > Todos > Weekly summary
-const val prefCategoryWeeklySummaryPrefs = "pref_category_weekly_summary_prefs"
-const val prefWeeklySummaryEnabled = "pref_weekly_summary_enabled"
-const val prefWeeklySummaryDay = "pref_weekly_summary_day"
-const val prefWeeklySummaryTime = "pref_weekly_summary_time"
-const val prefWeeklySummaryList = "pref_weekly_summary_list"
-const val prefWeeklySummaryNotificationOpts = "pref_weekly_summary_notification_opts"
