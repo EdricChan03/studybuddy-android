48f248a858af5f025a9efda5fc6807afada8d71d
Edric <edric.chan.1997@gmail.com>
2025-02-23 12:43:38 +0800

    BasePreference: Add preview with action

--- ui/preference/compose/src/main/kotlin/com/edricchan/studybuddy/ui/preference/compose/BasePreference.kt
+++ ui/preference/compose/src/main/kotlin/com/edricchan/studybuddy/ui/preference/compose/BasePreference.kt
@@ -2,26 +2,39 @@ package com.edricchan.studybuddy.ui.preference.compose
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Build
 import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.edricchan.studybuddy.ui.theming.compose.StudyBuddyTheme
 import com.edricchan.studybuddy.ui.widgets.compose.list.withDisabledColors
 
 /** Test tag for the [Preference]'s `actionDivider` composable. */
@@ -303,4 +316,25 @@ fun Preference(
         showActionDivider = showActionDivider,
         enabled = enabled
     )
+}
+
+@Preview
+@Composable
+private fun PreferenceWithActionPreview() {
+    var checked by remember { mutableStateOf(false) }
+    StudyBuddyTheme {
+        Preference(
+            modifier = Modifier.height(IntrinsicSize.Min),
+            title = { Text(text = "Title text") },
+            subtitle = { Text(text = "Subtitle text") },
+            icon = { Icon(Icons.Outlined.Build, contentDescription = null) },
+            showActionDivider = true,
+            action = {
+                Switch(
+                    checked = checked,
+                    onCheckedChange = { checked = it }
+                )
+            }
+        )
+    }
 }
