12d5f3dca1de6dc02ed601f724d648f1a16f6ffc
Edric <edric.chan.1997@gmail.com>
2025-03-15 00:43:37 +0800

    TipsFragment: Inherit from `ViewBindingFragment`

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/tips/fragment/TipsFragment.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/tips/fragment/TipsFragment.kt
@@ -1,31 +1,22 @@
 package com.edricchan.studybuddy.ui.modules.tips.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.view.MenuProvider
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
 import com.edricchan.studybuddy.BuildConfig
 import com.edricchan.studybuddy.R
 import com.edricchan.studybuddy.constants.Constants
 import com.edricchan.studybuddy.core.compat.navigation.navigateToDebug
 import com.edricchan.studybuddy.core.compat.navigation.navigateToSettings
 import com.edricchan.studybuddy.databinding.FragTipsBinding
-import com.edricchan.studybuddy.exts.androidx.viewbinding.viewInflateBinding
+import com.edricchan.studybuddy.ui.common.fragment.ViewBindingFragment
 import com.edricchan.studybuddy.utils.web.launchUri
 
-class TipsFragment : Fragment() {
-    private lateinit var navController: NavController
-
-    private val binding by viewInflateBinding(FragTipsBinding::inflate)
-
-    private val menuProvider = object : MenuProvider {
+class TipsFragment : ViewBindingFragment<FragTipsBinding>(FragTipsBinding::inflate) {
+    override val menuProvider = object : MenuProvider {
         override fun onPrepareMenu(menu: Menu) {
             if (!BuildConfig.DEBUG) {
                 menu.removeItem(R.id.action_debug)
@@ -54,18 +45,8 @@ class TipsFragment : Fragment() {
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = binding.root
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        navController = findNavController()
-
-        activity?.addMenuProvider(menuProvider, viewLifecycleOwner)
 
         binding.tipsEmptyStateCta.setOnClickListener {
             requireContext().launchUri(Constants.uriSubmitTip)
