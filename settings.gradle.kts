627b0a4ead64814f71f9c08461a3cb06f66cb071
Edric <edric.chan.1997@gmail.com>
2025-04-02 21:24:53 +0800

    Bump AGP to 8.11.0-alpha03
    Accompanying release for https://androidstudio.googleblog.com/2025/03/android-studio-narwhal-202511-canary-3.html

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,7 +37,7 @@ plugins {
     // https://docs.gradle.org/8.7/userguide/toolchains.html#sub:download_repositories
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 
-    id("com.android.settings") version "8.10.0-alpha07"
+    id("com.android.settings") version "8.11.0-alpha03"
 }
 
 val isCi = !System.getenv("CI").isNullOrEmpty()
