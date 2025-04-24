17c7b10fa37c8cfee604697d707aba40e6ee4b5b
Edric <edric.chan.1997@gmail.com>
2025-04-24 21:39:22 +0800

    GoogleAuth: Mark as deprecated

--- core/auth/gms/src/main/kotlin/com/edricchan/studybuddy/core/auth/gms/contracts/GoogleAuth.kt
+++ core/auth/gms/src/main/kotlin/com/edricchan/studybuddy/core/auth/gms/contracts/GoogleAuth.kt
@@ -11,6 +11,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 
+@Deprecated("Use the Sign in With Google functionality from androidx.credentials instead")
 class GoogleAuth : ActivityResultContract<GoogleSignInOptions, GoogleSignInAccount?>() {
     override fun createIntent(context: Context, input: GoogleSignInOptions): Intent {
         val gso = GoogleSignIn.getClient(context, input)
