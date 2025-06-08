bcb0ca49fac65bbf3a055122005ec7d5919e278b
Edric <edric.chan.1997@gmail.com>
2025-06-08 14:17:58 +0800

    Typography.kt: Fix typography unit tests

--- ui/theming/compose/src/test/kotlin/com/edricchan/studybuddy/ui/theming/compose/theme/TypographyUnitTest.kt
+++ ui/theming/compose/src/test/kotlin/com/edricchan/studybuddy/ui/theming/compose/theme/TypographyUnitTest.kt
@@ -1,10 +1,48 @@
 package com.edricchan.studybuddy.ui.theming.compose.theme
 
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import io.kotest.core.spec.style.FunSpec
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 class TypographyUnitTest : FunSpec({
-    test("Baloo font is applied to all text-styles") {
-        StudyBuddyTypography shouldUseFontFamily baloo2Family
+    test("Baloo font is applied to all body text-styles") {
+        with(StudyBuddyTypography) {
+            bodyLarge shouldUseFontFamily baloo2Family
+            bodyLargeEmphasized shouldUseFontFamily baloo2Family
+            bodyMedium shouldUseFontFamily baloo2Family
+            bodyMediumEmphasized shouldUseFontFamily baloo2Family
+            bodySmall shouldUseFontFamily baloo2Family
+            bodySmallEmphasized shouldUseFontFamily baloo2Family
+        }
+    }
+
+    test("Funnel Display font is applied to all non-body text-styles") {
+        with(StudyBuddyTypography) {
+            displayLarge shouldUseFontFamily funnelDisplayFamily
+            displayLargeEmphasized shouldUseFontFamily funnelDisplayFamily
+            displayMedium shouldUseFontFamily funnelDisplayFamily
+            displayMediumEmphasized shouldUseFontFamily funnelDisplayFamily
+            displaySmall shouldUseFontFamily funnelDisplayFamily
+            displaySmallEmphasized shouldUseFontFamily funnelDisplayFamily
+            headlineLarge shouldUseFontFamily funnelDisplayFamily
+            headlineLargeEmphasized shouldUseFontFamily funnelDisplayFamily
+            headlineMedium shouldUseFontFamily funnelDisplayFamily
+            headlineMediumEmphasized shouldUseFontFamily funnelDisplayFamily
+            headlineSmall shouldUseFontFamily funnelDisplayFamily
+            headlineSmallEmphasized shouldUseFontFamily funnelDisplayFamily
+            titleLarge shouldUseFontFamily funnelDisplayFamily
+            titleLargeEmphasized shouldUseFontFamily funnelDisplayFamily
+            titleMedium shouldUseFontFamily funnelDisplayFamily
+            titleMediumEmphasized shouldUseFontFamily funnelDisplayFamily
+            titleSmall shouldUseFontFamily funnelDisplayFamily
+            titleSmallEmphasized shouldUseFontFamily funnelDisplayFamily
+            labelLarge shouldUseFontFamily funnelDisplayFamily
+            labelLargeEmphasized shouldUseFontFamily funnelDisplayFamily
+            labelMedium shouldUseFontFamily funnelDisplayFamily
+            labelMediumEmphasized shouldUseFontFamily funnelDisplayFamily
+            labelSmall shouldUseFontFamily funnelDisplayFamily
+            labelSmallEmphasized shouldUseFontFamily funnelDisplayFamily
+        }
     }
 })
 
