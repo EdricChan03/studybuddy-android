6dffb7dd295249e8aa281bb5b9e9fa694c87c429
Edric <edric.chan.1997@gmail.com>
2025-07-21 14:35:46 +0800

    BasePreference.kt: Remove unused `PreferenceListItem` composable

--- ui/preference/compose/src/main/kotlin/com/edricchan/studybuddy/ui/preference/compose/BasePreference.kt
+++ ui/preference/compose/src/main/kotlin/com/edricchan/studybuddy/ui/preference/compose/BasePreference.kt
@@ -13,8 +13,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Build
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
@@ -35,7 +33,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.edricchan.studybuddy.ui.theming.compose.StudyBuddyTheme
-import com.edricchan.studybuddy.ui.widgets.compose.list.withDisabledColors
 
 /** Test tag for the [Preference]'s `actionDivider` composable. */
 const val ActionDividerTestTag = "Preference:ActionDivider"
@@ -175,85 +172,6 @@ private fun PreferenceContent(
         }
     }
 }
-
-/**
- * Simple UI composable to display a setting.
- *
- * Such preferences should ideally follow
- * [AOSP's settings design guidelines](https://source.android.com/docs/core/settings/settings-guidelines).
- *
- * Variants should be used where preferable:
- * * [CheckboxPreference] for displaying a preference with a
- * [androidx.compose.material3.Checkbox], representing a checkable state.
- * * [SwitchPreference] for displaying a preference with a
- * [androidx.compose.material3.Switch], representing a checkable state.
- * * [ListDialogPreference] for displaying a list of options in a dialog when clicked.
- * * [InputDialogPreference] for displaying an input dialog when clicked.
- * * [MainSwitchBar] if a root switch preference bar should be used,
- * [emulating AOSP's master setting](https://source.android.com/docs/core/settings/settings-guidelines#master_setting).
- * * [PreferenceCategory] for grouping a list of [Preference]s.
- * @param enabled Whether this preference is enabled. If disabled, clickable behaviours
- * should not have any effect.
- * @param icon [Composable] to be displayed at the start. This should be an
- * [androidx.compose.material3.Icon].
- * @param title [Composable] to be displayed in the middle. This should contain the
- * title of the preference.
- * @param subtitle (Optional) [Composable] to be displayed before the [title].
- * This should contain additional information.
- * @param action (Optional) [Composable] to be displayed at the end. An [showActionDivider]
- * can be optionally shown between it and its contents.
- * @param showActionDivider Whether a vertical divider should be shown between the [action]
- * and its contents.
- * @see CheckboxPreference
- * @see SwitchPreference
- * @see DialogPreference
- * @see ListDialogPreference
- * @see PreferenceCategory
- * @see MainSwitchBar
- */
-@Composable
-fun PreferenceListItem(
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    icon: (@Composable () -> Unit)? = null,
-    title: @Composable () -> Unit,
-    subtitle: (@Composable () -> Unit)? = null,
-    action: (@Composable () -> Unit)? = null,
-    showActionDivider: Boolean = false
-) = ListItem(
-    modifier = modifier,
-    colors = ListItemDefaults.colors().withDisabledColors(enabled),
-    headlineContent = title,
-    supportingContent = subtitle,
-    leadingContent = icon,
-    trailingContent = action?.let {
-        {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                if (showActionDivider) {
-                    val color = DividerDefaults.color.copy(
-                        alpha = if (enabled) {
-                            1f
-                        } else {
-                            0.6f
-                        },
-                    )
-                    VerticalDivider(
-                        color = color,
-                        modifier = Modifier
-                            .padding(vertical = 4.dp)
-                            .fillMaxHeight()
-                            .width(1.dp)
-                            .testTag(ActionDividerTestTag),
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
-                }
-                action()
-            }
-        }
-    },
-)
 
 /**
  * Simple UI composable to display a setting.
