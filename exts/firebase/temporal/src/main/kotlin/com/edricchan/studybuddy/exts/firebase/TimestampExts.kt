a5e191874caf45f48bacf48ac0ebe07b79645da4
Edric <edric.chan.1997@gmail.com>
2025-07-31 15:25:43 +0800

    TimestampExts.kt: Add explicit return types

--- exts/firebase/temporal/src/main/kotlin/com/edricchan/studybuddy/exts/firebase/TimestampExts.kt
+++ exts/firebase/temporal/src/main/kotlin/com/edricchan/studybuddy/exts/firebase/TimestampExts.kt
@@ -10,7 +10,7 @@ import java.time.format.DateTimeFormatter
 
 /** Formats the [Timestamp] using the given [formatter]. */
 @SuppressLint("NewApi") // Provided by core library desugaring
-fun Timestamp.format(formatter: DateTimeFormatter) = toInstant().format(formatter)
+fun Timestamp.format(formatter: DateTimeFormatter): String = toInstant().format(formatter)
 
 /**
  * Formats the [Timestamp] using the given [pattern] and configures
@@ -19,14 +19,15 @@ fun Timestamp.format(formatter: DateTimeFormatter) = toInstant().format(formatte
 @SuppressLint("NewApi") // Provided by core library desugaring
 fun Timestamp.format(
     pattern: String, formatterInit: DateTimeFormatter.() -> Unit = {}
-) = toInstant().format(pattern, formatterInit)
+): String = toInstant().format(pattern, formatterInit)
 
 /** Converts a [Timestamp] to a [LocalDateTime]. */
 fun Timestamp.toLocalDateTime(): LocalDateTime =
     LocalDateTime.ofEpochSecond(seconds, nanoseconds, ZoneOffset.UTC)
 
 /** Converts a [LocalDateTime] to a [Timestamp]. */
-fun LocalDateTime.toTimestamp() = Timestamp(second.toLong(), nano)
+fun LocalDateTime.toTimestamp(): Timestamp = Timestamp(second.toLong(), nano)
 
 /** Converts an [Instant] to a [Timestamp]. */
-fun Instant.toTimestamp() = Timestamp(epochSecond, nano)
+@SuppressLint("NewApi") // Provided by core library desugaring
+fun Instant.toTimestamp(): Timestamp = Timestamp(this)
