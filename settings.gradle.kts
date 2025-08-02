b0aebc5bd6fb883f817981f74e04fd93b7e4a02b
Edric <edric.chan.1997@gmail.com>
2025-08-02 23:26:45 +0800

    Bump AGP to 8.12.0
    Accompanying release for https://androidstudio.googleblog.com/2025/07/android-studio-narwhal-feature-drop_31.html

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     id("com.gradle.develocity") version "4.1"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 
-    id("com.android.settings") version "8.12.0-rc01"
+    id("com.android.settings") version "8.12.0"
 }
 
 val isCi = !System.getenv("CI").isNullOrEmpty()
