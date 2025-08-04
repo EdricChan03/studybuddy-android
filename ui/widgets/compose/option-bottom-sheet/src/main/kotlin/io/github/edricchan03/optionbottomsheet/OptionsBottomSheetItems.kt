a2ef787d6ffa7910ceaaf508cb9e0f338910fc6a
Edric <edric.chan.1997@gmail.com>
2025-08-05 02:16:46 +0800

    OptionsBottomSheetItems: Add missing import

--- ui/widgets/compose/option-bottom-sheet/src/main/kotlin/io/github/edricchan03/optionbottomsheet/OptionsBottomSheetItems.kt
+++ ui/widgets/compose/option-bottom-sheet/src/main/kotlin/io/github/edricchan03/optionbottomsheet/OptionsBottomSheetItems.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
