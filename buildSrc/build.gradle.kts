8dd9602f0620ffd19437e9a219d04cd4f996b232
Edric <edric.chan.1997@gmail.com>
2025-08-14 11:51:14 +0800

    Use AGP common dependency rather than the Gradle plugin dep

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,8 @@ private fun Provider<PluginDependency>.text() =
 dependencies {
     // Used for the custom reporter
     implementation(libs.gradleVersions.gradle)
-    implementation(libs.android.gradle.library)
+    // For precompiled scripts
+    implementation(libs.android.gradle)
     // See https://github.com/google/dagger/issues/3068
     implementation("com.squareup:javapoet:1.13.0") {
         because("AGP DataBinding uses an older version of Javapoet which breaks Hilt")
