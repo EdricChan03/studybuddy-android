27e282e5c7873992b5934ad0c1b26233f26e5534
Edric <edric.chan.1997@gmail.com>
2025-09-15 19:27:39 +0800

    DebugViewModel: Apply `@param` use-site target to `ApplicationContext` annotation

--- app/src/main/java/com/edricchan/studybuddy/ui/modules/debug/vm/DebugViewModel.kt
+++ app/src/main/java/com/edricchan/studybuddy/ui/modules/debug/vm/DebugViewModel.kt
@@ -16,7 +16,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DebugViewModel @Inject constructor(
-    @ApplicationContext private val context: Context
+    @param:ApplicationContext private val context: Context
 ) : ViewModel() {
     companion object {
         private const val INSTANT_UNSET = -1L
