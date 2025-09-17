e30c18648e202f69981de9d1a907fb111b44e568
Edric <edric.chan.1997@gmail.com>
2025-09-17 18:39:21 +0800

    [DNM] Reproduce no dependency substitution configuration

--- buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/common.gradle.kts
+++ buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/common.gradle.kts
@@ -1,21 +1,2 @@
 /** Convention script for all modules. */
 package com.edricchan.studybuddy.library
-
-private val commonMarkVersion =
-    versionCatalogs.named("libs")
-        .findVersion("commonmark")
-        .map { it.requiredVersion }.get()
-
-// Fix for https://github.com/noties/Markwon/issues/341
-configurations.configureEach {
-    resolutionStrategy.dependencySubstitution.all {
-        requested.let {
-            if (it is ModuleComponentSelector && it.group == "com.atlassian.commonmark") {
-                useTarget(
-                    "org.commonmark:${it.module}:$commonMarkVersion",
-                    "CommonMark has moved its groupId from com.atlassian to org.commonmark"
-                )
-            }
-        }
-    }
-}
