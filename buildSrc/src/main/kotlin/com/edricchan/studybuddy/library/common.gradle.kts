b015b4fc722ec4c24b78776098cc6510eea72c84
Edric <edric.chan.1997@gmail.com>
2025-09-20 13:39:16 +0800

    [DNM] Attempt fix for Gradle configuration resolution issue

--- buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/common.gradle.kts
+++ buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/common.gradle.kts
@@ -1,2 +1,23 @@
 /** Convention script for all modules. */
 package com.edricchan.studybuddy.library
+
+private val commonMarkVersion =
+    versionCatalogs.named("libs")
+        .findVersion("commonmark")
+        .map { it.requiredVersion }.get()
+
+// Fix for https://github.com/noties/Markwon/issues/341
+configurations.asMap.values
+    .filter { it.dependencies.any { dependency -> dependency.group == "com.atlassian.commonmark" } }
+    .forEach {
+        it.resolutionStrategy.dependencySubstitution.all {
+            requested.let {
+                if (it is ModuleComponentSelector && it.group == "com.atlassian.commonmark") {
+                    useTarget(
+                        "org.commonmark:${it.module}:$commonMarkVersion",
+                        "CommonMark has moved its groupId from com.atlassian to org.commonmark"
+                    )
+                }
+            }
+        }
+    }
