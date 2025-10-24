36cc8b42eeeaabf4621b466d69eaeedcd99ed6ad
Edric <edric.chan.1997@gmail.com>
2025-10-24 12:14:52 +0800

    LicensesContainer.kt: Re-import `plus` extension function
    See previous commit for more info

--- ui/common/src/main/kotlin/com/edricchan/studybuddy/ui/common/licenses/LicensesContainer.kt
+++ ui/common/src/main/kotlin/com/edricchan/studybuddy/ui/common/licenses/LicensesContainer.kt
@@ -66,6 +66,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.edricchan.studybuddy.core.resources.icons.AppIcons
 import com.edricchan.studybuddy.core.resources.icons.outlined.Code
 import com.edricchan.studybuddy.core.resources.icons.outlined.Link
+import com.edricchan.studybuddy.exts.androidx.compose.plus
 import com.edricchan.studybuddy.ui.common.R
 import com.edricchan.studybuddy.ui.theming.compose.StudyBuddyTheme
 import com.edricchan.studybuddy.ui.widgets.compose.IconButtonWithTooltip
