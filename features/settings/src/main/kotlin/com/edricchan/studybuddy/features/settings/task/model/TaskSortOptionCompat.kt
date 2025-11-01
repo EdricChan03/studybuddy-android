7c48ff3f24f5efd63f895d7540aeb7af352082ba
Edric <edric.chan.1997@gmail.com>
2025-11-01 13:28:55 +0800

    TaskSortOptionCompat: Use `TodoSortValues` as the source of truth for values

--- features/settings/src/main/kotlin/com/edricchan/studybuddy/features/settings/task/model/TaskSortOptionCompat.kt
+++ features/settings/src/main/kotlin/com/edricchan/studybuddy/features/settings/task/model/TaskSortOptionCompat.kt
@@ -2,6 +2,7 @@ package com.edricchan.studybuddy.features.settings.task.model
 
 import androidx.annotation.StringRes
 import com.edricchan.studybuddy.core.settings.tasks.resources.R
+import com.edricchan.studybuddy.features.tasks.constants.sharedprefs.TodoOptionsPrefConstants.TodoSortValues
 
 enum class TaskSortOptionCompat(
     @field:StringRes val stringResource: Int,
@@ -9,23 +10,23 @@ enum class TaskSortOptionCompat(
 ) {
     None(
         stringResource = R.string.pref_task_default_sort_entry_none,
-        value = "none"
+        value = TodoSortValues.NONE
     ),
     TitleAsc(
         stringResource = R.string.pref_task_default_sort_entry_title_asc,
-        value = "title_asc"
+        value = TodoSortValues.TITLE_ASC
     ),
     TitleDesc(
         stringResource = R.string.pref_task_default_sort_entry_title_desc,
-        value = "title_desc"
+        value = TodoSortValues.TITLE_DESC
     ),
     DueDateNewToOld(
         stringResource = R.string.pref_task_default_sort_entry_due_date_new_to_old,
-        value = "due_date_new_to_old"
+        value = TodoSortValues.DUE_DATE_NEW_TO_OLD
     ),
     DueDateOldToNew(
         stringResource = R.string.pref_task_default_sort_entry_due_date_old_to_new,
-        value = "due_date_old_to_new"
+        value = TodoSortValues.DUE_DATE_OLD_TO_NEW
     );
 
     companion object {
