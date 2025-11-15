22cefed6b99e70a0ad387d923538721899e2a945
Edric <edric.chan.1997@gmail.com>
2025-11-15 16:34:42 +0800

    TaskMapper.kt: Add mappers for `Field` enum entries

--- features/tasks/data/src/main/kotlin/com/edricchan/studybuddy/features/tasks/data/mapper/TaskMapper.kt
+++ features/tasks/data/src/main/kotlin/com/edricchan/studybuddy/features/tasks/data/mapper/TaskMapper.kt
@@ -57,3 +57,27 @@ fun TodoProject.toDomain(): TaskProject = TaskProject(
     createdAt = createdAt?.toInstant() ?: Instant.now(),
     lastModified = lastModified?.toInstant() ?: Instant.now()
 )
+
+fun TodoItem.Field.toDomain(): TaskItem.Field = when (this) {
+    TodoItem.Field.Title -> TaskItem.Field.Title
+    TodoItem.Field.Content -> TaskItem.Field.Content
+    TodoItem.Field.DueDate -> TaskItem.Field.DueDate
+    TodoItem.Field.Tags -> TaskItem.Field.Tags
+    TodoItem.Field.Project -> TaskItem.Field.Project
+    TodoItem.Field.IsDone -> TaskItem.Field.IsCompleted
+    TodoItem.Field.IsArchived -> TaskItem.Field.IsArchived
+    TodoItem.Field.CreatedAt -> TaskItem.Field.CreatedAt
+    TodoItem.Field.LastModified -> TaskItem.Field.LastModified
+}
+
+fun TaskItem.Field.toDto(): TodoItem.Field = when (this) {
+    TaskItem.Field.Title -> TodoItem.Field.Title
+    TaskItem.Field.Content -> TodoItem.Field.Content
+    TaskItem.Field.DueDate -> TodoItem.Field.DueDate
+    TaskItem.Field.Tags -> TodoItem.Field.Tags
+    TaskItem.Field.Project -> TodoItem.Field.Project
+    TaskItem.Field.IsCompleted -> TodoItem.Field.IsDone
+    TaskItem.Field.IsArchived -> TodoItem.Field.IsArchived
+    TaskItem.Field.CreatedAt -> TodoItem.Field.CreatedAt
+    TaskItem.Field.LastModified -> TodoItem.Field.LastModified
+}
