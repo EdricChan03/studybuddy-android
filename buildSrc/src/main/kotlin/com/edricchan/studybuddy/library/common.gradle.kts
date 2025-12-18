04ebb1530fcd0c85e456bd1016376a6ae9039ac5
Edric <edric.chan.1997@gmail.com>
2025-12-18 18:18:19 +0800

    Bump Kotlin from 2.2.21 to 2.3.0
    Add workaround for https://github.com/google/dagger/issues/5059 for now

--- buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/common.gradle.kts
+++ buildSrc/src/main/kotlin/com/edricchan/studybuddy/library/common.gradle.kts
@@ -1,4 +1,8 @@
 /** Convention script for all modules. */
 package com.edricchan.studybuddy.library
 
-// Empty for now
+// TODO: Remove when Dagger bumps Kotlin metadata dependency to 2.3.0
+//  (see also https://github.com/google/dagger/issues/5059)
+configurations.configureEach {
+    resolutionStrategy.force("org.jetbrains.kotlin:kotlin-metadata-jvm:2.3.0")
+}
