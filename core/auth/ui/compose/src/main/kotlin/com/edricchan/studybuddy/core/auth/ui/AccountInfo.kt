6dd14da3703d97bdb2085442d5d2526060163a33
Edric <edric.chan.1997@gmail.com>
2026-01-24 17:50:39 +0800

    AccountInfo.kt: Fix styling when only an email address is present
    Previously, the email address would always
    be shown as a subtitle text even if there's
    no display name
    
    * Also remove elvis logic for profile image
    * Extract new metadata logic to separate
      composable
    * Use `User` preview parameter provider to
      verify new logic

--- core/auth/ui/compose/src/main/kotlin/com/edricchan/studybuddy/core/auth/ui/AccountInfo.kt
+++ core/auth/ui/compose/src/main/kotlin/com/edricchan/studybuddy/core/auth/ui/AccountInfo.kt
@@ -5,11 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,9 +13,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.edricchan.studybuddy.core.auth.common.R
 import com.edricchan.studybuddy.core.auth.model.User
+import com.edricchan.studybuddy.core.auth.preview.UserPreviewParameterProvider
 import com.edricchan.studybuddy.ui.theming.compose.StudyBuddyTheme
 
 @Composable
@@ -38,27 +36,83 @@ fun AccountInfoRow(
 }
 
 @Composable
+private fun AccountInfoMetadataTitleText(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        style = MaterialTheme.typography.titleMedium
+    )
+}
+
+@Composable
+private fun AccountInfoMetadataSubtitleText(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        style = MaterialTheme.typography.titleSmall
+    )
+}
+
+@Composable
+private fun AccountInfoMetadata(
+    modifier: Modifier = Modifier,
+    displayName: String?,
+    email: String?
+) = Column(modifier = modifier) {
+    when {
+        !email.isNullOrBlank() && !displayName.isNullOrBlank() -> {
+            AccountInfoMetadataTitleText(
+                text = displayName
+            )
+            AccountInfoMetadataSubtitleText(
+                text = email
+            )
+        }
+
+        // Show just the email address as title text if we don't have a display name present
+        !email.isNullOrBlank() -> {
+            AccountInfoMetadataTitleText(
+                text = email
+            )
+        }
+
+        // Show just the display name as title text if there's no email address
+        !displayName.isNullOrBlank() -> {
+            AccountInfoMetadataTitleText(
+                text = displayName
+            )
+        }
+
+        else -> {
+            AccountInfoMetadataTitleText(
+                text = stringResource(R.string.account_anon_display_name)
+            )
+        }
+    }
+}
+
+@Composable
 fun AccountInfoRow(
     modifier: Modifier = Modifier,
-    displayName: String,
+    displayName: String?,
     email: String?,
     photoUri: Uri?,
 ) = AccountInfoRow(
     modifier = modifier,
     profileImage = {
-        photoUri?.let {
-            ProfileImage(displayName = displayName, photoUri = it)
-        } ?: Icon(
-            modifier = Modifier.fillMaxHeight(),
-            imageVector = Icons.Outlined.AccountCircle,
-            contentDescription = stringResource(R.string.account_profile_content_desc, displayName)
-        )
+        ProfileImage(displayName = displayName, photoUri = photoUri)
     },
     metadata = {
-        Column {
-            Text(text = displayName, style = MaterialTheme.typography.titleMedium)
-            email?.let { Text(text = it, style = MaterialTheme.typography.titleSmall) }
-        }
+        AccountInfoMetadata(
+            displayName = displayName,
+            email = email
+        )
     }
 )
 
@@ -68,20 +122,18 @@ fun AccountInfoRow(
     user: User
 ) = AccountInfoRow(
     modifier = modifier,
-    displayName = user.displayName ?: stringResource(R.string.account_anon_display_name),
+    displayName = user.displayName,
     email = user.email,
     photoUri = user.photoUri
 )
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-private fun AccountInfoRowPreview() {
+private fun AccountInfoRowPreview(@PreviewParameter(UserPreviewParameterProvider::class) user: User) {
     StudyBuddyTheme {
         AccountInfoRow(
             modifier = Modifier.padding(16.dp),
-            displayName = "John Appleseed",
-            email = "example@example.com",
-            photoUri = null
+            user = user
         )
     }
 }
