4cd3643df11d3abd86138ec98145d2fd0cef0285
Edric <edric.chan.1997@gmail.com>
2026-02-09 10:41:44 +0800

    BuildVersionPreconditions.kt: Add `ifApi` variant that returns `null` if false

--- utils/android/src/main/kotlin/com/edricchan/studybuddy/utils/android/BuildVersionPreconditions.kt
+++ utils/android/src/main/kotlin/com/edricchan/studybuddy/utils/android/BuildVersionPreconditions.kt
@@ -31,3 +31,16 @@ inline fun <T> ifApi(value: Int, ifTrue: () -> T, ifFalse: () -> T): T {
         ifTrue()
     } else ifFalse()
 }
+
+/**
+ * Returns the called value of [ifTrue] if the current device's [Build.VERSION.SDK_INT] is higher or
+ * equal to the specified [value], or `null`.
+ * @param value Desired [Build.VERSION_CODES] value.
+ * @param ifTrue Lambda to be called if `Build.VERSION.SDK_INT >= value` returns `true`, with the
+ * desired value to be used.
+ * @return The result of [ifTrue] or `null` depending upon the result of
+ * `Build.VERSION.SDK_INT >= value`.
+ */
+@ChecksSdkIntAtLeast(parameter = 0, lambda = 1)
+inline fun <T : Any> ifApiOrNull(value: Int, ifTrue: () -> T): T? =
+    ifApi(value = value, ifTrue = ifTrue, ifFalse = { null })
