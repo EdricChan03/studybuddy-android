3ff3c6feb7b94e5a715dd27813f76d1f64e9c0df
Edric <edric.chan.1997@gmail.com>
2026-03-30 14:22:53 +0800

    StudyBuddyThemeWrapper.kt: Rename to add Provider prefix

--- ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/theme/StudyBuddyThemeWrapperProvider.kt
+++ ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/theme/StudyBuddyThemeWrapperProvider.kt
@@ -1,11 +1,11 @@
 package com.edricchan.studybuddy.ui.theming.compose.theme
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.PreviewWrapper
+import androidx.compose.ui.tooling.preview.PreviewWrapperProvider
 import com.edricchan.studybuddy.ui.theming.compose.StudyBuddyTheme
 
-/** [PreviewWrapper] which wraps the content in the [StudyBuddyTheme] composable. */
-class StudyBuddyThemeWrapper : PreviewWrapper {
+/** [PreviewWrapperProvider] which wraps the content in the [StudyBuddyTheme] composable. */
+class StudyBuddyThemeWrapperProvider : PreviewWrapperProvider {
     @Composable
     override fun Wrap(content: @Composable (() -> Unit)) {
         StudyBuddyTheme(content = content)
