14ae81ed47fd013c2205733adb151cf1234628c1
Edric <edric.chan.1997@gmail.com>
2026-03-31 18:21:11 +0800

    AppTemporalFormat: Use system default DateTimeFormatter instead
    * Add top-level `AppDateTimeFormatter`
      property
    * Add `appFormat` overload which takes no
      args
      * Deprecate existing overload

--- core/resources/temporal/src/main/kotlin/com/edricchan/studybuddy/core/resources/temporal/AppTemporalFormat.kt
+++ core/resources/temporal/src/main/kotlin/com/edricchan/studybuddy/core/resources/temporal/AppTemporalFormat.kt
@@ -3,18 +3,39 @@ package com.edricchan.studybuddy.core.resources.temporal
 import android.content.Context
 import com.edricchan.studybuddy.exts.datetime.format
 import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.time.temporal.TemporalAccessor
 import com.edricchan.studybuddy.core.resources.R as CoreResR
+
+/** The application default [DateTimeFormatter]. */
+val AppDateTimeFormatter: DateTimeFormatter =
+    DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
 
 /**
  * The application default [DateTimeFormatter].
  * @see CoreResR.string.java_time_format_pattern_default
  * @see appFormat
  */
-val Context.appDateTimeFormatter: DateTimeFormatter
-    get() = DateTimeFormatter.ofPattern(
-        getString(CoreResR.string.java_time_format_pattern_default)
+@Deprecated(
+    "Use AppDateTimeFormatter directly",
+    ReplaceWith(
+        "AppDateTimeFormatter",
+        "com.edricchan.studybuddy.core.resources.temporal.AppDateTimeFormatter"
     )
+)
+@Suppress("UnusedReceiverParameter")
+val Context.appDateTimeFormatter: DateTimeFormatter
+    get() = AppDateTimeFormatter
+
+/**
+ * Formats the receiver [TemporalAccessor] using the application-default
+ * format pattern.
+ * @return The formatted [TemporalAccessor] as specified by
+ * [appDateTimeFormatter].
+ * @see CoreResR.string.java_time_format_pattern_default
+ * @see appDateTimeFormatter
+ */
+fun TemporalAccessor.appFormat() = format(AppDateTimeFormatter)
 
 /**
  * Formats the receiver [TemporalAccessor] using the application-default
@@ -25,5 +46,8 @@ val Context.appDateTimeFormatter: DateTimeFormatter
  * @see CoreResR.string.java_time_format_pattern_default
  * @see appDateTimeFormatter
  */
-fun TemporalAccessor.appFormat(context: Context) =
-    format(context.appDateTimeFormatter)
+@Deprecated(
+    "The context parameter is no longer required - use the overload with no arguments",
+    ReplaceWith("this.appFormat()")
+)
+fun TemporalAccessor.appFormat(context: Context) = appFormat()
