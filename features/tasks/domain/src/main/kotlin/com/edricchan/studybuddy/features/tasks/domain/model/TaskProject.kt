900ebafe3866d8b6e96bd80d1dd307cef61b5eea
Edric <edric.chan.1997@gmail.com>
2026-04-26 13:53:36 +0800

    TaskProject: Add Field + FieldValue classes

--- features/tasks/domain/src/main/kotlin/com/edricchan/studybuddy/features/tasks/domain/model/TaskProject.kt
+++ features/tasks/domain/src/main/kotlin/com/edricchan/studybuddy/features/tasks/domain/model/TaskProject.kt
@@ -1,9 +1,9 @@
 package com.edricchan.studybuddy.features.tasks.domain.model
 
-import com.edricchan.studybuddy.data.common.Color
 import com.edricchan.studybuddy.data.common.HasId
 import com.edricchan.studybuddy.domain.common.WithTimestampMetadata
 import java.time.Instant
+import com.edricchan.studybuddy.data.common.Color as ColorValue
 
 /**
  * Specifies a project that a [TaskItem] is assigned to.
@@ -13,7 +13,29 @@ import java.time.Instant
 data class TaskProject(
     override val id: String,
     val name: String,
-    val color: Color? = null,
+    val color: ColorValue? = null,
     override val createdAt: Instant,
     override val lastModified: Instant
-) : HasId, WithTimestampMetadata
+) : HasId, WithTimestampMetadata {
+    enum class Field {
+        Name,
+        Color,
+        CreatedAt,
+        LastModified
+    }
+
+    sealed class FieldValue<T>(
+        val field: Field,
+        open val value: T
+    ) {
+        data class Name(override val value: String) : FieldValue<String>(
+            field = Field.Name,
+            value = value
+        )
+
+        data class Color(override val value: ColorValue?) : FieldValue<ColorValue?>(
+            field = Field.Color,
+            value = value
+        )
+    }
+}
