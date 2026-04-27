fdcb92d038086077f1833e09d7b310f5365e8452
Edric <edric.chan.1997@gmail.com>
2026-04-27 18:44:46 +0800

    TaskProjectField.kt: Add previews

--- features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/ui/fields/TaskProjectField.kt
+++ features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/ui/fields/TaskProjectField.kt
@@ -3,19 +3,26 @@ package com.edricchan.studybuddy.features.tasks.ui.fields
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialShapes
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewWrapper
 import androidx.compose.ui.unit.dp
 import com.edricchan.studybuddy.core.resources.icons.AppIcons
 import com.edricchan.studybuddy.core.resources.icons.outlined.Assignment
+import com.edricchan.studybuddy.ui.theming.compose.theme.preview.StudyBuddyThemeWrapperProvider
 
 @Composable
 fun TaskProjectIconSurface(
@@ -47,4 +54,26 @@ object TaskProjectFieldDefaults {
             contentDescription = contentDescription
         )
     }
+}
+
+@PreviewWrapper(StudyBuddyThemeWrapperProvider::class)
+@Preview
+@Composable
+private fun TaskProjectIconSurfacePreview() {
+    TaskProjectIconSurface(color = null)
+}
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@PreviewWrapper(StudyBuddyThemeWrapperProvider::class)
+@Preview
+@Composable
+private fun TaskProjectIconSurfaceListItemPreview() {
+    ListItem(
+        onClick = {},
+        content = { Text(text = "StudyBuddy Compose rewrite") },
+        trailingContent = {
+            TaskProjectIconSurface(color = null)
+        },
+        shapes = ListItemDefaults.shapes()
+    )
 }
