96ff21a976ff8d79117faaa4dcf48b4b4b67003a
Edric <edric.chan.1997@gmail.com>
2026-05-19 09:19:35 +0800

    CreateTaskProjectInput: Mark as serializable

--- features/tasks/domain/src/main/kotlin/com/edricchan/studybuddy/features/tasks/domain/model/create/CreateTaskProjectInput.kt
+++ features/tasks/domain/src/main/kotlin/com/edricchan/studybuddy/features/tasks/domain/model/create/CreateTaskProjectInput.kt
@@ -1,7 +1,9 @@
 package com.edricchan.studybuddy.features.tasks.domain.model.create
 
 import com.edricchan.studybuddy.data.common.Color
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class CreateTaskProjectInput(
     val name: String,
     val color: Color?,
