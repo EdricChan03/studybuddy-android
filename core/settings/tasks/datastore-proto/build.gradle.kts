e94e2ab5ae88ebc04646eeb399d60c46abba899a
Edric <edric.chan.1997@gmail.com>
2026-05-22 16:07:37 +0800

    Convert tasks-related proto modules to pure JVM-based proto modules
    * Also declare :features:tasks:domain-proto
      as a proto library

--- core/settings/tasks/datastore-proto/build.gradle.kts
+++ core/settings/tasks/datastore-proto/build.gradle.kts
@@ -1,23 +1,5 @@
 plugins {
-    com.edricchan.studybuddy.library.`android-protobuf`
-}
-
-android {
-    namespace = "com.edricchan.studybuddy.core.settings.tasks.proto"
-
-    defaultConfig {
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+    com.edricchan.studybuddy.library.`jvm-protobuf`
 }
 
 dependencies {
--- features/tasks/domain-proto/build.gradle.kts
+++ features/tasks/domain-proto/build.gradle.kts
@@ -1,23 +1,9 @@
 plugins {
-    com.edricchan.studybuddy.library.`android-protobuf`
+    com.edricchan.studybuddy.library.`jvm-protobuf`
 }
 
-android {
-    namespace = "com.edricchan.studybuddy.features.tasks.domain.proto"
-
-    defaultConfig {
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+wire {
+    protoLibrary = true
 }
 
 dependencies {
