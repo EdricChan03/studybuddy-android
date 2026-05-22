924310e8f90f2ada46160a995261458a308c34b6
Edric <edric.chan.1997@gmail.com>
2026-05-22 15:37:26 +0800

    Add ProtoBuf messages for task filtering options
    * Add :features:tasks:domain-proto
      dependency to
      :core:settings:tasks:datastore-proto

--- core/settings/tasks/datastore-proto/build.gradle.kts
+++ core/settings/tasks/datastore-proto/build.gradle.kts
@@ -19,3 +19,10 @@ android {
         }
     }
 }
+
+dependencies {
+    // protoSource would include :features:tasks:domain-proto's sources in this library, which would
+    // result in a build-time error where the SortDirection enum is generated twice
+    protoPath(projects.features.tasks.domainProto)
+    api(projects.features.tasks.domainProto)
+}
