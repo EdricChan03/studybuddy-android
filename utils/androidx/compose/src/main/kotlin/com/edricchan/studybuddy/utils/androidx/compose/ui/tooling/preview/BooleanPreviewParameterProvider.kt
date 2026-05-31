fbe751b32df3bb6e97f06fe6d0b9eb7dab86250c
Edric <edric.chan.1997@gmail.com>
2026-06-01 00:25:43 +0800

    BooleanPreviewParameterProvider.kt: Fix broken Compose preview rendering
    Seems having other methods breaks the
    renderer?
    
    * Mark classes as final
    * Remove `getDisplayName` custom method

--- utils/androidx/compose/src/main/kotlin/com/edricchan/studybuddy/utils/androidx/compose/ui/tooling/preview/BooleanPreviewParameterProvider.kt
+++ utils/androidx/compose/src/main/kotlin/com/edricchan/studybuddy/utils/androidx/compose/ui/tooling/preview/BooleanPreviewParameterProvider.kt
@@ -3,26 +3,16 @@ package com.edricchan.studybuddy.utils.androidx.compose.ui.tooling.preview
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
 
 private val booleanValues = listOf(
-    false, true
+    true, false
 )
 
 /**
  * [androidx.compose.ui.tooling.preview.PreviewParameterProvider] that provides a list of preset
  * [Boolean] values.
  */
-open class BooleanPreviewParameterProvider : CollectionPreviewParameterProvider<Boolean>(
+class BooleanPreviewParameterProvider : CollectionPreviewParameterProvider<Boolean>(
     booleanValues
-) {
-    /**
-     * Returns the display name for the given [value].
-     * @see androidx.compose.ui.tooling.preview.PreviewParameterProvider.getDisplayName
-     */
-    open fun getDisplayName(value: Boolean): String? = null
-
-    final override fun getDisplayName(index: Int): String? {
-        return getDisplayName(value = booleanValues[index])
-    }
-}
+)
 
 private val nullableBooleanValues = booleanValues + null
 
@@ -30,16 +20,6 @@ private val nullableBooleanValues = booleanValues + null
  * [androidx.compose.ui.tooling.preview.PreviewParameterProvider] that provides a list of preset
  * [Boolean] values, including `null`.
  */
-open class NullableBooleanPreviewParameterProvider : CollectionPreviewParameterProvider<Boolean?>(
+class NullableBooleanPreviewParameterProvider : CollectionPreviewParameterProvider<Boolean?>(
     nullableBooleanValues
-) {
-    /**
-     * Returns the display name for the given [value].
-     * @see androidx.compose.ui.tooling.preview.PreviewParameterProvider.getDisplayName
-     */
-    open fun getDisplayName(value: Boolean?): String? = null
-
-    final override fun getDisplayName(index: Int): String? {
-        return getDisplayName(value = nullableBooleanValues[index])
-    }
-}
+)
