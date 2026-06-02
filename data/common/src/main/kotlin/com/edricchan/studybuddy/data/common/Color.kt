990078c84635c80c72f25c44956d36f6bc242a03
Edric <edric.chan.1997@gmail.com>
2026-06-02 15:21:23 +0800

    Color.kt: Add utils to create a colour from HSL/HSV
    Shamelessly stolen from Compose's Color.kt
    source code
    
    * Fix KDoc references

--- data/common/src/main/kotlin/com/edricchan/studybuddy/data/common/Color.kt
+++ data/common/src/main/kotlin/com/edricchan/studybuddy/data/common/Color.kt
@@ -2,12 +2,17 @@ package com.edricchan.studybuddy.data.common
 
 import androidx.annotation.ColorInt
 import androidx.annotation.Discouraged
+import androidx.annotation.FloatRange
 import androidx.annotation.IntRange
 import androidx.core.graphics.alpha
 import androidx.core.graphics.blue
 import androidx.core.graphics.green
 import androidx.core.graphics.red
 import kotlinx.serialization.Serializable
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.roundToInt
+import kotlin.random.Random
 
 private fun getWebHexFormat(
     withSignPrefix: Boolean = true,
@@ -105,6 +110,72 @@ value class Color(
         "#%06X".format(value and 0xFFFFFF)
 
     companion object {
+        //#region HSV/HSL operations
+        // Code shamelessly stolen from Compose's Color.kt
+        /**
+         * Return a [Color] from [hue], [saturation], and [value] (HSV representation).
+         *
+         * @param hue The colour value in the range (0..360), where 0 is red, 120 is green, and 240
+         *   is blue
+         * @param saturation The amount of [hue] represented in the colour in the range (0..1), where
+         *   0 has no colour and 1 is fully saturated.
+         * @param alpha Alpha channel to apply to the computed colour
+         * @param value The strength of the colour, where 0 is black.
+         */
+        fun hsv(
+            hue: Float,
+            saturation: Float,
+            value: Float,
+            alpha: Float = 1f,
+        ): Color {
+            require(hue in 0f..360f && saturation in 0f..1f && value in 0f..1f) {
+                "HSV ($hue, $saturation, $value) must be in range (0..360, 0..1, 0..1)"
+            }
+            val red = hsvToRgbComponent(5, hue, saturation, value)
+            val green = hsvToRgbComponent(3, hue, saturation, value)
+            val blue = hsvToRgbComponent(1, hue, saturation, value)
+            return Color(red = red, green = green, blue = blue, alpha = alpha)
+        }
+
+        private fun hsvToRgbComponent(n: Int, h: Float, s: Float, v: Float): Float {
+            val k = (n.toFloat() + h / 60f) % 6f
+            return v - (v * s * max(0f, minOf(k, 4 - k, 1f)))
+        }
+
+        /**
+         * Return a [Color] from [hue], [saturation], and [lightness] (HSL representation).
+         *
+         * @param hue The colour value in the range (0..360), where 0 is red, 120 is green, and 240
+         *   is blue
+         * @param saturation The amount of [hue] represented in the color in the range (0..1), where
+         *   0 has no colour and 1 is fully saturated.
+         * @param lightness A range of (0..1) where 0 is black, 0.5 is fully colored, and 1 is
+         *   white.
+         * @param alpha Alpha channel to apply to the computed color
+         */
+        fun hsl(
+            hue: Float,
+            saturation: Float,
+            lightness: Float,
+            alpha: Float = 1f,
+        ): Color {
+            require(hue in 0f..360f && saturation in 0f..1f && lightness in 0f..1f) {
+                "HSL ($hue, $saturation, $lightness) must be in range (0..360, 0..1, 0..1)"
+            }
+            val red = hslToRgbComponent(0, hue, saturation, lightness)
+            val green = hslToRgbComponent(8, hue, saturation, lightness)
+            val blue = hslToRgbComponent(4, hue, saturation, lightness)
+            return Color(red = red, green = green, blue = blue, alpha = alpha)
+        }
+
+        private fun hslToRgbComponent(n: Int, h: Float, s: Float, l: Float): Float {
+            val k = (n.toFloat() + h / 30f) % 12f
+            val a = s * min(l, 1f - l)
+            return l - a * max(-1f, minOf(k - 3, 9 - k, 1f))
+        }
+        //#endregion
+
+        //#region Generate random colour
         /** Generates a random colour with the red, green and blue channels set to random values. */
         fun random(): Color = Color(
             red = (0..255).random(),
@@ -126,7 +197,7 @@ value class Color(
          * Generates a random colour with the alpha, red, green and blue channels set to random
          * values.
          *
-         * If the alpha channel being randomised is not desired, consider using [random] instead.
+         * If the alpha channel being randomised is not desired, consider using [Color.random] instead.
          */
         fun randomArgb(): Color = Color(
             red = (0..255).random(),
@@ -139,7 +210,7 @@ value class Color(
          * Generates a random colour with the alpha, red, green and blue channels set to random
          * values as seeded by the `random` argument.
          *
-         * If the alpha channel being randomised is not desired, consider using [random] instead.
+         * If the alpha channel being randomised is not desired, consider using [Color.random] instead.
          */
         fun randomArgb(random: Random): Color = Color(
             red = (0..255).random(random),
@@ -147,6 +218,7 @@ value class Color(
             blue = (0..255).random(random),
             alpha = (0..255).random(random),
         )
+        //#endregion
     }
 }
 
