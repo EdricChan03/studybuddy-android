5febe6ce28caab75172e80b2155037ec90fd081b
Edric <edric.chan.1997@gmail.com>
2026-06-05 15:22:37 +0800

    TaskNavigation.kt: Specify default NavOptionsBuilder options

--- features/tasks/navigation/src/main/kotlin/com/edricchan/studybuddy/features/tasks/navigation/TaskNavigation.kt
+++ features/tasks/navigation/src/main/kotlin/com/edricchan/studybuddy/features/tasks/navigation/TaskNavigation.kt
@@ -3,6 +3,10 @@ package com.edricchan.studybuddy.features.tasks.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavOptionsBuilder
 
+val DefaultNavOptionsBuilder: NavOptionsBuilder.() -> Unit = {
+    launchSingleTop = true
+}
+
 /**
  * Navigates to the [TaskDestination.TaskGraphRoot] route.
  *
@@ -10,27 +14,27 @@ import androidx.navigation.NavOptionsBuilder
  * behaviour is undesired, [builder] should be specified.
  */
 fun NavController.navigateToTaskGraph(
-    builder: NavOptionsBuilder.() -> Unit = { launchSingleTop = true }
+    builder: NavOptionsBuilder.() -> Unit = DefaultNavOptionsBuilder
 ) = navigate(TaskDestination.TaskGraphRoot, builder)
 
 /** Navigates to the [TaskDestination.ListTasks] route. */
 fun NavController.navigateToTasksList(
-    builder: NavOptionsBuilder.() -> Unit = {}
+    builder: NavOptionsBuilder.() -> Unit = DefaultNavOptionsBuilder
 ) = navigate(TaskDestination.ListTasks, builder)
 
 /** Navigates to the [TaskDestination.ViewTask] route with the given [taskId]. */
 fun NavController.navigateToViewTask(
     taskId: String,
-    builder: NavOptionsBuilder.() -> Unit = {}
+    builder: NavOptionsBuilder.() -> Unit = DefaultNavOptionsBuilder
 ) = navigate(TaskDestination.ViewTask(taskId), builder)
 
 /** Navigates to the [TaskDestination.EditTask] route with the given [taskId]. */
 fun NavController.navigateToEditTask(
     taskId: String,
-    builder: NavOptionsBuilder.() -> Unit = {}
+    builder: NavOptionsBuilder.() -> Unit = DefaultNavOptionsBuilder
 ) = navigate(TaskDestination.EditTask(taskId), builder)
 
 /** Navigates to the [TaskDestination.NewTask] route. */
 fun NavController.navigateToCreateTask(
-    builder: NavOptionsBuilder.() -> Unit = {}
+    builder: NavOptionsBuilder.() -> Unit = DefaultNavOptionsBuilder
 ) = navigate(TaskDestination.NewTask, builder)
