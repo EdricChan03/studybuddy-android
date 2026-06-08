b60f036e69f6a4c431ef9eb75fc380e10ef9d7ec
Edric <edric.chan.1997@gmail.com>
2026-06-08 16:14:03 +0800

    Add initial NavGraph DSL for authentication feature module
    Ditto on context parameters; see
    https://youtrack.jetbrains.com/issue/KTIJ-38699

---
+++ features/auth/src/main/kotlin/com/edricchan/studybuddy/features/auth/navigation/graph/AuthGraph.kt
@@ -0,0 +1,40 @@
+package com.edricchan.studybuddy.features.auth.navigation.graph
+
+import android.content.Context
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.fragment.fragment
+import androidx.navigation.navigation
+import com.edricchan.studybuddy.features.auth.account.compat.AccountFragment
+import com.edricchan.studybuddy.features.auth.login.ui.compat.LoginFragment
+import com.edricchan.studybuddy.features.auth.navigation.AuthDestination
+import com.edricchan.studybuddy.features.auth.recovery.ui.compat.RecoveryFragment
+import com.edricchan.studybuddy.features.auth.register.ui.compat.RegisterFragment
+import com.edricchan.studybuddy.features.auth.navigation.R as NavR
+
+/**
+ * Adds the authentication feature's nav-graph to the receiver [NavGraphBuilder].
+ * @param context [Context] to be used to retrieve the label string resources from.
+ * @see AuthDestination
+ */
+context(context: Context)
+fun NavGraphBuilder.authGraph(
+    isLoggedIn: Boolean
+) = navigation<AuthDestination.AuthGraphRoot>(
+    startDestination = if (isLoggedIn) AuthDestination.AccountInfo else AuthDestination.Login
+) {
+    fragment<AccountFragment, AuthDestination.AccountInfo> {
+        label = context.getString(NavR.string.nav_auth_dest_account_info_label)
+    }
+
+    fragment<LoginFragment, AuthDestination.Login> {
+        label = context.getString(NavR.string.nav_auth_dest_login_label)
+    }
+
+    fragment<RegisterFragment, AuthDestination.Register> {
+        label = context.getString(NavR.string.nav_auth_dest_register_label)
+    }
+
+    fragment<RecoveryFragment, AuthDestination.Recovery> {
+        label = context.getString(NavR.string.nav_auth_dest_recover_account_label)
+    }
+}
