abab2a393f0c04877af0fcbf6e309c4020d8ba02
Edric <edric.chan.1997@gmail.com>
2026-07-01 14:55:50 +0800

    DarkTheme.kt: Remove unused proto overload

--- ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/night/DarkTheme.kt
+++ ui/theming/compose/src/main/kotlin/com/edricchan/studybuddy/ui/theming/compose/night/DarkTheme.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.edricchan.studybuddy.core.settings.appearance.DarkModeSetting
 import com.edricchan.studybuddy.core.settings.appearance.DarkThemeValue
-import com.edricchan.studybuddy.core.settings.appearance.proto.DarkModeSettingProto
 import com.edricchan.studybuddy.ui.theming.common.night.shouldApplyDarkTheme as commonShouldApplyDarkTheme
 
 @Composable
@@ -15,21 +14,6 @@ fun shouldApplyDarkTheme(
     themeValue: DarkThemeValue.Version2
 ): Boolean = context.commonShouldApplyDarkTheme(
     themeValue = themeValue,
-    isSystemInDarkTheme = isSystemInDarkTheme()
-)
-
-@Deprecated(
-    "Use the overload which takes the domain DarkModeSetting enum class instead",
-    ReplaceWith(
-        "shouldApplyDarkTheme(themeSetting = DarkModeSetting.fromProto(themeSetting))",
-        "com.edricchan.studybuddy.core.settings.appearance.DarkModeSetting"
-    )
-)
-@Composable
-fun shouldApplyDarkTheme(
-    themeSetting: DarkModeSettingProto
-): Boolean = commonShouldApplyDarkTheme(
-    themeSetting = themeSetting,
     isSystemInDarkTheme = isSystemInDarkTheme()
 )
 
