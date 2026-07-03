0c231be116c2e640be354ec0468a5fd63300c5cd
Edric <edric.chan.1997@gmail.com>
2026-07-03 09:13:54 +0800

    Add list-item composables for task state fields

---
+++ features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/ui/fields/TaskStateFields.kt
@@ -0,0 +1,72 @@
+package com.edricchan.studybuddy.features.tasks.ui.fields
+
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemColors
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.ListItemShapes
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.edricchan.studybuddy.features.tasks.R
+
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun TaskMarkCompletedListItem(
+    modifier: Modifier = Modifier,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    enabled: Boolean = true,
+    colors: ListItemColors = ListItemDefaults.colors(),
+    shapes: ListItemShapes
+) = ListItem(
+    modifier = modifier,
+    checked = checked,
+    onCheckedChange = onCheckedChange,
+    enabled = enabled,
+    leadingContent = {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = null
+        )
+    },
+    content = {
+        Text(
+            text = stringResource(R.string.action_mark_task_as_done)
+        )
+    },
+    colors = colors,
+    shapes = shapes
+)
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun TaskMarkArchivedListItem(
+    modifier: Modifier = Modifier,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    enabled: Boolean = true,
+    colors: ListItemColors = ListItemDefaults.colors(),
+    shapes: ListItemShapes
+) = ListItem(
+    modifier = modifier,
+    checked = checked,
+    onCheckedChange = onCheckedChange,
+    enabled = enabled,
+    leadingContent = {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = null
+        )
+    },
+    content = {
+        Text(
+            text = stringResource(R.string.action_mark_task_as_archived)
+        )
+    },
+    colors = colors,
+    shapes = shapes
+)
