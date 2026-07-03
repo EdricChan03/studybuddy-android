8f936c8e3a2f45d3796bff6b26d6139974e024db
Edric <edric.chan.1997@gmail.com>
2026-07-03 10:02:46 +0800

    SnackBarHost.kt: Use explicit backing field for `snackBarData` property

--- ui/common/src/main/kotlin/com/edricchan/studybuddy/ui/common/snackbar/SnackBarHost.kt
+++ ui/common/src/main/kotlin/com/edricchan/studybuddy/ui/common/snackbar/SnackBarHost.kt
@@ -4,7 +4,6 @@ import com.edricchan.studybuddy.ui.common.SnackBarData
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 
 /**
  * Implementation of the [SnackBarController] interface to be used
@@ -12,20 +11,19 @@ import kotlinx.coroutines.flow.asSharedFlow
  * [collecting][SharedFlow.collect] the [snackBarData] flow.
  */
 class SnackBarHost : SnackBarController {
-    private val _snackBarData = MutableSharedFlow<SnackBarData>()
-
     /**
      * [SharedFlow] of the current [SnackBarData] to be shown.
      *
      * Users of the [SnackBarHost] class should [collect][SharedFlow.collect] this flow
      * to receive the desired snack-bars to be shown from across the app.
      */
-    val snackBarData: SharedFlow<SnackBarData> = _snackBarData.asSharedFlow()
+    val snackBarData: SharedFlow<SnackBarData>
+        field = MutableSharedFlow()
 
     override suspend fun showSnackBar(
         data: SnackBarData
     ) {
-        _snackBarData.emit(data)
+        snackBarData.emit(data)
     }
 
     /**
