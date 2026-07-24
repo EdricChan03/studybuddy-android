3d4cc6104d2ec44ae3a90813ab7c212bf46f6631
Edric <edric.chan.1997@gmail.com>
2026-07-24 17:05:34 +0800

    Bump Compose M3 from 1.5.0-alpha23 to 1.5.0-alpha24
    * Add back opt-ins for tooltip usages

--- features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/ui/card/TaskCard.kt
+++ features/tasks/src/main/kotlin/com/edricchan/studybuddy/features/tasks/ui/card/TaskCard.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.ButtonGroupDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
@@ -262,7 +263,7 @@ private fun TaskCardSelectionIndicator(modifier: Modifier = Modifier, selected: 
 }
 
 @Composable
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 private fun TaskCardActions(
     modifier: Modifier = Modifier,
     isCompleted: Boolean,
