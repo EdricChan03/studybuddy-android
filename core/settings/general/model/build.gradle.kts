f6aeb9f8646f3b9248af4f5000ff76dd5f572dfb
Edric <edric.chan.1997@gmail.com>
2026-07-27 15:05:53 +0800

    Scaffold general settings modules
    * Scaffolds:
      * `:core:settings:general:datastore`
      * `:core:settings:general:datastore-proto`
      * `:core:settings:general:model`
      * `:core:settings:general:resources`

---
+++ core/settings/general/model/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    com.edricchan.studybuddy.library.android
+}
+
+android {
+    namespace = "com.edricchan.studybuddy.core.settings.general.model"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    api(projects.core.settings.general.datastoreProto)
+    api(projects.core.settings.general.resources)
+
+    api(libs.androidx.annotation)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
