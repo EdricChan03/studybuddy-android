641bfcf987ea4de49243204dda081a499cd488a1
Edric <edric.chan.1997@gmail.com>
2026-08-07 16:48:28 +0800

    SegmentedList.kt: Add overloads for interactive variants
    Interactive variants being:
    * clickable
    * selectable
    * checkable/toggleable
    
    * Rename `Default` to `NonInteractive`
    * Also fixes list item colours not being
    propagated properly in some cases as
    we were using the item scope's item
    colours rather than the data class

--- utils/androidx/compose/src/main/kotlin/com/edricchan/studybuddy/utils/compose/material3/list/SegmentedList.kt
+++ utils/androidx/compose/src/main/kotlin/com/edricchan/studybuddy/utils/compose/material3/list/SegmentedList.kt
@@ -20,7 +20,7 @@ private sealed interface SegmentedItemData {
     @Composable
     fun SegmentedListItemScope.Content()
 
-    data class Default(
+    data class NonInteractive(
         val content: @Composable () -> Unit,
         val leadingContent: @Composable (() -> Unit)? = null,
         val trailingContent: @Composable (() -> Unit)? = null,
@@ -37,7 +37,101 @@ private sealed interface SegmentedItemData {
                 trailingContent = trailingContent,
                 overlineContent = overlineContent,
                 supportingContent = supportingContent,
-                colors = this.colors ?: colors
+                colors = this@NonInteractive.colors ?: colors
+            )
+        }
+    }
+
+    data class Clickable(
+        val onClick: () -> Unit,
+        val content: @Composable () -> Unit,
+        val enabled: Boolean = true,
+        val onLongClick: (() -> Unit)? = null,
+        val onLongClickLabel: String? = null,
+        val leadingContent: @Composable (() -> Unit)? = null,
+        val trailingContent: @Composable (() -> Unit)? = null,
+        val overlineContent: @Composable (() -> Unit)? = null,
+        val supportingContent: @Composable (() -> Unit)? = null,
+        val colors: ListItemColors? = null
+    ) : SegmentedItemData {
+        @Composable
+        override fun SegmentedListItemScope.Content() {
+            SegmentedListItem(
+                onClick = onClick,
+                enabled = enabled,
+                onLongClick = onLongClick,
+                onLongClickLabel = onLongClickLabel,
+                shapes = shapes,
+                content = content,
+                leadingContent = leadingContent,
+                trailingContent = trailingContent,
+                overlineContent = overlineContent,
+                supportingContent = supportingContent,
+                colors = this@Clickable.colors ?: colors
+            )
+        }
+    }
+
+    data class Checkable(
+        val checked: Boolean,
+        val onCheckedChange: (Boolean) -> Unit,
+        val content: @Composable () -> Unit,
+        val enabled: Boolean = true,
+        val onLongClick: (() -> Unit)? = null,
+        val onLongClickLabel: String? = null,
+        val leadingContent: @Composable (() -> Unit)? = null,
+        val trailingContent: @Composable (() -> Unit)? = null,
+        val overlineContent: @Composable (() -> Unit)? = null,
+        val supportingContent: @Composable (() -> Unit)? = null,
+        val colors: ListItemColors? = null
+    ) : SegmentedItemData {
+        @Composable
+        override fun SegmentedListItemScope.Content() {
+            SegmentedListItem(
+                checked = checked,
+                onCheckedChange = onCheckedChange,
+                onLongClick = onLongClick,
+                onLongClickLabel = onLongClickLabel,
+                enabled = enabled,
+                shapes = shapes,
+                content = content,
+                leadingContent = leadingContent,
+                trailingContent = trailingContent,
+                overlineContent = overlineContent,
+                supportingContent = supportingContent,
+                colors = this@Checkable.colors ?: colors
+            )
+        }
+    }
+
+    data class Selectable(
+        val selected: Boolean,
+        val onClick: () -> Unit,
+        val content: @Composable () -> Unit,
+        val enabled: Boolean = true,
+        val onLongClick: (() -> Unit)? = null,
+        val onLongClickLabel: String? = null,
+        val leadingContent: @Composable (() -> Unit)? = null,
+        val trailingContent: @Composable (() -> Unit)? = null,
+        val overlineContent: @Composable (() -> Unit)? = null,
+        val supportingContent: @Composable (() -> Unit)? = null,
+        val colors: ListItemColors? = null
+    ) : SegmentedItemData {
+        @Composable
+        override fun SegmentedListItemScope.Content() {
+            SegmentedListItem(
+                selected = selected,
+                onClick = onClick,
+                onLongClick = onLongClick,
+                onLongClickLabel = onLongClickLabel,
+                enabled = enabled,
+                shapes = shapes,
+                content = content,
+                leadingContent = leadingContent,
+                trailingContent = trailingContent,
+                overlineContent = overlineContent,
+                supportingContent = supportingContent,
+                colors = this@Selectable.colors ?: colors
             )
         }
     }
@@ -64,6 +158,97 @@ interface SegmentedListScope {
      * @param content Main content for the list item.
      */
     fun item(
+        leadingContent: @Composable (() -> Unit)? = null,
+        trailingContent: @Composable (() -> Unit)? = null,
+        overlineContent: @Composable (() -> Unit)? = null,
+        supportingContent: @Composable (() -> Unit)? = null,
+        colors: ListItemColors? = null,
+        content: @Composable () -> Unit,
+    )
+
+    /**
+     * Adds a clickable [SegmentedListItem] to the segmented list.
+     * @param onClick Invoked when the list item is clicked on.
+     * @param enabled Controls the enabled state of this list item. When `false`,
+     * this component will not respond to user input, and it will appear visually
+     * disabled and disabled to accessibility services.
+     * @param onLongClick Invoked when the list item is long clicked on.
+     * @param onLongClickLabel Accessible label for the long click event.
+     * @param leadingContent Leading (start) content for the list item.
+     * @param trailingContent Trailing (end) content for the list item.
+     * @param overlineContent Overline content for the list item.
+     * @param supportingContent Supporting content for the list item.
+     * @param colors Desired [ListItemColors] for this list item. If not specified, it
+     * defaults to the [ListItemColors] that are passed to the segmented list.
+     * @param content Main content for the list item.
+     */
+    fun item(
+        onClick: () -> Unit,
+        enabled: Boolean = true,
+        onLongClick: (() -> Unit)? = null,
+        onLongClickLabel: String? = null,
+        leadingContent: @Composable (() -> Unit)? = null,
+        trailingContent: @Composable (() -> Unit)? = null,
+        overlineContent: @Composable (() -> Unit)? = null,
+        supportingContent: @Composable (() -> Unit)? = null,
+        colors: ListItemColors? = null,
+        content: @Composable () -> Unit,
+    )
+
+    /**
+     * Adds a checkable [SegmentedListItem] to the segmented list.
+     * @param checked Whether the list item is checked.
+     * @param onCheckedChange Invoked when the list item's checked state has changed.
+     * @param enabled Controls the enabled state of this list item. When `false`,
+     * this component will not respond to user input, and it will appear visually
+     * disabled and disabled to accessibility services.
+     * @param onLongClick Invoked when the list item is long clicked on.
+     * @param onLongClickLabel Accessible label for the long click event.
+     * @param leadingContent Leading (start) content for the list item.
+     * @param trailingContent Trailing (end) content for the list item.
+     * @param overlineContent Overline content for the list item.
+     * @param supportingContent Supporting content for the list item.
+     * @param colors Desired [ListItemColors] for this list item. If not specified, it
+     * defaults to the [ListItemColors] that are passed to the segmented list.
+     * @param content Main content for the list item.
+     */
+    fun item(
+        checked: Boolean,
+        onCheckedChange: (Boolean) -> Unit,
+        enabled: Boolean = true,
+        onLongClick: (() -> Unit)? = null,
+        onLongClickLabel: String? = null,
+        leadingContent: @Composable (() -> Unit)? = null,
+        trailingContent: @Composable (() -> Unit)? = null,
+        overlineContent: @Composable (() -> Unit)? = null,
+        supportingContent: @Composable (() -> Unit)? = null,
+        colors: ListItemColors? = null,
+        content: @Composable () -> Unit,
+    )
+
+    /**
+     * Adds a selectable [SegmentedListItem] to the segmented list.
+     * @param selected Whether the list item is selected.
+     * @param onClick Invoked when the list item is clicked on.
+     * @param enabled Controls the enabled state of this list item. When `false`,
+     * this component will not respond to user input, and it will appear visually
+     * disabled and disabled to accessibility services.
+     * @param onLongClick Invoked when the list item is long clicked on.
+     * @param onLongClickLabel Accessible label for the long click event.
+     * @param leadingContent Leading (start) content for the list item.
+     * @param trailingContent Trailing (end) content for the list item.
+     * @param overlineContent Overline content for the list item.
+     * @param supportingContent Supporting content for the list item.
+     * @param colors Desired [ListItemColors] for this list item. If not specified, it
+     * defaults to the [ListItemColors] that are passed to the segmented list.
+     * @param content Main content for the list item.
+     */
+    fun item(
+        selected: Boolean,
+        onClick: () -> Unit,
+        enabled: Boolean = true,
+        onLongClick: (() -> Unit)? = null,
+        onLongClickLabel: String? = null,
         leadingContent: @Composable (() -> Unit)? = null,
         trailingContent: @Composable (() -> Unit)? = null,
         overlineContent: @Composable (() -> Unit)? = null,
@@ -110,7 +295,7 @@ private class SegmentedListScopeImpl(
         colors: ListItemColors?,
         content: @Composable (() -> Unit),
     ) {
-        items += SegmentedItemData.Default(
+        items += SegmentedItemData.NonInteractive(
             content = content,
             leadingContent = leadingContent,
             trailingContent = trailingContent,
@@ -120,6 +305,87 @@ private class SegmentedListScopeImpl(
         )
     }
 
+    override fun item(
+        onClick: () -> Unit,
+        enabled: Boolean,
+        onLongClick: (() -> Unit)?,
+        onLongClickLabel: String?,
+        leadingContent: @Composable (() -> Unit)?,
+        trailingContent: @Composable (() -> Unit)?,
+        overlineContent: @Composable (() -> Unit)?,
+        supportingContent: @Composable (() -> Unit)?,
+        colors: ListItemColors?,
+        content: @Composable (() -> Unit)
+    ) {
+        items += SegmentedItemData.Clickable(
+            content = content,
+            onClick = onClick,
+            enabled = enabled,
+            onLongClick = onLongClick,
+            onLongClickLabel = onLongClickLabel,
+            leadingContent = leadingContent,
+            trailingContent = trailingContent,
+            overlineContent = overlineContent,
+            supportingContent = supportingContent,
+            colors = colors
+        )
+    }
+
+    override fun item(
+        checked: Boolean,
+        onCheckedChange: (Boolean) -> Unit,
+        enabled: Boolean,
+        onLongClick: (() -> Unit)?,
+        onLongClickLabel: String?,
+        leadingContent: @Composable (() -> Unit)?,
+        trailingContent: @Composable (() -> Unit)?,
+        overlineContent: @Composable (() -> Unit)?,
+        supportingContent: @Composable (() -> Unit)?,
+        colors: ListItemColors?,
+        content: @Composable (() -> Unit)
+    ) {
+        items += SegmentedItemData.Checkable(
+            content = content,
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            enabled = enabled,
+            onLongClick = onLongClick,
+            onLongClickLabel = onLongClickLabel,
+            leadingContent = leadingContent,
+            trailingContent = trailingContent,
+            overlineContent = overlineContent,
+            supportingContent = supportingContent,
+            colors = colors
+        )
+    }
+
+    override fun item(
+        selected: Boolean,
+        onClick: () -> Unit,
+        enabled: Boolean,
+        onLongClick: (() -> Unit)?,
+        onLongClickLabel: String?,
+        leadingContent: @Composable (() -> Unit)?,
+        trailingContent: @Composable (() -> Unit)?,
+        overlineContent: @Composable (() -> Unit)?,
+        supportingContent: @Composable (() -> Unit)?,
+        colors: ListItemColors?,
+        content: @Composable (() -> Unit)
+    ) {
+        items += SegmentedItemData.Selectable(
+            content = content,
+            selected = selected,
+            onClick = onClick,
+            enabled = enabled,
+            onLongClick = onLongClick,
+            onLongClickLabel = onLongClickLabel,
+            leadingContent = leadingContent,
+            trailingContent = trailingContent,
+            overlineContent = overlineContent,
+            supportingContent = supportingContent,
+            colors = colors
+        )
+    }
 
     override fun customItem(content: @Composable SegmentedListItemScope.() -> Unit) {
         items += SegmentedItemData.Custom(
