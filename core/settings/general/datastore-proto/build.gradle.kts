6f5f30a0429b169465981895f6e1231590027882
Edric <edric.chan.1997@gmail.com>
2026-08-15 23:53:33 +0800

    Add initial locale settings ProtoBuf message/enums (#993)
    For the upcoming general settings page
    re-organisation (#992)
    
    * Add dependency on
      :data:common:protobuf

--- core/settings/general/datastore-proto/build.gradle.kts
+++ core/settings/general/datastore-proto/build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     com.edricchan.studybuddy.library.`jvm-protobuf`
 }
+
+dependencies {
+    // protoSource would include :data:common:protobuf's sources in this library, which
+    // would result in duplicate code
+    protoPath(projects.data.common.protobuf)
+    api(projects.data.common.protobuf)
+}
