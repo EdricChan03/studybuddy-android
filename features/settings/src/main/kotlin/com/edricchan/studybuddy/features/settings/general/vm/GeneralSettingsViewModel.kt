2b687269810eae613bce372d905a73d34fa6c6f9
Edric <edric.chan.1997@gmail.com>
2026-08-23 17:58:25 +0800

    GeneralSettingsViewModel.kt: Remove unused preference properties

--- features/settings/src/main/kotlin/com/edricchan/studybuddy/features/settings/general/vm/GeneralSettingsViewModel.kt
+++ features/settings/src/main/kotlin/com/edricchan/studybuddy/features/settings/general/vm/GeneralSettingsViewModel.kt
@@ -3,11 +3,9 @@ package com.edricchan.studybuddy.features.settings.general.vm
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.edricchan.studybuddy.core.settings.appearance.DarkThemeValue
 import com.edricchan.studybuddy.core.settings.general.repo.GeneralSettingsRepository
 import com.edricchan.studybuddy.core.settings.tracking.keyPrefEnableUserTracking
 import com.edricchan.studybuddy.exts.androidx.preference.defaultSharedPreferences
-import com.edricchan.studybuddy.ui.theming.common.ThemePreferences
 import com.fredporciuncula.flow.preferences.FlowSharedPreferences
 import com.fredporciuncula.flow.preferences.Preference
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -24,8 +22,6 @@ class GeneralSettingsViewModel @Inject constructor(
         context.defaultSharedPreferences
     )
 
-    private val themePrefs = ThemePreferences(context)
-
     val prefEnableUserTracking: Preference<Boolean> = appPreferences.getBoolean(
         keyPrefEnableUserTracking,
         defaultValue = false
@@ -38,8 +34,4 @@ class GeneralSettingsViewModel @Inject constructor(
             repository.setOpenLinksInApp(value)
         }
     }
-
-    val prefDarkTheme: Preference<DarkThemeValue.Version2> = themePrefs.prefDarkTheme
-
-    val prefEnableDynamicTheme: Preference<Boolean> = themePrefs.prefEnableDynamicTheme
 }
