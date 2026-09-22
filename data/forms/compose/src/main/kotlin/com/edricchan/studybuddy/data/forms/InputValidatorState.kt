d4795c335d753d2aeedd30cf2f908a11dda474f9
Edric <edric.chan.1997@gmail.com>
2026-09-22 15:56:12 +0800

    InputValidatorState.kt: Add overload which accepts an interaction source
    This allows for the error to only be shown when the text-field is no
    longer focused

--- data/forms/compose/src/main/kotlin/com/edricchan/studybuddy/data/forms/InputValidatorState.kt
+++ data/forms/compose/src/main/kotlin/com/edricchan/studybuddy/data/forms/InputValidatorState.kt
@@ -1,5 +1,7 @@
 package com.edricchan.studybuddy.data.forms
 
+import androidx.compose.foundation.interaction.FocusInteraction
+import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
@@ -7,9 +9,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.edricchan.studybuddy.data.forms.compose.InputValidationError
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -28,6 +33,25 @@ fun <ValidationError : InputValidationError> TextFieldState.validationErrorAsSta
     snapshotFlow { text }
         .debounce(debounceTimeout)
         .map(validator::validate)
+}.collectAsStateWithLifecycle(null)
+
+/**
+ * Collects and returns the [ValidationError] of the receiver [TextFieldState]'s text
+ * as a Compose [State], emitting new [ValidationError]s only when the [interactionSource]
+ * receives a focus interaction.
+ * @param debounceTimeout Timeout to be passed to the [debounce] operator.
+ * @param validator Validator to be used to validate the [TextFieldState]'s text with.
+ */
+@OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+@Composable
+fun <ValidationError : InputValidationError> TextFieldState.validationErrorAsState(
+    validator: InputValidator<ValidationError>,
+    interactionSource: InteractionSource,
+    debounceTimeout: Duration = 200.milliseconds,
+): State<ValidationError?> = remember {
+    interactionSource.interactions.filterIsInstance<FocusInteraction.Unfocus>()
+        .debounce(debounceTimeout)
+        .mapLatest { validator.validate(text) }
 }.collectAsStateWithLifecycle(null)
 
 /**
